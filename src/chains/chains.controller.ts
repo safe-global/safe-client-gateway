d063878de60b2979dceee609beaf8cd0454d23b6
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-08-09 10:14:46 +0200

    Add managed API versioning

--- src/chains/chains.controller.ts
+++ src/chains/chains.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, VERSION_NEUTRAL } from '@nestjs/common';
 import { ChainsService } from './chains.service';
 import { Chain } from './entities/chain.entity';
 import { Page } from './entities/page.entity';
 
-@Controller('chains')
+@Controller({
+  path: 'chains',
+  version: VERSION_NEUTRAL,
+})
 export class ChainsController {
   constructor(private readonly chainsService: ChainsService) {}
 
