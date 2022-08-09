187cfd46c95e5b68b2b3bfd6afe1c74ec61eb75a
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-08-09 11:40:59 +0200

    Move chains endpoint to /v1

--- src/chains/chains.controller.ts
+++ src/chains/chains.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, VERSION_NEUTRAL } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ChainsService } from './chains.service';
 import { Chain } from './entities/chain.entity';
 import { Page } from './entities/page.entity';
 
 @Controller({
   path: 'chains',
-  version: VERSION_NEUTRAL,
+  version: '1',
 })
 export class ChainsController {
   constructor(private readonly chainsService: ChainsService) {}
