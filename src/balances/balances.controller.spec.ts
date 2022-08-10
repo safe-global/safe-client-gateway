488445401adc3703b9ef3159c5b639b6e3a17aad
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2022-08-10 12:05:18 +0200

    Mock axios

--- src/balances/balances.controller.spec.ts
+++ src/balances/balances.controller.spec.ts
@@ -1,22 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { SafeTransactionModule } from '../services/safe-transaction/safe-transaction.module';
-import { BalancesController } from './balances.controller';
-import { BalancesService } from './balances.service';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import axios from 'axios';
+import { BalancesModule } from './balances.module';
 
-describe('BalancesController', () => {
-  let controller: BalancesController;
+jest.mock('axios');
+const axiosMock = axios as jest.Mocked<typeof axios>;
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [SafeTransactionModule],
-      controllers: [BalancesController],
-      providers: [BalancesService],
+    jest.clearAllMocks();
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [BalancesModule],
     }).compile();
-
-    controller = module.get<BalancesController>(BalancesController);
+    app = moduleFixture.createNestApplication();
+    await app.init();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it(`GET /balances`, async () => {
+    const chainId = 1;
+    const safeAddress = '0x0000000';
+    const safeTransactionServiceBalancesResponse = {
+      data: [{ tokenAddress: '0x000000', balance: '1' }],
+    };
+    const safeConfigServiceResponse = {
+      data: {
+        chainId: '1',
+        chainName: 'mainnet',
+        transactionService: 'https://test.safe.transaction.service',
+        vpcTransactionService: 'https://test.safe.transaction.service',
+      },
+    };
+    axiosMock.get.mockImplementation((url) => {
+      if (url == 'https://safe-config.gnosis.io/api/v1/chains/1') {
+        return Promise.resolve(safeConfigServiceResponse);
+      } else if (
+        url ==
+        `https://test.safe.transaction.service/api/v1/safes/${safeAddress}/balances/usd/`
+      ) {
+        return Promise.resolve(safeTransactionServiceBalancesResponse);
+      } else {
+        return Promise.reject(new Error(`Could not match ${url}`));
+      }
+    });
+
+    await request(app.getHttpServer())
+      .get(`/chains/${chainId}/safes/${safeAddress}/balances`)
+      .expect(200)
+      .expect([{ tokenAddress: '0x000000', balance: '1' }]);
+
+    expect(axiosMock.get.mock.calls[0][0]).toBe(
+      'https://safe-config.gnosis.io/api/v1/chains/1',
+    );
+    expect(axiosMock.get.mock.calls[1][0]).toBe(
+      `https://test.safe.transaction.service/api/v1/safes/${safeAddress}/balances/usd/`,
+    );
+    expect(axiosMock.get.mock.calls[1][1]).toStrictEqual({
+      params: { trusted: undefined, excludeSpam: undefined },
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
