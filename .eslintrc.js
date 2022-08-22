586700253ae8f0f76c6ae3d70a55c5bd6c777245
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-08-22 17:30:05 +0200

    Test prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'prettier',
+    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
@@ -21,6 +21,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'max-len': ['warn', 120],
+    'prettier/prettier': 'off',
   },
 };
