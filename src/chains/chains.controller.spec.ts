3fef66096fce7abf60aefb5e403fa522fb2176ea
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-08-22 11:46:41 +0200

    Integrate with eslint max-len

--- src/chains/chains.controller.spec.ts
+++ src/chains/chains.controller.spec.ts
@@ -6,10 +6,7 @@ import { ChainsModule } from './chains.module';
 import { Backbone, Chain, Page } from './entities';
 import backboneFactory from './entities/__tests__/backbone.factory';
 import { mockNetworkService, TestNetworkModule } from '../common/network/__tests__/test.network.module';
-import {
-  fakeConfigurationService,
-  TestConfigurationModule,
-} from '../common/config/__tests__/test.configuration.module';
+import { fakeConfigurationService, TestConfigurationModule } from '../common/config/__tests__/test.configuration.module';
 
 describe('Chains Controller (Unit)', () => {
   let app: INestApplication;
@@ -84,10 +81,7 @@ describe('Chains Controller (Unit)', () => {
       mockNetworkService.get.mockResolvedValueOnce({ data: chainResponse });
       mockNetworkService.get.mockResolvedValueOnce({ data: backboneResponse });
 
-      await request(app.getHttpServer())
-        .get('/chains/1/about/backbone')
-        .expect(HttpStatus.OK)
-        .expect(backboneResponse);
+      await request(app.getHttpServer()).get('/chains/1/about/backbone').expect(HttpStatus.OK).expect(backboneResponse);
 
       expect(mockNetworkService.get).toBeCalledTimes(2);
       expect(mockNetworkService.get).toHaveBeenNthCalledWith(1, expect.stringContaining('/api/v1/chains/1'));
@@ -99,13 +93,10 @@ describe('Chains Controller (Unit)', () => {
         status: HttpStatus.BAD_REQUEST,
       });
 
-      await request(app.getHttpServer())
-        .get('/chains/1/about/backbone')
-        .expect(HttpStatus.SERVICE_UNAVAILABLE)
-        .expect({
-          message: 'Service unavailable',
-          code: HttpStatus.SERVICE_UNAVAILABLE,
-        });
+      await request(app.getHttpServer()).get('/chains/1/about/backbone').expect(HttpStatus.SERVICE_UNAVAILABLE).expect({
+        message: 'Service unavailable',
+        code: HttpStatus.SERVICE_UNAVAILABLE,
+      });
 
       expect(mockNetworkService.get).toBeCalledTimes(1);
       expect(mockNetworkService.get).toBeCalledWith(expect.stringContaining('/api/v1/chains/1'));
@@ -117,13 +108,10 @@ describe('Chains Controller (Unit)', () => {
         status: HttpStatus.BAD_GATEWAY,
       });
 
-      await request(app.getHttpServer())
-        .get('/chains/1/about/backbone')
-        .expect(HttpStatus.SERVICE_UNAVAILABLE)
-        .expect({
-          message: 'Service unavailable',
-          code: HttpStatus.SERVICE_UNAVAILABLE,
-        });
+      await request(app.getHttpServer()).get('/chains/1/about/backbone').expect(HttpStatus.SERVICE_UNAVAILABLE).expect({
+        message: 'Service unavailable',
+        code: HttpStatus.SERVICE_UNAVAILABLE,
+      });
 
       expect(mockNetworkService.get).toBeCalledTimes(2);
       expect(mockNetworkService.get).toHaveBeenNthCalledWith(1, expect.stringContaining('/api/v1/chains/1'));
