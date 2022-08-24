278ccf34d0903a421acb629c401209cfcef15af4
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-08-24 13:13:08 +0200

    Separate responsibilities between eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
@@ -20,7 +19,6 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
   },
 };
