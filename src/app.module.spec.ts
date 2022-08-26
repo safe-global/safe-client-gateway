0c513a115e170120e3d67bffbba6d476227debe0
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-08-26 16:15:24 +0200

    Import TestConfigurationModule when testing AppModule

--- src/app.module.spec.ts
+++ src/app.module.spec.ts
@@ -1,10 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from './app.module';
+import { TestConfigurationModule } from './common/config/__tests__/test.configuration.module';
 
 describe('AppModule', () => {
   it(`AppModule is successfully created`, async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, TestConfigurationModule],
     }).compile();
 
     const app = moduleFixture.createNestApplication();
