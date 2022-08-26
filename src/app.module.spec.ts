14ed4b2dfbaff34c7bf26a6928b4e8564c30cfd5
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-08-26 16:18:55 +0200

    Add mock configuration when testing AppModule

--- src/app.module.spec.ts
+++ src/app.module.spec.ts
@@ -1,8 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from './app.module';
-import { TestConfigurationModule } from './common/config/__tests__/test.configuration.module';
+import { fakeConfigurationService, TestConfigurationModule } from './common/config/__tests__/test.configuration.module';
 
 describe('AppModule', () => {
+  beforeAll(async () => {
+    fakeConfigurationService.set('exchange.baseUri', 'https://test.exchange');
+    fakeConfigurationService.set('exchange.apiKey', 'testKey');
+    fakeConfigurationService.set(
+      'safeConfig.baseUri',
+      'https://test.safe.config',
+    );
+  });
+
   it(`AppModule is successfully created`, async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule, TestConfigurationModule],
