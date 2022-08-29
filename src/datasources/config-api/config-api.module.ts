e62939ed256315dd4745ead511a7e6aeadca05cf
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2022-08-29 15:46:05 +0100

    Rename cacheFirstDataSourceModule.ts to cache.first.data.source.module.ts

--- src/datasources/config-api/config-api.module.ts
+++ src/datasources/config-api/config-api.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigApi } from './config-api.service';
-import { CacheFirstDataSourceModule } from '../cache/cacheFirstDataSourceModule';
+import { CacheFirstDataSourceModule } from '../cache/cache.first.data.source.module';
 
 @Module({
   imports: [CacheFirstDataSourceModule],
--- src/datasources/transaction-api/transaction-api.module.ts
+++ src/datasources/transaction-api/transaction-api.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigApiModule } from '../config-api/config-api.module';
 import { TransactionApiManager } from './transaction-api.manager';
-import { CacheFirstDataSourceModule } from '../cache/cacheFirstDataSourceModule';
+import { CacheFirstDataSourceModule } from '../cache/cache.first.data.source.module';
 
 @Module({
   imports: [ConfigApiModule, CacheFirstDataSourceModule],
