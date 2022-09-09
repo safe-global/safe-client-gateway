dd97b195bed07638be2c3cd6bb02dfb672c8861d
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-09-09 14:23:40 +0200

    Code formatting

--- src/domain/exchange/exchange.validator.ts
+++ src/domain/exchange/exchange.validator.ts
@@ -1,21 +1,23 @@
-import { Injectable } from "@nestjs/common";
-import { ValidateFunction, DefinedError } from "ajv";
-import { JsonSchemaService } from "../../common/schemas/json-schema.service";
-import { ValidationErrorFactory } from "../errors/validation-error-factory";
-import { IValidator } from "../interfaces/validator.interface";
+import { Injectable } from '@nestjs/common';
+import { ValidateFunction, DefinedError } from 'ajv';
+import { JsonSchemaService } from '../../common/schemas/json-schema.service';
+import { ValidationErrorFactory } from '../errors/validation-error-factory';
+import { IValidator } from '../interfaces/validator.interface';
 import { ExchangeResult } from './entities/exchange-result.entity';
 import { FiatCodesExchangeResult } from './entities/fiat-codes-result.entity';
-import { exchangeResultSchema } from "./entities/schemas/exchange-result.schema";
-import { fiatCodesExchangeResultSchema } from "./entities/schemas/fiat-codes-exchange-result.schema";
+import { exchangeResultSchema } from './entities/schemas/exchange-result.schema';
+import { fiatCodesExchangeResultSchema } from './entities/schemas/fiat-codes-exchange-result.schema';
 
 @Injectable()
-export class ExchangeValidator implements IValidator<ExchangeResult | FiatCodesExchangeResult> {
+export class ExchangeValidator
+  implements IValidator<ExchangeResult | FiatCodesExchangeResult>
+{
   private readonly isValidExchangeResult: ValidateFunction<ExchangeResult>;
   private readonly isValidFiatCodesExchangeResult: ValidateFunction<FiatCodesExchangeResult>;
 
   constructor(
     private readonly validationErrorFactory: ValidationErrorFactory,
-    private readonly jsonSchemaService: JsonSchemaService
+    private readonly jsonSchemaService: JsonSchemaService,
   ) {
     this.isValidExchangeResult = this.jsonSchemaService.compile(
       exchangeResultSchema,
@@ -26,26 +28,31 @@ export class ExchangeValidator implements IValidator<ExchangeResult | FiatCodesE
     ) as ValidateFunction<FiatCodesExchangeResult>;
   }
 
-  isExchangeResult(data: ExchangeResult | FiatCodesExchangeResult): data is ExchangeResult {
+  isExchangeResult(
+    data: ExchangeResult | FiatCodesExchangeResult,
+  ): data is ExchangeResult {
     return (data as ExchangeResult).rates !== undefined;
   }
 
-  isFiatCodesExchangeResult(data: ExchangeResult | FiatCodesExchangeResult): data is FiatCodesExchangeResult {
+  isFiatCodesExchangeResult(
+    data: ExchangeResult | FiatCodesExchangeResult,
+  ): data is FiatCodesExchangeResult {
     return (data as FiatCodesExchangeResult).symbols !== undefined;
   }
 
   validate(data: unknown): ExchangeResult | FiatCodesExchangeResult {
     if (this.isExchangeResult(data as ExchangeResult)) {
-      if ( !this.isValidExchangeResult(data)) {
+      if (!this.isValidExchangeResult(data)) {
         const errors = this.isValidExchangeResult.errors as DefinedError[];
         throw this.validationErrorFactory.from(errors);
       }
-  
+
       return data as ExchangeResult;
     }
 
-    if ( !this.isValidFiatCodesExchangeResult(data)) {
-      const errors = this.isValidFiatCodesExchangeResult.errors as DefinedError[];
+    if (!this.isValidFiatCodesExchangeResult(data)) {
+      const errors = this.isValidFiatCodesExchangeResult
+        .errors as DefinedError[];
       throw this.validationErrorFactory.from(errors);
     }
 
@@ -54,5 +61,4 @@ export class ExchangeValidator implements IValidator<ExchangeResult | FiatCodesE
   validateMany(data: unknown[]): (ExchangeResult | FiatCodesExchangeResult)[] {
     return data.map((item) => this.validate(item));
   }
-
 }
