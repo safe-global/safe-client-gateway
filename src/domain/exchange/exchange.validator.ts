276598ed245201261493856fd95863787bd4a672
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-09-16 11:00:53 +0200

    Add generic validation fn to ExchangeValidator

--- src/domain/exchange/exchange.validator.ts
+++ src/domain/exchange/exchange.validator.ts
@@ -11,14 +11,14 @@ import { ratesExchangeResultSchema } from './entities/schemas/rates-exchange-res
 
 @Injectable()
 export class ExchangeValidator implements IValidator<ExchangeResult> {
-  private readonly isValidExchangeResult: ValidateFunction<RatesExchangeResult>;
+  private readonly isValidRatesExchangeResult: ValidateFunction<RatesExchangeResult>;
   private readonly isValidFiatCodesExchangeResult: ValidateFunction<FiatCodesExchangeResult>;
 
   constructor(
     private readonly validationErrorFactory: ValidationErrorFactory,
     private readonly jsonSchemaService: JsonSchemaService,
   ) {
-    this.isValidExchangeResult = this.jsonSchemaService.compile(
+    this.isValidRatesExchangeResult = this.jsonSchemaService.compile(
       ratesExchangeResultSchema,
     ) as ValidateFunction<RatesExchangeResult>;
 
@@ -27,7 +27,7 @@ export class ExchangeValidator implements IValidator<ExchangeResult> {
     ) as ValidateFunction<FiatCodesExchangeResult>;
   }
 
-  isExchangeResult(data: ExchangeResult): data is RatesExchangeResult {
+  isRatesExchangeResult(data: ExchangeResult): data is RatesExchangeResult {
     return (data as RatesExchangeResult).rates !== undefined;
   }
 
@@ -37,25 +37,52 @@ export class ExchangeValidator implements IValidator<ExchangeResult> {
     return (data as FiatCodesExchangeResult).symbols !== undefined;
   }
 
+  /**
+   * Validates arbitrary data against one of the subtypes of {@link ExchangeResult}
+   * @param data arbitrary data to be validated
+   * @returns validated data coerced to a known type (extending from {@link ExchangeResult})
+   */
   validate(data: unknown): ExchangeResult {
-    if (this.isExchangeResult(data as RatesExchangeResult)) {
-      if (!this.isValidExchangeResult(data)) {
-        const errors = this.isValidExchangeResult.errors as DefinedError[];
-        throw this.validationErrorFactory.from(errors);
-      }
-
-      return data as RatesExchangeResult;
+    if (this.isRatesExchangeResult(data as RatesExchangeResult)) {
+      return this.validateByType<RatesExchangeResult>(
+        data as RatesExchangeResult,
+        this.isValidRatesExchangeResult,
+      );
     }
 
-    if (!this.isValidFiatCodesExchangeResult(data)) {
-      const errors = this.isValidFiatCodesExchangeResult
-        .errors as DefinedError[];
+    if (this.isFiatCodesExchangeResult(data as FiatCodesExchangeResult)) {
+      return this.validateByType<FiatCodesExchangeResult>(
+        data as FiatCodesExchangeResult,
+        this.isValidFiatCodesExchangeResult,
+      );
+    }
+
+    throw this.validationErrorFactory.from([]);
+  }
+
+  /**
+   * Validates an array of items by calling {@link validate} for each one
+   */
+  validateMany(data: unknown[]): ExchangeResult[] {
+    return data.map((item) => this.validate(item));
+  }
+
+  /**
+   * Generic validation function to be executed against the provided {@link ValidateFunction}
+   * Returns an object of the provided type, or throws an error provided by the {@link ValidationErrorFactory}
+   * @param data arbitrary data to be validated
+   * @param validationFn {@link ValidateFunction} to be applied to the data
+   * @returns validated data coerced to a known type (extending from {@link ExchangeResult})
+   */
+  private validateByType<Type extends ExchangeResult>(
+    data: Type,
+    validationFn: ValidateFunction<Type>,
+  ): Type {
+    if (!validationFn(data)) {
+      const errors = validationFn.errors as DefinedError[];
       throw this.validationErrorFactory.from(errors);
     }
 
-    return data as FiatCodesExchangeResult;
-  }
-  validateMany(data: unknown[]): ExchangeResult[] {
-    return data.map((item) => this.validate(item));
+    return data as Type;
   }
 }
