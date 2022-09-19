69c34bc48a9e9a2ab14c20861671e1c9793aa66c
moisses89 <moisses89.fn@gmail.com>
2022-09-19 16:36:56 +0200

    Add beforeEach and beforeAll

--- src/datasources/exchange-api/exchange-api.service.spec.ts
+++ src/datasources/exchange-api/exchange-api.service.spec.ts
@@ -1,28 +1,23 @@
 import { ExchangeApi } from './exchange-api.service';
-import { ExchangeResult } from '../../domain/exchange/entities/exchange-result.entity';
+import exchangeResultFactory from '../../domain/exchange/entities/__tests__/exchange.factory';
 import { FiatCodesExchangeResult } from '../../domain/exchange/entities/fiat-codes-result.entity';
 import { FakeConfigurationService } from '../../common/config/__tests__/fake.configuration.service';
 import { mockNetworkService } from '../../datasources/network/__tests__/test.network.module';
 
-const FIATCODES: FiatCodesExchangeResult = {
-  success: true,
-  symbols: {},
-};
-
-const EXCHANGERESULT: ExchangeResult = {
-  success: true,
-  rates: {},
-  base: '',
-};
-
 describe('ExchangeApi', () => {
-  const fakeConfigurationService = new FakeConfigurationService();
-  fakeConfigurationService.set('exchange.baseUri', 'http://exchange.test');
-  fakeConfigurationService.set('exchange.apiKey', 'aaaaaaaabbbbbbb');
-  const service: ExchangeApi = new ExchangeApi(
-    fakeConfigurationService,
-    mockNetworkService,
-  );
+  let service: ExchangeApi;
+  let fakeConfigurationService: FakeConfigurationService;
+
+  beforeAll(async () => {
+    fakeConfigurationService = new FakeConfigurationService();
+    fakeConfigurationService.set('exchange.baseUri', 'http://exchange.test');
+    fakeConfigurationService.set('exchange.apiKey', 'aaaaaaaabbbbbbb');
+  });
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+    service = new ExchangeApi(fakeConfigurationService, mockNetworkService);
+  });
 
   it('should error if configuration is not defined', async () => {
     const fakeConfigurationService = new FakeConfigurationService();
@@ -32,14 +27,23 @@ describe('ExchangeApi', () => {
   });
 
   it('Should return the fiatcodes', async () => {
-    mockNetworkService.get.mockResolvedValue({ data: FIATCODES });
+    const expectedFiatCodes: FiatCodesExchangeResult = {
+      success: true,
+      symbols: {},
+    };
+    mockNetworkService.get.mockResolvedValue({ data: expectedFiatCodes });
+
     const fiatcodes = await service.getFiatCodes();
-    expect(fiatcodes).toBe(FIATCODES);
+
+    expect(fiatcodes).toBe(expectedFiatCodes);
   });
 
   it('Should return the exchange result', async () => {
-    mockNetworkService.get.mockResolvedValue({ data: EXCHANGERESULT });
+    const expectedExchangeResult = exchangeResultFactory(true, { USD: 2.0 });
+    mockNetworkService.get.mockResolvedValue({ data: expectedExchangeResult });
+
     const exchangeresult = await service.getExchangeResult();
-    expect(exchangeresult).toBe(EXCHANGERESULT);
+
+    expect(exchangeresult).toBe(expectedExchangeResult);
   });
 });
