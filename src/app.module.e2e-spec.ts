5eb61361d03a38ea46795c557edb36c8556aba3b
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-10-21 12:58:20 +0200

    Merge AppModule tests

--- src/app.module.e2e-spec.ts
+++ src/app.module.e2e-spec.ts
@@ -1,24 +1,15 @@
-import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from './app.module';
 
-describe('Application bootstrap e2e tests', () => {
-  let app: INestApplication;
-
-  beforeAll(async () => {
+describe('Application bootstrap', () => {
+  it('should init the app', async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleRef.createNestApplication();
+    const app = moduleRef.createNestApplication();
     await app.init();
-  });
-
-  it('should init the app', () => {
     expect(app).toBeDefined();
-  });
-
-  afterAll(async () => {
     await app.close();
   });
 });
