0ac6686a098896bef201a51b1bd5b1094495b2d7
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-10-21 17:51:46 +0200

    Check cache content

--- src/routes/contracts/__tests__/get-contract.e2e-spec.ts
+++ src/routes/contracts/__tests__/get-contract.e2e-spec.ts
@@ -1,22 +1,10 @@
 import * as request from 'supertest';
-import { INestApplication, Logger } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../../../app.module';
 import { readFileSync } from 'fs';
 import { Contract } from '../entities/contract.entity';
 import { RedisClientType, createClient } from 'redis';
-
-const logger = new Logger(redisClientFactory.name);
-
-async function redisClientFactory(): Promise<RedisClientType> {
-  const { REDIS_HOST = 'localhost', REDIS_PORT = 6379 } = process.env;
-  const client: RedisClientType = createClient({
-    url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
-  });
-  client.on('error', (err) => logger.error('Redis Client Error', err));
-  client.connect();
-  return client;
-}
 
 describe('Get contract e2e test', () => {
   let app: INestApplication;
@@ -29,12 +17,15 @@ describe('Get contract e2e test', () => {
     }).compile();
 
     app = moduleRef.createNestApplication();
-    redisClient = await redisClientFactory();
     await app.init();
+
+    redisClient = await redisClientFactory();
+    await redisClient.flushAll();
   });
 
   it('GET /contracts/<address>', async () => {
     const contractAddress = '0x7cbB62EaA69F79e6873cD1ecB2392971036cFAa4';
+    const contractCacheKey = `${chainId}_${contractAddress}_contract`;
     const expectedResponse: Contract = JSON.parse(
       readFileSync(
         'src/routes/contracts/__tests__/resources/contract-expected-response.json',
@@ -50,10 +41,23 @@ describe('Get contract e2e test', () => {
       .then(({ body }) => {
         expect(body).toEqual(expectedResponse);
       });
+
+    const cacheContent = await redisClient.hGet(contractCacheKey, '');
+    expect(cacheContent).toEqual(JSON.stringify(expectedResponse));
   });
 
   afterAll(async () => {
     await app.close();
+    await redisClient.flushAll();
     await redisClient.quit();
   });
 });
+
+async function redisClientFactory(): Promise<RedisClientType> {
+  const { REDIS_HOST = 'localhost', REDIS_PORT = 6379 } = process.env;
+  const client: RedisClientType = createClient({
+    url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+  });
+  client.connect();
+  return client;
+}
