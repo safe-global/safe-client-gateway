38cd293431b693c7afa1c0e6eeb71e7861e1f8f9
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-11-10 12:13:09 +0100

    Add OpenAPI spec

--- src/routes/health/entities/health.entity.ts
+++ src/routes/health/entities/health.entity.ts
@@ -1,3 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export enum HealthStatus {
+  OK = 'OK',
+  KO = 'KO',
+}
+
 export class Health {
-  status: string;
+  @ApiProperty({ enum: Object.values(HealthStatus) })
+  status: HealthStatus;
 }
--- src/routes/health/health.controller.ts
+++ src/routes/health/health.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
-import { Health } from './entities/health.entity';
+import { Health, HealthStatus } from './entities/health.entity';
 
 @ApiTags('health')
 @Controller({ path: 'health' })
@@ -8,6 +8,6 @@ export class HealthController {
   @ApiOkResponse({ type: Health })
   @Get()
   getHealth(): Health {
-    return { status: 'OK' };
+    return { status: HealthStatus.OK };
   }
 }
