7c48a92ada0ad07ad5d0d15def6aeb4ea741618c
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-11-10 10:29:09 +0100

    Add E2E test

---
+++ src/routes/health/__tests__/get-health.e2e-spec.ts
@@ -0,0 +1,26 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AppModule } from '../../../app.module';
+
+describe('Get health e2e test', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it('GET /health', async () => {
+    await request(app.getHttpServer())
+      .get(`/health`)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toEqual('');
+      });
+  });
+});
