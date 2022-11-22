1bd619b9a9fdc395c8c1c4dc73dddae7c78eea16
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-11-23 00:27:09 +0100

    Start mapping multisig transactions

--- src/routes/transactions/entities/multisig-transaction.entity.ts
+++ src/routes/transactions/entities/multisig-transaction.entity.ts
@@ -1,78 +1,16 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import {
-  Confirmation as DomainConfirmation,
-  MultisigTransaction as DomainMultisigTransaction,
-} from '../../../domain/safe/entities/multisig-transaction.entity';
-import { Operation } from '../../../domain/safe/entities/operation.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class Confirmation implements DomainConfirmation {
-  @ApiProperty()
-  owner: string;
-  @ApiProperty()
-  submissionDate: Date;
-  @ApiPropertyOptional()
-  transactionHash?: string;
-  @ApiProperty()
-  signatureType: string;
-  @ApiPropertyOptional()
-  signature?: string;
+export class TransactionSummary {
+  id: string;
+  timestamp?: number;
+  txStatus: string;
 }
 
-export class MultisigTransaction implements DomainMultisigTransaction {
+export class MultisigTransaction {
   @ApiProperty()
-  safe: string;
+  type: string;
   @ApiProperty()
-  to: string;
-  @ApiPropertyOptional()
-  value?: string;
-  @ApiPropertyOptional()
-  data?: string;
-  @ApiPropertyOptional()
-  dataDecoded?: any;
+  transaction: TransactionSummary;
   @ApiProperty()
-  operation: Operation;
-  @ApiPropertyOptional()
-  gasToken?: string;
-  @ApiPropertyOptional()
-  safeTxGas?: number;
-  @ApiPropertyOptional()
-  baseGas?: number;
-  @ApiPropertyOptional()
-  gasPrice?: string;
-  @ApiPropertyOptional()
-  refundReceiver?: string;
-  @ApiProperty()
-  nonce: number;
-  @ApiPropertyOptional()
-  executionDate?: Date;
-  @ApiPropertyOptional()
-  submissionDate?: Date;
-  @ApiPropertyOptional()
-  modified?: Date;
-  @ApiPropertyOptional()
-  blockNumber?: number;
-  @ApiPropertyOptional()
-  transactionHash?: string;
-  @ApiProperty()
-  safeTxHash: string;
-  @ApiPropertyOptional()
-  executor?: string;
-  @ApiProperty()
-  isExecuted: boolean;
-  @ApiPropertyOptional()
-  isSuccessful?: boolean;
-  @ApiPropertyOptional()
-  ethGasPrice?: string;
-  @ApiPropertyOptional()
-  gasUsed?: number;
-  @ApiPropertyOptional()
-  fee?: string;
-  @ApiPropertyOptional()
-  origin?: string;
-  @ApiPropertyOptional()
-  confirmationsRequired?: number;
-  @ApiPropertyOptional()
-  confirmations?: DomainConfirmation[];
-  @ApiPropertyOptional()
-  signatures?: string;
+  conflictType: string;
 }
--- src/routes/transactions/transactions.service.ts
+++ src/routes/transactions/transactions.service.ts
@@ -1,8 +1,13 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { MultisigTransaction as DomainMultisigTransaction } from '../../domain/safe/entities/multisig-transaction.entity';
+import { Safe } from '../../domain/safe/entities/safe.entity';
 import { SafeRepository } from '../../domain/safe/safe.repository';
 import { ISafeRepository } from '../../domain/safe/safe.repository.interface';
 import { Page } from '../common/entities/page.entity';
-import { MultisigTransaction } from './entities/multisig-transaction.entity';
+import {
+  MultisigTransaction,
+  TransactionSummary,
+} from './entities/multisig-transaction.entity';
 
 @Injectable()
 export class TransactionsService {
@@ -22,17 +27,78 @@ export class TransactionsService {
     limit?: number,
     offset?: number,
   ): Promise<Page<MultisigTransaction>> {
-    return this.safeRepository.getMultiSigTransactions(
-      chainId,
-      safeAddress,
-      executionDateGte,
-      executionDateLte,
-      to,
-      value,
-      nonce,
-      executed,
-      limit,
-      offset,
-    );
+    const multisigTransactions =
+      await this.safeRepository.getMultiSigTransactions(
+        chainId,
+        safeAddress,
+        executionDateGte,
+        executionDateLte,
+        to,
+        value,
+        nonce,
+        executed,
+        limit,
+        offset,
+      );
+
+    const safeInfo = await this.safeRepository.getSafe(chainId, safeAddress);
+
+    return {
+      ...multisigTransactions,
+      results: multisigTransactions.results.map((multiSignTransaction) => ({
+        type: 'TRANSACTION',
+        transaction: this.getSummary(multiSignTransaction, safeInfo),
+        conflictType: 'None',
+      })),
+    };
+  }
+
+  private getSummary(
+    multiSignTransaction: DomainMultisigTransaction,
+    safeInfo: Safe,
+  ): TransactionSummary {
+    return {
+      id: `multisig_${multiSignTransaction.safe}_${multiSignTransaction.safeTxHash}`,
+      timestamp:
+        multiSignTransaction?.executionDate?.getTime() ??
+        multiSignTransaction?.submissionDate?.getTime(),
+      txStatus: this.getTxStatus(multiSignTransaction, safeInfo),
+    };
+  }
+
+  private getTxStatus(
+    multiSignTransaction: DomainMultisigTransaction,
+    safeInfo: Safe,
+  ): string {
+    if (multiSignTransaction.isExecuted) {
+      if (multiSignTransaction.isSuccessful) {
+        return 'SUCCESS';
+      } else {
+        return 'FAILED';
+      }
+    }
+    if (safeInfo.nonce > multiSignTransaction.nonce) {
+      return 'CANCELLED';
+    }
+    if (
+      this.getConfirmationsCount(multiSignTransaction) <
+      this.getConfirmationsRequired(multiSignTransaction, safeInfo)
+    ) {
+      return 'AWAITING_CONFIRMATIONS';
+    }
+    return 'AWAITING_EXECUTION';
+  }
+
+  private getConfirmationsCount(
+    multiSignTransaction: DomainMultisigTransaction,
+  ): number {
+    return multiSignTransaction.confirmations?.length ?? 0; // TODO:
+  }
+
+  private getConfirmationsRequired(
+    multiSignTransaction: DomainMultisigTransaction,
+    safeInfo: Safe,
+  ): number {
+    return multiSignTransaction.confirmationsRequired ?? safeInfo.threshold; // TODO:
   }
 }
