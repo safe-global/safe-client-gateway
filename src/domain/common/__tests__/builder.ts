57d99b45b519c9ac3f89e2684749af0783766d9b
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2023-01-03 17:22:04 +0100

    Add toJson function to Builder
    - Adds a `toJson` function to `Builder`
    - This function should be used whenever in a testing scenario a JSON payload is required for `T`
    - The result of `build()` can, in some cases, be the same result as `toJson`. However, `toJson` can be used to convert types that are serialised to `Date` back to a `string`

--- src/domain/common/__tests__/builder.ts
+++ src/domain/common/__tests__/builder.ts
@@ -1,3 +1,12 @@
 export interface Builder<T> {
+  /**
+   * Returns the concrete class T using the internal Builder state
+   */
   build(): T;
+
+  /**
+   * Returns the JSON-like implementation for the concrete class T using the
+   * internal Builder state
+   */
+  toJson(): unknown;
 }
