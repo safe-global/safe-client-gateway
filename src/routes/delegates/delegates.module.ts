04628c01ebcec0eb0cd6129196bfb51ffdfc99c3
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2023-02-16 17:35:14 +0100

    Add ValidationModule

--- src/routes/delegates/delegates.module.ts
+++ src/routes/delegates/delegates.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
-import { GenericValidator } from '../../domain/schema/generic.validator';
-import { JsonSchemaService } from '../../domain/schema/json-schema.service';
-import { ValidationErrorFactory } from '../../domain/schema/validation-error-factory';
+import { ValidationModule } from '../../validation.module';
 import { DelegatesController } from './delegates.controller';
 import { DelegatesService } from './delegates.service';
 
 @Module({
   controllers: [DelegatesController],
-  providers: [
-    JsonSchemaService,
-    GenericValidator,
-    ValidationErrorFactory,
-    DelegatesService,
-  ],
+  providers: [DelegatesService],
+  imports: [ValidationModule],
 })
 export class DelegatesModule {}
