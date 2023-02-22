1ddeeaecf40f2b46d16314890d294c129580cd8f
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2023-02-22 11:25:27 +0100

    Move tests to controller layer

--- src/routes/messages/messages.controller.spec.ts
+++ src/routes/messages/messages.controller.spec.ts
@@ -304,5 +304,123 @@ describe('Messages controller', () => {
           preparedSignature: null,
         });
     });
+
+    it('should return null name and logo if the Safe App is not found', async () => {
+      const chain = chainBuilder().build();
+      const messageConfirmations = range(random(2, 5)).map(() =>
+        messageConfirmationBuilder().build(),
+      );
+      const message = messageBuilder()
+        .with('safeAppId', faker.datatype.number())
+        .with('confirmations', messageConfirmations)
+        .build();
+      const safe = safeBuilder()
+        .with(
+          'threshold',
+          faker.datatype.number({ min: messageConfirmations.length + 1 }),
+        )
+        .build();
+      mockNetworkService.get.mockImplementation((url) => {
+        switch (url) {
+          case `${safeConfigUrl}/api/v1/chains/${chain.chainId}`:
+            return Promise.resolve({ data: chain });
+          case `${chain.transactionService}/api/v1/messages/${message.messageHash}`:
+            return Promise.resolve({ data: messageToJson(message) });
+          case `${chain.transactionService}/api/v1/safes/${message.safe}`:
+            return Promise.resolve({ data: safe });
+          case `${safeConfigUrl}/api/v1/safe-apps/`:
+            return Promise.resolve({ data: [] });
+          default:
+            return Promise.reject(`No matching rule for url: ${url}`);
+        }
+      });
+
+      await request(app.getHttpServer())
+        .get(`/v1/chains/${chain.chainId}/messages/${message.messageHash}`)
+        .expect(200)
+        .expect({
+          messageHash: message.messageHash,
+          status: MessageStatus.NeedsConfirmation,
+          logoUri: null,
+          name: null,
+          message: message.message,
+          creationTimestamp: message.created.getTime(),
+          modifiedTimestamp: message.modified.getTime(),
+          confirmationsSubmitted: messageConfirmations.length,
+          confirmationsRequired: safe.threshold,
+          proposedBy: {
+            value: message.proposedBy,
+            name: null,
+            logoUri: null,
+          },
+          confirmations: messageConfirmations.map((confirmation) => ({
+            owner: {
+              value: confirmation.owner,
+              name: null,
+              logoUri: null,
+            },
+            signature: confirmation.signature,
+          })),
+          preparedSignature: null,
+        });
+    });
+
+    it('should return null name and logo if no safeAppId in the message', async () => {
+      const chain = chainBuilder().build();
+      const messageConfirmations = range(random(2, 5)).map(() =>
+        messageConfirmationBuilder().build(),
+      );
+      const message = messageBuilder()
+        .with('safeAppId', null)
+        .with('confirmations', messageConfirmations)
+        .build();
+      const safe = safeBuilder()
+        .with(
+          'threshold',
+          faker.datatype.number({ min: messageConfirmations.length + 1 }),
+        )
+        .build();
+      mockNetworkService.get.mockImplementation((url) => {
+        switch (url) {
+          case `${safeConfigUrl}/api/v1/chains/${chain.chainId}`:
+            return Promise.resolve({ data: chain });
+          case `${chain.transactionService}/api/v1/messages/${message.messageHash}`:
+            return Promise.resolve({ data: messageToJson(message) });
+          case `${chain.transactionService}/api/v1/safes/${message.safe}`:
+            return Promise.resolve({ data: safe });
+          default:
+            return Promise.reject(`No matching rule for url: ${url}`);
+        }
+      });
+
+      await request(app.getHttpServer())
+        .get(`/v1/chains/${chain.chainId}/messages/${message.messageHash}`)
+        .expect(200)
+        .expect({
+          messageHash: message.messageHash,
+          status: MessageStatus.NeedsConfirmation,
+          logoUri: null,
+          name: null,
+          message: message.message,
+          creationTimestamp: message.created.getTime(),
+          modifiedTimestamp: message.modified.getTime(),
+          confirmationsSubmitted: messageConfirmations.length,
+          confirmationsRequired: safe.threshold,
+          proposedBy: {
+            value: message.proposedBy,
+            name: null,
+            logoUri: null,
+          },
+          confirmations: messageConfirmations.map((confirmation) => ({
+            owner: {
+              value: confirmation.owner,
+              name: null,
+              logoUri: null,
+            },
+            signature: confirmation.signature,
+          })),
+          preparedSignature: null,
+        });
+    });
   });
 });
