41d9065b97cca8675af6a19e6c7bbbe183d623ac
Héctor Gómez <hector.gomez.varela@gmail.com>
2023-03-03 19:24:07 +0100

    Fix bad schema reference (#286)
    This PR fixed a bad reference in `getEstimationDtoSchema`

--- src/routes/estimations/entities/schemas/get-estimation.dto.schema.ts
+++ src/routes/estimations/entities/schemas/get-estimation.dto.schema.ts
@@ -2,7 +2,7 @@ import { JSONSchemaType } from 'ajv';
 import { GetEstimationDto } from '../get-estimation.dto.entity';
 
 export const getEstimationDtoSchema: JSONSchemaType<GetEstimationDto> = {
-  $id: 'https://safe-client.safe.global/schemas/delegates/get-estimation.dto.json',
+  $id: 'https://safe-client.safe.global/schemas/estimations/get-estimation.dto.json',
   type: 'object',
   properties: {
     to: { type: 'string' },
