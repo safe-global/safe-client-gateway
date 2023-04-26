fc078e9a78f10454e18563a87ae6e5c0edd41c4a
Héctor Gómez <hector.gomez.varela@gmail.com>
2023-04-26 16:27:42 +0200

    Cache 404 errors (#295)
    **Motivation**
    This PR tries to avoid the following scenario:
    
    ```
    debug: [Cache] Cache miss: 5_0x710085cB90e4Ce09F80E354Cf579a32C9790A140_contract
    debug: [Cache] Cache miss: 5_0x710085cB90e4Ce09F80E354Cf579a32C9790A140_contract
    debug: [Cache] Cache miss: 5_0x40A2aCCbd92BCA938b02010E17A5b8929b49130D_contract
    debug: [Cache] Cache miss: 5_0xf10E2042ec19747401E5EA174EfB63A0058265E6_contract
    debug: [Cache] Cache miss: 5_0x7f3774E7D0e410444b6f19a10D50Cd34857Bb236_contract
    debug: [Cache] Cache miss: 5_0x7f3774E7D0e410444b6f19a10D50Cd34857Bb236_contract
    debug: [Cache] Cache miss: 5_0x7f3774E7D0e410444b6f19a10D50Cd34857Bb236_contract
    ...
    error: An error occurred {"code":404}
    error: An error occurred {"code":404}
    error: An error occurred {"code":404}
    error: An error occurred {"code":404}
    error: An error occurred {"code":404}
    error: An error occurred {"code":404}
    error: An error occurred {"code":404}
    ...
    ```
    
    i.e: Avoid making successive calls to the Transaction Service asking for
    the same item.
    
    This is common in the current flow since a list of transactions could
    contain references to the same contract for each transaction. The
    current behavior is to repeat the call to the Transaction Service N
    times for N contracts, whether the contract is found or it doesn't.
    
    So this PR:
    - Modifies the `get` function of the `CacheFirstDataSource` class to
    cache 404 errors. The errors are cached with a fixed TTL of 30 seconds
    in this first approach. Optionally in the future, we could want to
    externalize this configuration and place this TTL value behind an
    environment variable.

--- src/datasources/cache/cache.first.data.source.ts
+++ src/datasources/cache/cache.first.data.source.ts
@@ -7,6 +7,8 @@ import {
 import { Inject, Injectable } from '@nestjs/common';
 import * as winston from 'winston';
 import { CacheDir } from './entities/cache-dir.entity';
+import { NetworkResponseError } from '../network/entities/network.error.entity';
+import { get } from 'lodash';
 
 /**
  * A data source which tries to retrieve values from cache using
@@ -19,6 +21,8 @@ import { CacheDir } from './entities/cache-dir.entity';
  */
 @Injectable()
 export class CacheFirstDataSource {
+  private readonly ERROR_TTL_SECONDS = 30;
+
   constructor(
     @Inject(CacheService) private readonly cacheService: ICacheService,
     @Inject(NetworkService) private readonly networkService: INetworkService,
@@ -28,13 +32,12 @@ export class CacheFirstDataSource {
    * Gets the cached value behind the {@link CacheDir}.
    * If the value is not present, it tries to get the respective JSON
    * payload from {@link url}.
-   * Errors are not cached.
+   * 404 errors are cached with {@link ERROR_TTL_SECONDS} seconds expiration time.
    *
    * @param cacheDir - {@link CacheDir} containing the key and field to be used to retrieve from cache
    * @param url - the HTTP endpoint to retrieve the JSON payload
    * @param params - the parameters to be used for the HTTP request
-   * @param expireTimeSeconds - the time to live in seconds for the payload
-   * behind {@link key}
+   * @param expireTimeSeconds - the time to live in seconds for the payload behind {@link CacheDir}
    */
   async get<T>(
     cacheDir: CacheDir,
@@ -45,16 +48,38 @@ export class CacheFirstDataSource {
     const cached = await this.cacheService.get(cacheDir);
     if (cached != null) {
       winston.debug(`[Cache] Cache hit: [${cacheDir.key}, ${cacheDir.field}]`);
-      return JSON.parse(cached);
+      const cachedData = JSON.parse(cached);
+      if (get(cachedData, 'status') === 404) {
+        throw new NetworkResponseError(cachedData.status, cachedData.data);
+      }
+      return cachedData;
     }
-    winston.debug(`[Cache] Cache miss: [${cacheDir.key}, ${cacheDir.field}]`);
-    const { data } = await this.networkService.get(url, params);
-    const rawJson = JSON.stringify(data);
-    await this.cacheService.set(
-      new CacheDir(cacheDir.key, cacheDir.field),
-      rawJson,
-      expireTimeSeconds,
-    );
-    return data;
+    try {
+      winston.debug(`[Cache] Cache miss: [${cacheDir.key}, ${cacheDir.field}]`);
+      const { data } = await this.networkService.get(url, params);
+      const rawJson = JSON.stringify(data);
+      await this.cacheService.set(cacheDir, rawJson, expireTimeSeconds);
+      return data;
+    } catch (error) {
+      if (get(error, 'status') === 404) {
+        await this.cacheNotFoundError(
+          cacheDir,
+          new NetworkResponseError(error.status, error),
+        );
+      }
+      throw error;
+    }
+  }
+
+  /**
+   * Caches an error.
+   * @param cacheDir - {@link CacheDir} where the error should be placed
+   */
+  private async cacheNotFoundError(
+    cacheDir: CacheDir,
+    error: NetworkResponseError,
+  ): Promise<void> {
+    const value = JSON.stringify({ status: error.status, data: error });
+    return this.cacheService.set(cacheDir, value, this.ERROR_TTL_SECONDS);
   }
 }
