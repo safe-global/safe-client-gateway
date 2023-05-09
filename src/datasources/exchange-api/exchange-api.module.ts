5bd842d48e378cb64d7c10a42a5b1bb89ac79071
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2023-05-09 13:07:54 +0200

    Use CacheFirstDataSource in ExchangeApi
    - `ExchangeApi` now uses `CacheFirstDataSource` in order to avoid calling external fiat rate exchange endpoints more often than required

--- src/datasources/exchange-api/exchange-api.module.ts
+++ src/datasources/exchange-api/exchange-api.module.ts
@@ -2,13 +2,15 @@ import { Global, Module } from '@nestjs/common';
 import { HttpErrorFactory } from '../errors/http-error-factory';
 import { ExchangeApi } from './exchange-api.service';
 import { IExchangeApi } from '../../domain/interfaces/exchange-api.interface';
+import { CacheFirstDataSourceModule } from '../cache/cache.first.data.source.module';
 
 @Global()
 @Module({
+  exports: [IExchangeApi],
+  imports: [CacheFirstDataSourceModule],
   providers: [
     HttpErrorFactory,
     { provide: IExchangeApi, useClass: ExchangeApi },
   ],
-  exports: [IExchangeApi],
 })
 export class ExchangeApiModule {}
