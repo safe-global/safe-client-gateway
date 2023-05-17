fe71eee78e020cc800833be99ac21a2c2fa81d31
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2023-05-17 14:40:47 +0200

    GetDataDecodedDto 'to' field is now optional
    - Sets `GetDataDecodedDto.to` as an optional field
    - Improves description on the `to` field for Swagger
    - Removes type predicate validation in `GetDataDecodedDtoValidationPipe` – this should be accomplished by AJV and respective schema validation

--- src/domain/interfaces/transaction-api.interface.ts
+++ src/domain/interfaces/transaction-api.interface.ts
@@ -29,7 +29,7 @@ export interface ITransactionApi {
 
   clearLocalBalances(safeAddress: string): Promise<void>;
 
-  getDataDecoded(data: string, to: string): Promise<DataDecoded>;
+  getDataDecoded(data: string, to?: string): Promise<DataDecoded>;
 
   getCollectibles(
     safeAddress: string,
