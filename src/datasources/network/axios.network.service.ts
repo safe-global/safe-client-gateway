44c15ef2df1e5f754230c608b8750e7284f6b92e
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2023-05-24 14:37:09 +0200

    Update external_request logs (#358)
    To improve compatibility with Datadog defaults, some properties of the
    `external_request` log changed. More specifically:
    
    - `host` – is used as the originating host of the log by Datadog. We
    want to represent to which host was the external request made to
    instead. Changed to `target_host`.
    - `status` – is used to set the level/severity of the log. We want to
    represent the HTTP status code of the external request made instead.
    Change to `request_status`
    - `message` – its contents are displayed in views like Live Tail.
    Changed to `detail` as `message` is already set as the JSON payload.
    
    See
    https://docs.datadoghq.com/logs/log_configuration/attributes_naming_convention/#reserved-attributes

--- src/datasources/network/axios.network.service.ts
+++ src/datasources/network/axios.network.service.ts
@@ -79,10 +79,10 @@ export class AxiosNetworkService implements INetworkService {
     this.loggingService.debug({
       type: 'external_request',
       protocol: error.request.protocol,
-      host: error.request.host,
+      target_host: error.request.host,
       path: error.request.path,
-      status: error.response.status,
-      message: error.response.statusText,
+      request_status: error.response.status,
+      detail: error.response.statusText,
       response_time_ms: responseTimeMs,
     });
   }
