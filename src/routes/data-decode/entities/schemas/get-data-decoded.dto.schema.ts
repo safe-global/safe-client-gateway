9e4e6f9a0cb014ed35d818398c7365fbb3af5fd9
iamacook <aaron@safe.global>
2023-05-25 16:24:41 +0200

    feat: validate `GetDataDecodedDto` by pattern

--- src/routes/data-decode/entities/schemas/get-data-decoded.dto.schema.ts
+++ src/routes/data-decode/entities/schemas/get-data-decoded.dto.schema.ts
@@ -1,12 +1,13 @@
 import { JSONSchemaType } from 'ajv';
 import { GetDataDecodedDto } from '../get-data-decoded.dto.entity';
+import { HEX_PATTERN } from '../../../../validation/patterns';
 
 export const getDataDecodedDtoSchema: JSONSchemaType<GetDataDecodedDto> = {
   $id: 'https://safe-client.safe.global/schemas/delegates/get-data-decoded.dto.json',
   type: 'object',
   properties: {
-    data: { type: 'string' },
-    to: { type: 'string', nullable: true },
+    data: { type: 'string', pattern: HEX_PATTERN },
+    to: { type: 'string', pattern: HEX_PATTERN, nullable: true },
   },
   required: ['data'],
 };
---
+++ src/validation/patterns.ts
@@ -0,0 +1,1 @@
+export const HEX_PATTERN = '0[x][0-9a-fA-F]+';
