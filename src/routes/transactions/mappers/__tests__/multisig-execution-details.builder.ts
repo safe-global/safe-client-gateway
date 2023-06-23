c8736bcbe34317051bb479e774300b213d228a82
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2023-06-23 16:48:24 +0200

    Add MultisigTransactionDetailsMapper tests

---
+++ src/routes/transactions/mappers/__tests__/multisig-execution-details.builder.ts
@@ -0,0 +1,41 @@
+import { faker } from '@faker-js/faker';
+import { random, range, sampleSize } from 'lodash';
+import { Builder, IBuilder } from '../../../../__tests__/builder';
+import { tokenBuilder } from '../../../../domain/tokens/__tests__/token.builder';
+import { addressInfoBuilder } from '../../../common/__tests__/entities/address-info.builder';
+import {
+  MultisigConfirmationDetails,
+  MultisigExecutionDetails,
+} from '../../entities/transaction-details/multisig-execution-details.entity';
+
+function multisigConfirmationDetailsBuilder(): IBuilder<MultisigConfirmationDetails> {
+  return Builder.new<MultisigConfirmationDetails>()
+    .with('signer', addressInfoBuilder().build())
+    .with('signature', faker.string.sample())
+    .with('submittedAt', faker.number.int());
+}
+
+export function multisigExecutionDetailsBuilder(): IBuilder<MultisigExecutionDetails> {
+  const signers = range(random(2, 5)).map(() => addressInfoBuilder().build());
+  const confirmations = range(random(2, 5)).map(() =>
+    multisigConfirmationDetailsBuilder().build(),
+  );
+
+  return Builder.new<MultisigExecutionDetails>()
+    .with('type', 'MULTISIG')
+    .with('submittedAt', faker.number.int())
+    .with('nonce', faker.number.int())
+    .with('safeTxGas', faker.string.numeric())
+    .with('baseGas', faker.string.numeric())
+    .with('gasPrice', faker.string.numeric())
+    .with('gasToken', faker.finance.ethereumAddress())
+    .with('refundReceiver', addressInfoBuilder().build())
+    .with('safeTxHash', faker.string.sample())
+    .with('executor', addressInfoBuilder().build())
+    .with('signers', signers)
+    .with('confirmationsRequired', faker.number.int())
+    .with('confirmations', confirmations)
+    .with('rejectors', sampleSize(signers, 2))
+    .with('gasTokenInfo', tokenBuilder().build())
+    .with('trusted', faker.datatype.boolean());
+}
--- src/routes/transactions/mappers/multisig-transactions/multisig-transaction-details.mapper.spec.ts
+++ src/routes/transactions/mappers/multisig-transactions/multisig-transaction-details.mapper.spec.ts
@@ -2,10 +2,12 @@ import { faker } from '@faker-js/faker';
 import { sample } from 'lodash';
 import { multisigTransactionBuilder } from '../../../../domain/safe/entities/__tests__/multisig-transaction.builder';
 import { safeBuilder } from '../../../../domain/safe/entities/__tests__/safe.builder';
+import { addressInfoBuilder } from '../../../common/__tests__/entities/address-info.builder';
 import { AddressInfoHelper } from '../../../common/address-info/address-info.helper';
 import { safeAppInfoBuilder } from '../../entities/__tests__/safe-app-info.builder';
 import { transferTransactionInfoBuilder } from '../../entities/__tests__/transfer-transaction-info.builder';
 import { TransactionStatus } from '../../entities/transaction-status.entity';
+import { multisigExecutionDetailsBuilder } from '../__tests__/multisig-execution-details.builder';
 import { SafeAppInfoMapper } from '../common/safe-app-info.mapper';
 import { TransactionDataMapper } from '../common/transaction-data.mapper';
 import { MultisigTransactionInfoMapper } from '../common/transaction-info.mapper';
@@ -34,11 +36,11 @@ const safeAppInfoMapper = jest.mocked({
   mapSafeAppInfo: jest.fn(),
 } as unknown as SafeAppInfoMapper);
 
-const multisigTransactionExecutionDetailsMapper = jest.mocked({
+const multisigExecutionDetailsMapper = jest.mocked({
   mapMultisigExecutionDetails: jest.fn(),
 } as unknown as MultisigTransactionExecutionDetailsMapper);
 
-describe.skip('MultisigTransactionDetails mapper (Unit)', () => {
+describe('MultisigTransactionDetails mapper (Unit)', () => {
   let mapper: MultisigTransactionDetailsMapper;
 
   beforeEach(() => {
@@ -49,11 +51,11 @@ describe.skip('MultisigTransactionDetails mapper (Unit)', () => {
       transactionInfoMapper,
       transactionDataMapper,
       safeAppInfoMapper,
-      multisigTransactionExecutionDetailsMapper,
+      multisigExecutionDetailsMapper,
     );
   });
 
-  it('should return a TransactionDetails object', async () => {
+  it('should return a TransactionDetails object with null addressInfoIndex', async () => {
     const chainId = faker.string.numeric();
     const transaction = multisigTransactionBuilder().build();
     const safe = safeBuilder().build();
@@ -64,6 +66,14 @@ describe.skip('MultisigTransactionDetails mapper (Unit)', () => {
     transactionInfoMapper.mapTransactionInfo.mockResolvedValue(txInfo);
     const safeAppInfo = safeAppInfoBuilder().build();
     safeAppInfoMapper.mapSafeAppInfo.mockResolvedValue(safeAppInfo);
+    const multisigExecutionDetails = multisigExecutionDetailsBuilder().build();
+    multisigExecutionDetailsMapper.mapMultisigExecutionDetails.mockResolvedValue(
+      multisigExecutionDetails,
+    );
+    transactionDataMapper.isTrustedDelegateCall.mockResolvedValue(true);
+    transactionDataMapper.buildAddressInfoIndex.mockResolvedValue({});
+    const to = addressInfoBuilder().build();
+    addressInfoHelper.getOrDefault.mockResolvedValue(to);
 
     const actual = await mapper.mapDetails(chainId, transaction, safe);
 
@@ -73,9 +83,66 @@ describe.skip('MultisigTransactionDetails mapper (Unit)', () => {
       executedAt: transaction.executionDate?.getTime(),
       txStatus,
       txInfo,
-      // txData,
+      txData: expect.objectContaining({
+        hexData: transaction.data,
+        dataDecoded: transaction.dataDecoded,
+        to,
+        value: transaction.value,
+        operation: transaction.operation,
+        trustedDelegateCallTarget: true,
+        addressInfoIndex: null,
+      }),
       txHash: transaction.transactionHash,
-      // detailedExecutionInfo,
+      detailedExecutionInfo: multisigExecutionDetails,
+      safeAppInfo,
+    });
+  });
+
+  it('should return a TransactionDetails object with non-null addressInfoIndex', async () => {
+    const chainId = faker.string.numeric();
+    const transaction = multisigTransactionBuilder().build();
+    const safe = safeBuilder().build();
+    const txStatus =
+      sample(Object.values(TransactionStatus)) ?? TransactionStatus.Success;
+    statusMapper.mapTransactionStatus.mockReturnValue(txStatus);
+    const txInfo = transferTransactionInfoBuilder().build();
+    transactionInfoMapper.mapTransactionInfo.mockResolvedValue(txInfo);
+    const safeAppInfo = safeAppInfoBuilder().build();
+    safeAppInfoMapper.mapSafeAppInfo.mockResolvedValue(safeAppInfo);
+    const multisigExecutionDetails = multisigExecutionDetailsBuilder().build();
+    multisigExecutionDetailsMapper.mapMultisigExecutionDetails.mockResolvedValue(
+      multisigExecutionDetails,
+    );
+    transactionDataMapper.isTrustedDelegateCall.mockResolvedValue(true);
+    const addressInfoIndex = {
+      [faker.string.sample()]: addressInfoBuilder().build(),
+      [faker.string.sample()]: addressInfoBuilder().build(),
+    };
+    transactionDataMapper.buildAddressInfoIndex.mockResolvedValue(
+      addressInfoIndex,
+    );
+    const to = addressInfoBuilder().build();
+    addressInfoHelper.getOrDefault.mockResolvedValue(to);
+
+    const actual = await mapper.mapDetails(chainId, transaction, safe);
+
+    expect(actual).toEqual({
+      safeAddress: safe.address,
+      txId: `multisig_${safe.address}_${transaction.safeTxHash}`,
+      executedAt: transaction.executionDate?.getTime(),
+      txStatus,
+      txInfo,
+      txData: expect.objectContaining({
+        hexData: transaction.data,
+        dataDecoded: transaction.dataDecoded,
+        to,
+        value: transaction.value,
+        operation: transaction.operation,
+        trustedDelegateCallTarget: true,
+        addressInfoIndex,
+      }),
+      txHash: transaction.transactionHash,
+      detailedExecutionInfo: multisigExecutionDetails,
       safeAppInfo,
     });
   });
