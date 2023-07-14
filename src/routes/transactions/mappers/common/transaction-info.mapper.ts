391f0beb2767b9c67c585f7401910a0f848035c5
iamacook <aaron@safe.global>
2023-07-14 16:28:46 +0200

    fix: keep check for custom transaction

--- src/routes/transactions/mappers/common/transaction-info.mapper.ts
+++ src/routes/transactions/mappers/common/transaction-info.mapper.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ModuleTransaction } from '../../../../domain/safe/entities/module-transaction.entity';
 import { MultisigTransaction } from '../../../../domain/safe/entities/multisig-transaction.entity';
+import { Operation } from '../../../../domain/safe/entities/operation.entity';
 import { TokenRepository } from '../../../../domain/tokens/token.repository';
 import { ITokenRepository } from '../../../../domain/tokens/token.repository.interface';
 import { TokenType } from '../../../balances/entities/token-type.entity';
@@ -53,6 +54,14 @@ export class MultisigTransactionInfoMapper {
 
     const dataSize =
       dataByteLength >= 2 ? Math.floor((dataByteLength - 2) / 2) : 0;
+
+    if (this.isCustomTransaction(value, dataSize, transaction.operation)) {
+      return await this.customTransactionMapper.mapCustomTransaction(
+        transaction,
+        dataSize,
+        chainId,
+      );
+    }
 
     if (this.isNativeCoinTransfer(value, dataSize)) {
       return this.nativeCoinTransferMapper.mapNativeCoinTransfer(
@@ -116,6 +125,14 @@ export class MultisigTransactionInfoMapper {
       dataSize,
       chainId,
     );
+  }
+
+  private isCustomTransaction(
+    value: number,
+    dataSize: number,
+    operation: Operation,
+  ): boolean {
+    return (value > 0 && dataSize > 0) || operation !== 0;
   }
 
   private isNativeCoinTransfer(value: number, dataSize: number): boolean {
