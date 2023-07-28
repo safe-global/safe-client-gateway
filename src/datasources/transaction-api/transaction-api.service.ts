fbd071ea613d7b2e9eec3cfc2f3dd9a391fa053f
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2023-07-28 12:31:42 +0200

    Forward origin field to Safe Transaction Service
    - The `origin` field should be forwarded to the Safe Transaction Service.

--- src/datasources/transaction-api/transaction-api.service.ts
+++ src/datasources/transaction-api/transaction-api.service.ts
@@ -883,6 +883,7 @@ export class TransactionApi implements ITransactionApi {
         contractTransactionHash: proposeTransactionDto.safeTxHash,
         sender: proposeTransactionDto.sender,
         signature: proposeTransactionDto.signature,
+        origin: proposeTransactionDto.origin,
       });
     } catch (error) {
       throw this.httpErrorFactory.from(error);
