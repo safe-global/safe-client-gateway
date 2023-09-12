532f5ad1ede95d7d1e9ce70f2334db02ec293e1e
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2023-09-12 15:59:57 +0200

    Remove @openzeppelin/contracts dependency

--- src/domain/relay/contracts/erc20-contract.helper.ts
+++ src/domain/relay/contracts/erc20-contract.helper.ts
@@ -1,13 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { Hex } from 'viem/src/types/misc';
-import * as erc20Abi from '@openzeppelin/contracts/build/contracts/ERC20.json';
 import { ContractFunctionArgsMapper } from '@/domain/relay/entities/contract-function.entity';
 import { Abi, isHex } from 'viem';
 import { ContractHelper } from '@/domain/relay/contracts/contract.helper';
 
+// Consider adding @openzeppelin/contracts if we need to support more ERC-20 token functions
+const TRANSFER_ABI: readonly unknown[] | Abi = [
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'transfer',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
+
 @Injectable()
 export class Erc20ContractHelper extends ContractHelper {
-  protected readonly abi: readonly unknown[] | Abi = erc20Abi.abi;
+  protected readonly abi: readonly unknown[] | Abi = TRANSFER_ABI;
 
   static TRANSFER: ContractFunctionArgsMapper<{ to: Hex }> = {
     name: 'transfer',
