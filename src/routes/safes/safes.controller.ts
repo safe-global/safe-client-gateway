67c475a5c16792635bbd8ebfe60299b73cdb27ab
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2023-10-26 15:54:14 +0200

    Add recommended nonce endpoint

--- src/routes/safes/safes.controller.ts
+++ src/routes/safes/safes.controller.ts
@@ -2,6 +2,7 @@ import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { Controller, Get, Param } from '@nestjs/common';
 import { SafeState } from '@/routes/safes/entities/safe-info.entity';
 import { SafesService } from '@/routes/safes/safes.service';
+import { RecommendedNonce } from '@/routes/safes/entities/recommended-nonce.entity';
 
 @ApiTags('safes')
 @Controller({
@@ -17,5 +18,14 @@ export class SafesController {
     @Param('safeAddress') safeAddress: string,
   ): Promise<SafeState> {
     return this.service.getSafeInfo({ chainId, safeAddress });
+  }
+
+  @ApiOkResponse({ type: RecommendedNonce })
+  @Get('chains/:chainId/safes/:safeAddress/recommended-nonce')
+  async getRecommendedNonce(
+    @Param('chainId') chainId: string,
+    @Param('safeAddress') safeAddress: string,
+  ): Promise<RecommendedNonce> {
+    return this.service.getRecommendedNonce({ chainId, safeAddress });
   }
 }
