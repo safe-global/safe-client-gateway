a57a546ec1e5824d6de4f58f09b739af118f2211
iamacook <aaron@safe.global>
2023-10-31 15:06:13 +0100

    chore: dummy `ALERTS_PROVIDER_SIGNING_KEY` for E2E

--- test/e2e-setup.ts
+++ test/e2e-setup.ts
@@ -1,3 +1,4 @@
 process.env.SAFE_CONFIG_BASE_URI = 'https://safe-config.staging.5afe.dev';
 process.env.EXPIRATION_TIME_DEFAULT_SECONDS = `${60}`; // long enough timeout for cache state assertions
 process.env.FF_HUMAN_DESCRIPTION = 'true';
+process.env.ALERTS_PROVIDER_SIGNING_KEY = 'fake-signing-key';
