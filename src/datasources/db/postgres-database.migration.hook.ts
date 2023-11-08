7fbe2cb56845e474800d93d9952e58c4ebf11e23
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2023-11-08 12:31:40 +0100

    Add pg_advisory_lock

--- src/datasources/db/postgres-database.migration.hook.ts
+++ src/datasources/db/postgres-database.migration.hook.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
-import postgres from 'postgres';
 import { ILoggingService, LoggingService } from '@/logging/logging.interface';
 import * as shift from 'postgres-shift';
+import postgres from 'postgres';
 
 /**
  * The {@link PostgresDatabaseMigrationHook} is a Module Init hook meaning
@@ -12,14 +12,38 @@ import * as shift from 'postgres-shift';
  */
 @Injectable({})
 export class PostgresDatabaseMigrationHook implements OnModuleInit {
+  private static LOCK_MAGIC_NUMBER = 132;
+
   constructor(
-    @Inject('DB_INSTANCE') private readonly db: postgres.Sql,
+    @Inject('DB_INSTANCE') private readonly sql: postgres.Sql,
     @Inject(LoggingService) private readonly loggingService: ILoggingService,
   ) {}
 
   async onModuleInit() {
     this.loggingService.info('Checking migrations');
-    await shift({ sql: this.db });
-    this.loggingService.info('Pending migrations executed');
+    try {
+      const [lockAcquired] = await this
+        .sql`SELECT pg_try_advisory_lock(${PostgresDatabaseMigrationHook.LOCK_MAGIC_NUMBER})`;
+      if (!lockAcquired) {
+        this.loggingService.debug(
+          'Lock was already acquired by different session',
+        );
+        // If the lock was not acquired by this session, we exit since the migration is being
+        // performed by a different instance.
+        return;
+      }
+      // Perform migration
+      await shift({ sql: this.sql });
+      this.loggingService.info('Pending migrations executed');
+    } catch (e) {
+      // If there's an error performing a migration, we should throw the error
+      // and prevent the service from starting
+      this.loggingService.error(e);
+      throw e;
+    } finally {
+      // the lock should be released if the migration completed (successfully or not)
+      await this
+        .sql`SELECT pg_advisory_unlock(${PostgresDatabaseMigrationHook.LOCK_MAGIC_NUMBER})`;
+    }
   }
 }
