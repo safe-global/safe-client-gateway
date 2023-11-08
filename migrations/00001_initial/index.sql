a13284bd83f97991c9d55157c139ad5bc109fc04
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2023-11-08 12:10:56 +0100

    Update schema

--- migrations/00001_initial/index.sql
+++ migrations/00001_initial/index.sql
@@ -10,13 +10,13 @@ CREATE table emails.signer_emails
     safe_address  character varying(42) NOT NULL,
     signer        character varying(42) NOT NULL,
     verified      boolean               NOT NULL DEFAULT false,
-    UNIQUE (chain_id, email_address, safe_address, signer)
+    UNIQUE (chain_id, safe_address, signer)
 );
 
 DROP TABLE IF EXISTS emails.verification CASCADE;
 CREATE table emails.verification
 (
     id                SERIAL PRIMARY KEY REFERENCES emails.signer_emails ON DELETE CASCADE,
-    verification_code text NOT NULL,
-    sent_on           timestamp with time zone DEFAULT now()
+    verification_code text                                   NOT NULL,
+    sent_on           timestamp with time zone DEFAULT now() NOT NULL
 )
