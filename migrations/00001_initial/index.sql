641e816121dfc9b16c31832b4841af8862bb2f3d
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2023-11-09 10:24:56 +0100

    Sort constraint declarations

--- migrations/00001_initial/index.sql
+++ migrations/00001_initial/index.sql
@@ -5,12 +5,12 @@ DROP TABLE IF EXISTS emails.signer_emails CASCADE;
 CREATE table emails.signer_emails
 (
     id                   SERIAL PRIMARY KEY,
-    chain_id             int                   NOT NULL,
-    email_address        text                  NOT NULL,
-    safe_address         character varying(42) NOT NULL,
-    signer               character varying(42) NOT NULL,
-    verified             boolean               NOT NULL DEFAULT false,
+    chain_id             int                      NOT NULL,
+    email_address        text                     NOT NULL,
+    safe_address         character varying(42)    NOT NULL,
+    signer               character varying(42)    NOT NULL,
+    verified             boolean                  NOT NULL DEFAULT false,
     verification_code    text,
-    verification_sent_on timestamp with time zone       DEFAULT now() NOT NULL,
+    verification_sent_on timestamp with time zone NOT NULL DEFAULT now(),
     UNIQUE (chain_id, safe_address, signer)
 );
