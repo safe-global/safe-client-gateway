ba626480cf6d1960ec91554ec7d890a13b3579e4
Aaron Cook <aaron@safe.global>
2023-11-13 12:03:45 +0100

    chore: add Alerts docs (#839)

--- src/domain/alerts/alerts.repository.interface.ts
+++ src/domain/alerts/alerts.repository.interface.ts
@@ -4,7 +4,15 @@ import { AlertLog } from '@/routes/alerts/entities/alert.dto.entity';
 export const IAlertsRepository = Symbol('IAlertsRepository');
 
 export interface IAlertsRepository {
+  /**
+   * Adds the {@link contracts} to the Alerts provider
+   * @param contracts - the network-specific {@link AlertsRegistration} to add
+   */
   addContracts(contracts: Array<AlertsRegistration>): Promise<void>;
 
+  /**
+   * Parses and notifies the user about the {@link log} from the Alerts provider
+   * @param log - the {@link AlertLog} to decode
+   */
   handleAlertLog(log: AlertLog): void;
 }
