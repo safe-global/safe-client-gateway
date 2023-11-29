9eefbf1746dff8d7f922eb5e83530583b8d0be23
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2023-11-29 17:56:14 +0100

    Add Test/AlertsDecodersModule

--- src/domain/alerts/alerts.domain.module.ts
+++ src/domain/alerts/alerts.domain.module.ts
@@ -2,18 +2,11 @@ import { Module } from '@nestjs/common';
 import { AlertsApiModule } from '@/datasources/alerts-api/alerts-api.module';
 import { AlertsRepository } from '@/domain/alerts/alerts.repository';
 import { IAlertsRepository } from '@/domain/alerts/alerts.repository.interface';
-import { DelayModifierDecoder } from '@/domain/alerts/contracts/delay-modifier-decoder.helper';
-import { SafeDecoder } from '@/domain/alerts/contracts/safe-decoder.helper';
-import { MultiSendDecoder } from '@/domain/alerts/contracts/multi-send-decoder.helper';
+import { AlertsDecodersModule } from '@/domain/alerts/alerts-decoders.module';
 
 @Module({
-  imports: [AlertsApiModule],
-  providers: [
-    { provide: IAlertsRepository, useClass: AlertsRepository },
-    DelayModifierDecoder,
-    MultiSendDecoder,
-    SafeDecoder,
-  ],
+  imports: [AlertsApiModule, AlertsDecodersModule],
+  providers: [{ provide: IAlertsRepository, useClass: AlertsRepository }],
   exports: [IAlertsRepository],
 })
 export class AlertsDomainModule {}
