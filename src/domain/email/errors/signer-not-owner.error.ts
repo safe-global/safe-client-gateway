c0f25b273cd6ba66eaf7112a7ce09af9fc745aef
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2023-11-30 15:14:39 +0100

    Remove unused SignerNotOwnerError
    Removes `SignerNotOwnerError`. This error was previously used in the domain layer for validating if the signer is an owner. However, that validation is now part of the route layer.

--- src/domain/email/errors/signer-not-owner.error.ts
+++
@@ -1,7 +0,0 @@
-export class SignerNotOwnerError extends Error {
-  constructor(chainId: string, safeAddress: string, signer: string) {
-    super(
-      `Signer ${signer} is not an owner of the safe ${safeAddress} on chain ${chainId}.`,
-    );
-  }
-}
