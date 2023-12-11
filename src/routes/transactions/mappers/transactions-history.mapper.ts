ab788384c3dd2ec7d4213ed37200caee9b80d585
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2023-12-11 17:32:13 +0000

    Fix transaction history timezone mapping

--- src/routes/transactions/mappers/transactions-history.mapper.ts
+++ src/routes/transactions/mappers/transactions-history.mapper.ts
@@ -96,6 +96,7 @@ export class TransactionsHistoryMapper {
             chainId,
             safe,
             onlyTrusted,
+            timezoneOffset,
           )
         )
           .filter(<T>(x: T | undefined): x is T => x != null)
@@ -189,6 +190,7 @@ export class TransactionsHistoryMapper {
     chainId: string,
     safe: Safe,
     onlyTrusted: boolean,
+    timezoneOffsetMs: number,
   ): Promise<TransactionItem[]> {
     const limitedTransfers = transfers.slice(0, this.maxNestedTransfers);
     const result: TransactionItem[] = [];
@@ -198,6 +200,7 @@ export class TransactionsHistoryMapper {
         chainId,
         transfer,
         safe,
+        timezoneOffsetMs,
       );
 
       const transferWithValue = this.mapZeroValueTransfer(nestedTransaction);
@@ -248,6 +251,7 @@ export class TransactionsHistoryMapper {
     chainId: string,
     safe: Safe,
     onlyTrusted: boolean,
+    timezoneOffsetMs: number,
   ): Promise<(TransactionItem | TransactionItem[] | undefined)[]> {
     return Promise.all(
       transactionGroup.transactions.map(async (transaction) => {
@@ -257,6 +261,7 @@ export class TransactionsHistoryMapper {
               chainId,
               transaction,
               safe,
+              timezoneOffsetMs,
             ),
           );
         } else if (isModuleTransaction(transaction)) {
@@ -264,6 +269,7 @@ export class TransactionsHistoryMapper {
             await this.moduleTransactionMapper.mapTransaction(
               chainId,
               transaction,
+              timezoneOffsetMs,
             ),
           );
         } else if (isEthereumTransaction(transaction)) {
@@ -274,6 +280,7 @@ export class TransactionsHistoryMapper {
               chainId,
               safe,
               onlyTrusted,
+              timezoneOffsetMs,
             );
           }
         } else if (isCreationTransaction(transaction)) {
@@ -282,6 +289,7 @@ export class TransactionsHistoryMapper {
               chainId,
               transaction,
               safe,
+              timezoneOffsetMs,
             ),
           );
         } else {
