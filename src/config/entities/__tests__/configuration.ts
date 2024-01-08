7fadf6805602e76bf6e71270bf133e079b009b06
Héctor Gómez <hector.gomez.varela@gmail.com>
2024-01-08 16:54:10 +0100

    RedisCacheService tests refactor (#985)
    Modifies `redis.cache.service.spec.ts` to use a real Redis test instance instead of a mocked class.

--- src/config/entities/__tests__/configuration.ts
+++ src/config/entities/__tests__/configuration.ts
@@ -135,8 +135,8 @@ export default (): ReturnType<typeof configuration> => ({
     },
   },
   redis: {
-    host: faker.internet.domainName(),
-    port: faker.internet.port().toString(),
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || '6379',
   },
   relay: { limit: faker.number.int({ min: 1 }) },
   safeConfig: {
