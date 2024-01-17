d585e5a2cc19c14f0334bb4cd6175a4e30b36902
Héctor Gómez <hector.gomez.varela@gmail.com>
2024-01-17 13:25:23 +0100

    Remove flush endpoint (#1038)
    - Removes the `FlushModule` routes module, including controllers, services, and entities.
    - Removes flush-associated classes in the domain layer.

--- src/domain/flush/entities/invalidation-target.entity.ts
+++
@@ -1,3 +0,0 @@
-export enum InvalidationTarget {
-  Chains = 'Chains',
-}
--- src/routes/flush/flush.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FlushController } from '@/routes/flush/flush.controller';
-import { FlushService } from '@/routes/flush/flush.service';
-
-@Module({
-  controllers: [FlushController],
-  providers: [FlushService],
-})
-export class FlushModule {}
