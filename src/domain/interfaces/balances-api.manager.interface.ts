e299bc5e83f5f1ab34c5a1a612aee972a4c54d5d
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2024-01-23 18:24:43 +0100

    Add documentation to interface functions

--- src/domain/interfaces/balances-api.manager.interface.ts
+++ src/domain/interfaces/balances-api.manager.interface.ts
@@ -3,6 +3,22 @@ import { IBalancesApi } from '@/domain/interfaces/balances-api.interface';
 export const IBalancesApiManager = Symbol('IBalancesApiManager');
 
 export interface IBalancesApiManager {
+  /**
+   * Determines whether an external balances provider is being used
+   * for a given chain ID.
+   *
+   * @param chainId - the chain identifier to check.
+   * @returns true if the chain balances are externalized.
+   */
   isExternalized(chainId: string): boolean;
+
+  /**
+   * Gets an {@link IBalancesApi} implementation.
+   * Each chain is associated to an implementation (i.e.: to a balances
+   * provider) via configuration.
+   *
+   * @param chainId - the chain identifier to check.
+   * @returns {@link IBalancesApi} configured for the input chain ID.
+   */
   getBalancesApi(chainId: string): IBalancesApi;
 }
