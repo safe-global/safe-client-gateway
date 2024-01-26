88ca2c8304a694797c36e4cd4595f1a14fbd7ca7
iamacook <aaron@safe.global>
2024-01-26 09:38:59 +0100

    Revert test value

--- src/config/entities/configuration.ts
+++ src/config/entities/configuration.ts
@@ -128,8 +128,7 @@ export default () => ({
   },
   safeConfig: {
     baseUri:
-      process.env.SAFE_CONFIG_BASE_URI ||
-      'https://safe-config.staging.5afe.dev//',
+      process.env.SAFE_CONFIG_BASE_URI || 'https://safe-config.safe.global/',
   },
   safeTransaction: {
     useVpcUrl: process.env.USE_TX_SERVICE_VPC_URL?.toLowerCase() === 'true',
