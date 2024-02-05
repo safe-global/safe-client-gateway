9b815fe3e5901d162b64f7a0fe7f53877f89d0cf
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2024-02-05 14:53:33 +0100

    Rename account-repository.service.ts to account.repository.ts
    Renames `account-repository.service.ts` to `account.repository.ts` to follow the file naming convention set for repositories.

--- src/domain/account/account.domain.module.ts
+++ src/domain/account/account.domain.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AccountDataSourceModule } from '@/datasources/account/account.datasource.module';
 import { IAccountRepository } from '@/domain/account/account.repository.interface';
-import { AccountRepository } from '@/domain/account/account-repository.service';
+import { AccountRepository } from '@/domain/account/account.repository';
 import { EmailApiModule } from '@/datasources/email-api/email-api.module';
 import { ISubscriptionRepository } from '@/domain/subscriptions/subscription.repository.interface';
 import { SubscriptionRepository } from '@/domain/subscriptions/subscription.repository';
