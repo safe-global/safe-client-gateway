6323654c0ee9e3fa8c3c158ee0a174176194605d
Frederico Sabino <3332770+fmrsabino@users.noreply.github.com>
2024-02-14 14:48:43 +0100

    Add SSL configuration to database (#1143)
    - Adds the option to configure the SSL component of the database connection.
    - Enabling SSL can be done by setting `POSTGRES_SSL_ENABLED` to `true`. (default is `false`)
    - `POSTGRES_SSL_REJECT_UNAUTHORIZED` can be used to reject any connections which are not authorised with the list of supplied CAs (default is `true`)

--- src/config/entities/configuration.ts
+++ src/config/entities/configuration.ts
@@ -92,6 +92,15 @@ export default () => ({
       database: process.env.POSTGRES_DB || 'safe-client-gateway',
       username: process.env.POSTGRES_USER || 'postgres',
       password: process.env.POSTGRES_PASSWORD || 'postgres',
+      ssl: {
+        enabled: process.env.POSTGRES_SSL_ENABLED?.toLowerCase() === 'true',
+        // If the value is not explicitly set to false, default should be true
+        // If not false the server will reject any connection which is not authorized with the list of supplied CAs
+        // https://nodejs.org/docs/latest-v20.x/api/tls.html#tlscreateserveroptions-secureconnectionlistener
+        rejectUnauthorized:
+          process.env.POSTGRES_SSL_REJECT_UNAUTHORIZED?.toLowerCase() !==
+          'false',
+      },
     },
   },
   email: {
