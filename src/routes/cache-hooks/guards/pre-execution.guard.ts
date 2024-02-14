5c9cfb7870538e0177c956a10ef91b3e7b9b620f
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2024-02-14 11:18:22 +0100

    PR comments

--- src/routes/cache-hooks/guards/pre-execution.guard.ts
+++ src/routes/cache-hooks/guards/pre-execution.guard.ts
@@ -1,38 +1,30 @@
+import { ILoggingService, LoggingService } from '@/logging/logging.interface';
 import {
   CanActivate,
   ExecutionContext,
   Inject,
   Injectable,
 } from '@nestjs/common';
-import { ILoggingService, LoggingService } from '@/logging/logging.interface';
-import { formatRouteLogMessage } from '@/logging/utils';
 
 /**
- * The PreExecutionLogGuard guard outputs a log line containing the request data.
+ * The PreExecutionLogGuard guard outputs a log line containing parts of the request data.
+ * Currently only the request path is being logged.
  */
 @Injectable()
 export class PreExecutionLogGuard implements CanActivate {
-  private readonly PRE_EXECUTION_LOGGING_DETAIL = 'pre-execution-log';
-  private readonly PRE_EXECUTION_LOGGING_STATUS = 202;
+  private static readonly PRE_EXECUTION_LOGGING_DETAIL = 'pre-execution-log';
 
   constructor(
     @Inject(LoggingService) private readonly loggingService: ILoggingService,
   ) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const startTimeMs: number = performance.now();
     const httpContext = context.switchToHttp();
     const request = httpContext.getRequest();
-
-    this.loggingService.info(
-      formatRouteLogMessage(
-        this.PRE_EXECUTION_LOGGING_STATUS,
-        request,
-        startTimeMs,
-        this.PRE_EXECUTION_LOGGING_DETAIL,
-      ),
-    );
-
+    this.loggingService.info({
+      type: PreExecutionLogGuard.PRE_EXECUTION_LOGGING_DETAIL,
+      route: request.route.path,
+    });
     return true;
   }
 }
