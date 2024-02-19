79938db47b1786760541a8c0efd880947b76287d
Aaron Cook <aaron@safe.global>
2024-02-19 12:14:52 +0100

    Reapply "Reapply ABI generation outside of `/dist` (#1157)" (#1158) (#1159)
    This reverts commit 73f5941a340d7e9c8032f26f0a1e30ff50ca32b5.
    
    This generates type safe ABIs are generated from the `@safe-global/safe-deployment` package.

--- src/domain/relay/contracts/proxy-factory-decoder.helper.ts
+++ src/domain/relay/contracts/proxy-factory-decoder.helper.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { parseAbi } from 'viem';
+import ProxyFactory130 from '@/abis/safe/v1.3.0/GnosisSafeProxyFactory.abi';
 import { AbiDecoder } from '@/domain/contracts/contracts/abi-decoder.helper';
 
-const PROXY_FACTORY_ABI = parseAbi([
-  'function createProxyWithNonce(address _singleton, bytes memory initializer, uint256 saltNonce)',
-]);
-
 @Injectable()
-export class ProxyFactoryDecoder extends AbiDecoder<typeof PROXY_FACTORY_ABI> {
+export class ProxyFactoryDecoder extends AbiDecoder<typeof ProxyFactory130> {
   constructor() {
-    super(PROXY_FACTORY_ABI);
+    super(ProxyFactory130);
   }
 }
