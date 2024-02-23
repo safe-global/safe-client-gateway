115f729fcaeabcfe9feaddc231eebdf41328c0ae
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2024-02-23 16:23:50 +0100

    Use checksummed addresses in safeBuilder
    Some routes, such as the Alert Controller routes, receive data from third parties which might include non-checksummed addresses.
    
    The processing for some of these routes might include converting non-checksummed addresses to checksummed ones. If the Safe Builder doesn't provide checksummed addresses by default, we'd have to know if we should mock third party routes with a checksummed address or not.
    
    With this new default, we provide support for mocking datasource calls that use checksummed addresses and non-checksummed ones (without having to change all the mocks that use the Safe address from the builder).

--- src/domain/safe/entities/__tests__/safe.builder.ts
+++ src/domain/safe/entities/__tests__/safe.builder.ts
@@ -1,16 +1,17 @@
 import { faker } from '@faker-js/faker';
 import { Builder, IBuilder } from '@/__tests__/builder';
 import { Safe } from '@/domain/safe/entities/safe.entity';
+import { getAddress } from 'viem';
 
 export function safeBuilder(): IBuilder<Safe> {
   return new Builder<Safe>()
-    .with('address', faker.finance.ethereumAddress())
+    .with('address', getAddress(faker.finance.ethereumAddress()))
     .with('nonce', faker.number.int())
     .with('threshold', faker.number.int())
-    .with('owners', [faker.finance.ethereumAddress()])
-    .with('masterCopy', faker.finance.ethereumAddress())
+    .with('owners', [getAddress(faker.finance.ethereumAddress())])
+    .with('masterCopy', getAddress(faker.finance.ethereumAddress()))
     .with('modules', null)
-    .with('fallbackHandler', faker.finance.ethereumAddress())
-    .with('guard', faker.finance.ethereumAddress())
+    .with('fallbackHandler', getAddress(faker.finance.ethereumAddress()))
+    .with('guard', getAddress(faker.finance.ethereumAddress()))
     .with('version', faker.system.semver());
 }
