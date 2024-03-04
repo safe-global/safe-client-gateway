f228b3cb6cc2640fa838f943f4445037535f14f6
iamacook <aaron@safe.global>
2024-03-04 12:48:04 +0100

    Add newline

--- src/datasources/locking-api/locking-api.module.ts
+++ src/datasources/locking-api/locking-api.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ILockingApi } from '@/domain/interfaces/locking-api.interface';
 import { LockingApi } from '@/datasources/locking-api/locking-api.service';
+
 @Module({
   providers: [{ provide: ILockingApi, useClass: LockingApi }],
   exports: [ILockingApi],
