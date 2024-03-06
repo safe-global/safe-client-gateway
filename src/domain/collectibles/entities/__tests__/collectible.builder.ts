e09063cf717e9aa77ee468ccfa6e08094cac259f
iamacook <aaron@safe.global>
2024-03-06 13:30:55 +0100

    Fix builder

--- src/domain/collectibles/entities/__tests__/collectible.builder.ts
+++ src/domain/collectibles/entities/__tests__/collectible.builder.ts
@@ -1,10 +1,11 @@
 import { faker } from '@faker-js/faker';
 import { Builder, IBuilder } from '@/__tests__/builder';
 import { Collectible } from '@/domain/collectibles/entities/collectible.entity';
+import { getAddress } from 'viem';
 
 export function collectibleBuilder(): IBuilder<Collectible> {
   return new Builder<Collectible>()
-    .with('address', faker.finance.ethereumAddress())
+    .with('address', getAddress(faker.finance.ethereumAddress()))
     .with('tokenName', faker.company.name())
     .with('tokenSymbol', faker.finance.currencySymbol())
     .with('logoUri', faker.internet.url({ appendSlash: false }))
