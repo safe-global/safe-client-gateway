1ca87509e0a3a8884158df72394d3e55897916a8
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2024-03-06 17:07:06 +0100

    Migrate DeleteTransactionDto validation to zod

--- src/routes/transactions/entities/delete-transaction.dto.entity.ts
+++ src/routes/transactions/entities/delete-transaction.dto.entity.ts
@@ -1,6 +1,10 @@
+import { DeleteTransactionSchema } from '@/routes/transactions/entities/schemas/delete-transaction.dto.schema';
 import { ApiProperty } from '@nestjs/swagger';
+import { z } from 'zod';
 
-export class DeleteTransactionDto {
+export class DeleteTransactionDto
+  implements z.infer<typeof DeleteTransactionSchema>
+{
   @ApiProperty()
   signature!: string;
 }
