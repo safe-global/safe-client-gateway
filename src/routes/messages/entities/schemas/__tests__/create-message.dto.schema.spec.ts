82141327b35ea953c3663631f660ec8f47aab568
iamacook <aaron@safe.global>
2024-03-07 12:48:41 +0100

    Add test

---
+++ src/routes/messages/entities/schemas/__tests__/create-message.dto.schema.spec.ts
@@ -0,0 +1,88 @@
+import { fakeJson } from '@/__tests__/faker';
+import { createMessageDtoBuilder } from '@/routes/messages/entities/__tests__/create-message.dto.builder';
+import { CreateMessageDtoSchema } from '@/routes/messages/entities/schemas/create-message.dto.schema';
+import { faker } from '@faker-js/faker';
+import { ZodError } from 'zod';
+
+describe('CreateMessageDtoSchema', () => {
+  it('should validate a valid record message', () => {
+    const createMessageDto = createMessageDtoBuilder()
+      .with('message', fakeJson())
+      .build();
+
+    const result = CreateMessageDtoSchema.safeParse(createMessageDto);
+
+    expect(result.success).toBe(true);
+  });
+
+  it('should validate a valid string message', () => {
+    const createMessageDto = createMessageDtoBuilder()
+      .with('message', faker.word.words())
+      .build();
+
+    const result = CreateMessageDtoSchema.safeParse(createMessageDto);
+
+    expect(result.success).toBe(true);
+  });
+
+  it('should allow optional safeAppId, defaulting to null', () => {
+    const createMessageDto = createMessageDtoBuilder().build();
+    // @ts-expect-error - inferred type doesn't allow optional properties
+    delete createMessageDto.safeAppId;
+
+    const result = CreateMessageDtoSchema.safeParse(createMessageDto);
+
+    expect(result.success && result.data.safeAppId).toBe(null);
+  });
+
+  it('should not validate invalid CreateMessageDto', () => {
+    const createMessageDto = { invalid: 'createMessageDto' };
+
+    const result = CreateMessageDtoSchema.safeParse(createMessageDto);
+
+    expect(!result.success && result.error).toStrictEqual(
+      new ZodError([
+        {
+          code: 'invalid_union',
+          unionErrors: [
+            // @ts-expect-error - inferred type doesn't allow optional properties
+            {
+              issues: [
+                {
+                  code: 'invalid_type',
+                  expected: 'object',
+                  received: 'undefined',
+                  path: ['message'],
+                  message: 'Required',
+                },
+              ],
+              name: 'ZodError',
+            },
+            // @ts-expect-error - inferred type doesn't allow optional properties
+            {
+              issues: [
+                {
+                  code: 'invalid_type',
+                  expected: 'string',
+                  received: 'undefined',
+                  path: ['message'],
+                  message: 'Required',
+                },
+              ],
+              name: 'ZodError',
+            },
+          ],
+          path: ['message'],
+          message: 'Invalid input',
+        },
+        {
+          code: 'invalid_type',
+          expected: 'string',
+          received: 'undefined',
+          path: ['signature'],
+          message: 'Required',
+        },
+      ]),
+    );
+  });
+});
