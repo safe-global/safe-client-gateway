015b14f1a90dd6b5778cb77a563742d964ddc586
iamacook <aaron@safe.global>
2024-03-07 10:22:44 +0100

    Allow optional value and default `null`

--- src/domain/collectibles/entities/schemas/collectible.schema.ts
+++ src/domain/collectibles/entities/schemas/collectible.schema.ts
@@ -8,10 +8,10 @@ export const CollectibleSchema = z.object({
   logoUri: z.string(),
   id: z.string(),
   // AJV remnant - do not use format: 'uri' as it fails on some payloads that should be considered valid
-  uri: z.string().nullable(),
-  name: z.string().nullable(),
-  description: z.string().nullable(),
+  uri: z.string().optional().nullable().default(null),
+  name: z.string().optional().nullable().default(null),
+  description: z.string().optional().nullable().default(null),
   // AJV remnant - do not use format: 'uri' as it fails on some payloads that should be considered valid
-  imageUri: z.string().nullable(),
-  metadata: z.record(z.unknown()).nullable(),
+  imageUri: z.string().optional().nullable().default(null),
+  metadata: z.record(z.unknown()).optional().nullable().default(null),
 });
