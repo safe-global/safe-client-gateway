3652db7c692e1ffe691d39373cae2175758fe9c7
iamacook <aaron@safe.global>
2024-03-07 10:32:15 +0100

    Allow optional values but default to `null`

--- src/routes/messages/entities/schemas/create-message.dto.schema.ts
+++ src/routes/messages/entities/schemas/create-message.dto.schema.ts
@@ -2,6 +2,6 @@ import { z } from 'zod';
 
 export const CreateMessageDtoSchema = z.object({
   message: z.union([z.record(z.unknown()), z.string()]),
-  safeAppId: z.number().nullable(),
+  safeAppId: z.number().optional().nullable().default(null),
   signature: z.string(),
 });
