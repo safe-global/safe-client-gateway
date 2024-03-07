b614d9ef1c46d157f9048e2d047aa98cadaa45a0
iamacook <aaron@safe.global>
2024-03-07 16:24:46 +0100

    Use `nullish`

--- src/domain/collectibles/entities/schemas/collectible.schema.ts
+++ src/domain/collectibles/entities/schemas/collectible.schema.ts
@@ -8,10 +8,10 @@ export const CollectibleSchema = z.object({
   logoUri: z.string(),
   id: z.string(),
   // AJV remnant - do not use format: 'uri' as it fails on some payloads that should be considered valid
-  uri: z.string().optional().nullable().default(null),
-  name: z.string().optional().nullable().default(null),
-  description: z.string().optional().nullable().default(null),
+  uri: z.string().nullish().default(null),
+  name: z.string().nullish().default(null),
+  description: z.string().nullish().default(null),
   // AJV remnant - do not use format: 'uri' as it fails on some payloads that should be considered valid
-  imageUri: z.string().optional().nullable().default(null),
-  metadata: z.record(z.unknown()).optional().nullable().default(null),
+  imageUri: z.string().nullish().default(null),
+  metadata: z.record(z.unknown()).nullish().default(null),
 });
