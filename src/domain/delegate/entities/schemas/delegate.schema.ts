80fb2db8386717ba7ebe6cec5186c4d09a08a3fd
iamacook <aaron@safe.global>
2024-03-07 10:27:41 +0100

    Allow optional value but default to `null`

--- src/domain/delegate/entities/schemas/delegate.schema.ts
+++ src/domain/delegate/entities/schemas/delegate.schema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { AddressSchema } from '@/validation/entities/schemas/address.schema';
 
 export const DelegateSchema = z.object({
-  safe: AddressSchema.nullable(),
+  safe: AddressSchema.optional().nullable().default(null),
   delegate: AddressSchema,
   delegator: AddressSchema,
   label: z.string(),
