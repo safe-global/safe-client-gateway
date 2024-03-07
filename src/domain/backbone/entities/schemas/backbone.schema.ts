6b4e522a95b3f57cdd5292cbb9be4e3e008df054
iamacook <aaron@safe.global>
2024-03-07 11:06:34 +0100

    Add tests

--- src/domain/backbone/entities/schemas/backbone.schema.ts
+++ src/domain/backbone/entities/schemas/backbone.schema.ts
@@ -6,6 +6,6 @@ export const BackboneSchema = z.object({
   api_version: z.string(),
   secure: z.boolean(),
   host: z.string(),
-  headers: z.array(z.string()).optional(),
-  settings: z.record(z.unknown()).optional(),
+  headers: z.array(z.string()).optional().nullable().default(null),
+  settings: z.record(z.unknown()).optional().nullable().default(null),
 });
