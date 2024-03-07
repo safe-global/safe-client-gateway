babe10a63ec1f27b856c85310dffcc6c5e7483f0
iamacook <aaron@safe.global>
2024-03-07 16:25:45 +0100

    Use `nullish`

--- src/routes/messages/entities/schemas/create-message.dto.schema.ts
+++ src/routes/messages/entities/schemas/create-message.dto.schema.ts
@@ -2,6 +2,6 @@ import { z } from 'zod';
 
 export const CreateMessageDtoSchema = z.object({
   message: z.union([z.record(z.unknown()), z.string()]),
-  safeAppId: z.number().optional().nullable().default(null),
+  safeAppId: z.number().nullish().default(null),
   signature: z.string(),
 });
