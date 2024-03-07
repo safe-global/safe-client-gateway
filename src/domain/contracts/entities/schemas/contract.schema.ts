c032b43720cef3d316cc80ba439c36fb1b6fb9ea
iamacook <aaron@safe.global>
2024-03-07 16:25:16 +0100

    Use `nullish`

--- src/domain/contracts/entities/schemas/contract.schema.ts
+++ src/domain/contracts/entities/schemas/contract.schema.ts
@@ -5,7 +5,7 @@ export const ContractSchema = z.object({
   address: AddressSchema,
   name: z.string(),
   displayName: z.string(),
-  logoUri: z.string().optional().nullable().default(null),
-  contractAbi: z.record(z.unknown()).optional().nullable().default(null),
+  logoUri: z.string().nullish().default(null),
+  contractAbi: z.record(z.unknown()).nullish().default(null),
   trustedForDelegateCall: z.boolean(),
 });
