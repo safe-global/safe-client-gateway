5233abd673d458c8e1b04f20268b3cf4f44dc1cb
Héctor Gómez <hector.gomez.varela@gmail.com>
2024-03-12 16:15:01 +0100

    Narrow  post delegates types (#1279)
    Narrow some `TransactionApi.postDelegate` input types form plain `string` to `0x${string}`.

--- src/datasources/transaction-api/transaction-api.service.ts
+++ src/datasources/transaction-api/transaction-api.service.ts
@@ -202,9 +202,9 @@ export class TransactionApi implements ITransactionApi {
   }
 
   async postDelegate(args: {
-    safeAddress: string | null;
-    delegate: string;
-    delegator: string;
+    safeAddress: `0x${string}` | null;
+    delegate: `0x${string}`;
+    delegator: `0x${string}`;
     signature: string;
     label: string;
   }): Promise<void> {
