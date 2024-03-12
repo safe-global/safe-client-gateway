db117f82d687f40dcc2a002ba2e3b4c8c7faf449
iamacook <aaron@safe.global>
2024-03-12 16:16:38 +0100

    Merge `try/catch` blocks

--- src/routes/recovery/guards/enable-recovery-alerts.guard.ts
+++ src/routes/recovery/guards/enable-recovery-alerts.guard.ts
@@ -60,17 +60,16 @@ export class EnableRecoveryAlertsGuard implements CanActivate {
     const message = `${EnableRecoveryAlertsGuard.ACTION_PREFIX}-${chainId}-${safeAddress}-${moduleAddress}-${signer}-${timestamp}`;
 
     try {
-      await verifyMessage({
+      const isValid = await verifyMessage({
         address: signer,
         message,
         signature,
       });
-    } catch (e) {
-      this.loggingService.debug(e);
-      return false;
-    }
 
-    try {
+      if (!isValid) {
+        return false;
+      }
+
       const { safes } = await this.safeRepository.getSafesByModule({
         chainId,
         moduleAddress,
