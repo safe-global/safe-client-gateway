5971db2fa138fe6ffa9b8a64400520010b64102b
iamacook <aaron@safe.global>
2024-03-14 09:26:03 +0100

    Add tests

--- src/routes/data-decode/entities/schemas/__tests__/get-data-decoded.dto.schema.spec.ts
+++ src/routes/data-decode/entities/schemas/__tests__/get-data-decoded.dto.schema.spec.ts
@@ -28,6 +28,52 @@ describe('GetDataDecodedDtoSchema', () => {
     );
   });
 
+  it('should allow optional to', () => {
+    const getDataDecodedDto = getDataDecodedDtoBuilder().build();
+
+    const result = GetDataDecodedDtoSchema.safeParse(getDataDecodedDto);
+
+    expect(result.success).toBe(true);
+  });
+
+  it('should not allow non-hex data', () => {
+    const getDataDecodedDto = getDataDecodedDtoBuilder()
+      .with('data', 'non-hex' as `0x${string}`)
+      .build();
+
+    const result = GetDataDecodedDtoSchema.safeParse(getDataDecodedDto);
+
+    expect(!result.success && result.error).toStrictEqual(
+      new ZodError([
+        {
+          code: 'custom',
+          message: 'Invalid input',
+          path: ['data'],
+        },
+      ]),
+    );
+  });
+
+  it('should not allow no hex', () => {
+    const getDataDecodedDto = getDataDecodedDtoBuilder().build();
+    // @ts-expect-error - inferred type does not allow optional propety
+    delete getDataDecodedDto.data;
+
+    const result = GetDataDecodedDtoSchema.safeParse(getDataDecodedDto);
+
+    expect(!result.success && result.error).toStrictEqual(
+      new ZodError([
+        {
+          code: 'invalid_type',
+          expected: 'string',
+          received: 'undefined',
+          path: ['data'],
+          message: 'Required',
+        },
+      ]),
+    );
+  });
+
   it('should not validate an invalid GetDataDecodedDto', () => {
     const getDataDecodedDto = { invalid: 'getDataDecodedDto' };
 
