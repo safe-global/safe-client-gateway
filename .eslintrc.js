31b85683348e1fab162b35e21c45139a95d6f626
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2024-03-15 14:43:32 +0100

    Remove duplicate eslint rule
    We were setting the `@typescript-eslint/no-explicit-any` rule twice: `off` and `error`. This leaves the `error` rule only (the active one).

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'error',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/consistent-type-assertions': [
       'warn',
