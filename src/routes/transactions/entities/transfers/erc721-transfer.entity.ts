9bdf73d6951ac2ba52fb2643dbf654b3ff98d65d
iamacook <aaron@safe.global>
2024-04-11 16:01:17 +0200

    Make `tokenAddress` non-nullable

--- src/routes/transactions/entities/transfers/erc721-transfer.entity.ts
+++ src/routes/transactions/entities/transfers/erc721-transfer.entity.ts
@@ -5,8 +5,8 @@ import {
 } from '@/routes/transactions/entities/transfers/transfer.entity';
 
 export class Erc721Transfer extends Transfer {
-  @ApiPropertyOptional({ type: String, nullable: true })
-  tokenAddress: `0x${string}` | null;
+  @ApiProperty()
+  tokenAddress: `0x${string}`;
   @ApiProperty()
   tokenId: string;
   @ApiPropertyOptional({ type: String, nullable: true })
@@ -19,7 +19,7 @@ export class Erc721Transfer extends Transfer {
   trusted: boolean | null;
 
   constructor(
-    tokenAddress: `0x${string}` | null,
+    tokenAddress: `0x${string}`,
     tokenId: string,
     tokenName: string | null = null,
     tokenSymbol: string | null = null,
