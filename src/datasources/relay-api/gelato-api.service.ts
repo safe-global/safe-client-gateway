29e67bea999f7b0524f50bb03aa142af35472543
iamacook <aaron@safe.global>
2024-04-12 13:24:19 +0200

    Move relay count CRUD to datasource

--- src/datasources/relay-api/gelato-api.service.ts
+++ src/datasources/relay-api/gelato-api.service.ts
@@ -6,6 +6,11 @@ import {
 import { IRelayApi } from '@/domain/interfaces/relay-api.interface';
 import { IConfigurationService } from '@/config/configuration.service.interface';
 import { HttpErrorFactory } from '@/datasources/errors/http-error-factory';
+import { CacheRouter } from '@/datasources/cache/cache.router';
+import {
+  CacheService,
+  ICacheService,
+} from '@/datasources/cache/cache.service.interface';
 
 @Injectable()
 export class GelatoApi implements IRelayApi {
@@ -18,6 +23,7 @@ export class GelatoApi implements IRelayApi {
   private static GAS_LIMIT_BUFFER = BigInt(150_000);
 
   private readonly baseUri: string;
+  private readonly ttlSeconds: number;
 
   constructor(
     @Inject(NetworkService)
@@ -25,9 +31,11 @@ export class GelatoApi implements IRelayApi {
     @Inject(IConfigurationService)
     private readonly configurationService: IConfigurationService,
     private readonly httpErrorFactory: HttpErrorFactory,
+    @Inject(CacheService) private readonly cacheService: ICacheService,
   ) {
     this.baseUri =
       this.configurationService.getOrThrow<string>('relay.baseUri');
+    this.ttlSeconds = configurationService.getOrThrow('relay.ttlSeconds');
   }
 
   async relay(args: {
@@ -62,5 +70,27 @@ export class GelatoApi implements IRelayApi {
 
   private getRelayGasLimit(gasLimit: bigint): bigint {
     return gasLimit + GelatoApi.GAS_LIMIT_BUFFER;
+  }
+
+  async getRelayCount(args: {
+    chainId: string;
+    address: string;
+  }): Promise<number> {
+    const cacheDir = CacheRouter.getRelayCacheDir(args);
+    const count = await this.cacheService.get(cacheDir);
+    return count ? parseInt(count) : 0;
+  }
+
+  async setRelayCount(args: {
+    chainId: string;
+    address: string;
+    count: number;
+  }): Promise<void> {
+    const cacheDir = CacheRouter.getRelayCacheDir(args);
+    await this.cacheService.set(
+      cacheDir,
+      args.count.toString(),
+      this.ttlSeconds,
+    );
   }
 }
