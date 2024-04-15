e7e347173245f6162971d03ea1675340d417929e
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2024-04-15 17:07:14 +0200

    Rename GetDataDecodedDto to TransactionDataDto
    Renames the `GetDataDecodedDto` to `TransactionDataDto`. Since the payload will be used by other endpoints (not related only with data-decoding). The main reason is that it's more descriptive to have a name that describes the payload itself instead of the action (as the payload can be used in multiple actions).

--- src/routes/data-decode/entities/schemas/get-data-decoded.dto.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-import { HexSchema } from '@/validation/entities/schemas/hex.schema';
-import { AddressSchema } from '@/validation/entities/schemas/address.schema';
-
-export const GetDataDecodedDtoSchema = z.object({
-  data: HexSchema,
-  to: AddressSchema.optional(),
-});
