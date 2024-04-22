1984c8a61b2cb81c77f08a8390b378542829fcbe
Hector Gómez Varela <hector@safe.global>
2024-04-22 13:07:40 +0200

    Add e2e tests

---
+++ src/__tests__/util/retry.ts
@@ -0,0 +1,33 @@
+/**
+ * Stops execution for {@link milliseconds} milliseconds.
+ */
+export async function milliseconds(milliseconds: number): Promise<void> {
+  await new Promise((_) => setTimeout(_, milliseconds));
+}
+
+/**
+ * Executes the function {@link fn}, retrying if it throws an error.
+ * Uses a linear strategy by retrying each {@link delayMs} milliseconds.
+ * If {@link maxAttempts} is reached, it throws the error returned by {@link fn} last execution.
+ */
+export async function retry(
+  fn: () => void,
+  maxAttempts: number = 40,
+  delayMs: number = 100,
+): Promise<void> {
+  let attempt = 0;
+  const execute = async (): Promise<void> => {
+    try {
+      return await fn();
+    } catch (error) {
+      if (attempt >= maxAttempts) {
+        throw error;
+      }
+    }
+
+    await milliseconds(delayMs);
+    attempt++;
+    return execute();
+  };
+  return execute();
+}
