98913038748dcd72d83e3c05c83dd21845e7b646
Aaron Cook <aaron@safe.global>
2024-05-02 13:07:14 +0200

    Merge message (confirmation) entities and schemas (#1488)
    This merges the message (confirmation) schemas in the relevant entity files:
    
    - Move `MessageConfirmationSchema` to `domain/messages/entities/message-confirmation.entity.ts`
    - Move `Message(Page)Schema` to `domain/messages/entities/message.entity.ts`
    - Split the schema-related tests into `domain/messages/entities/message(-confirmation-)entity.spec.ts`

--- src/domain/messages/entities/message.entity.spec.ts
+++ src/domain/messages/entities/message.entity.spec.ts
@@ -1,96 +1,15 @@
 import { fakeJson } from '@/__tests__/faker';
 import { pageBuilder } from '@/domain/entities/__tests__/page.builder';
-import { messageConfirmationBuilder } from '@/domain/messages/entities/__tests__/message-confirmation.builder';
 import { messageBuilder } from '@/domain/messages/entities/__tests__/message.builder';
-import { SignatureType } from '@/domain/messages/entities/message-confirmation.entity';
 import {
-  MessageConfirmationSchema,
-  MessagePageSchema,
   MessageSchema,
-} from '@/domain/messages/entities/schemas/message.schema';
+  MessagePageSchema,
+} from '@/domain/messages/entities/message.entity';
 import { faker } from '@faker-js/faker';
 import { getAddress } from 'viem';
 import { ZodError } from 'zod';
 
-describe('Message schemas', () => {
-  describe('MessageConfirmationSchema', () => {
-    it('should validate a valid MessageConfirmation', () => {
-      const messageConfirmation = messageConfirmationBuilder().build();
-
-      const result = MessageConfirmationSchema.safeParse(messageConfirmation);
-
-      expect(result.success).toBe(true);
-    });
-
-    it.each(['created' as const, 'modified' as const])(
-      'should coerce %s to a date',
-      (key) => {
-        const messageConfirmation = messageConfirmationBuilder()
-          .with(key, faker.date.recent().toISOString() as unknown as Date)
-          .build();
-
-        const result = MessageConfirmationSchema.safeParse(messageConfirmation);
-
-        expect(result.success && result.data[key]).toStrictEqual(
-          new Date(messageConfirmation[key]),
-        );
-      },
-    );
-
-    it('should checksum the owner', () => {
-      const nonChecksummedAddress = faker.finance
-        .ethereumAddress()
-        .toLowerCase() as `0x${string}`;
-      const messageConfirmation = messageConfirmationBuilder()
-        .with('owner', nonChecksummedAddress)
-        .build();
-
-      const result = MessageConfirmationSchema.safeParse(messageConfirmation);
-
-      expect(result.success && result.data.owner).toBe(
-        getAddress(nonChecksummedAddress),
-      );
-    });
-
-    it('should not allow non-hex signature', () => {
-      const messageConfirmation = messageConfirmationBuilder()
-        .with('signature', faker.string.numeric() as `0x${string}`)
-        .build();
-
-      const result = MessageConfirmationSchema.safeParse(messageConfirmation);
-
-      expect(!result.success && result.error).toStrictEqual(
-        new ZodError([
-          {
-            code: 'custom',
-            message: 'Invalid "0x" notated hex string',
-            path: ['signature'],
-          },
-        ]),
-      );
-    });
-
-    it('should not allow invalid signature types', () => {
-      const messageConfirmation = messageConfirmationBuilder()
-        .with('signatureType', faker.lorem.word() as SignatureType)
-        .build();
-
-      const result = MessageConfirmationSchema.safeParse(messageConfirmation);
-
-      expect(!result.success && result.error).toStrictEqual(
-        new ZodError([
-          {
-            received: messageConfirmation.signatureType,
-            code: 'invalid_enum_value',
-            options: ['CONTRACT_SIGNATURE', 'APPROVED_HASH', 'EOA', 'ETH_SIGN'],
-            path: ['signatureType'],
-            message: `Invalid enum value. Expected 'CONTRACT_SIGNATURE' | 'APPROVED_HASH' | 'EOA' | 'ETH_SIGN', received '${messageConfirmation.signatureType}'`,
-          },
-        ]),
-      );
-    });
-  });
-
+describe('Message entity schemas', () => {
   describe('MessageSchema', () => {
     it('should validate a valid Message', () => {
       const message = messageBuilder().build();
--- src/domain/messages/entities/message.entity.ts
+++ src/domain/messages/entities/message.entity.ts
@@ -1,4 +1,21 @@
-import { MessageSchema } from '@/domain/messages/entities/schemas/message.schema';
+import { buildPageSchema } from '@/domain/entities/schemas/page.schema.factory';
+import { MessageConfirmationSchema } from '@/domain/messages/entities/message-confirmation.entity';
+import { AddressSchema } from '@/validation/entities/schemas/address.schema';
+import { HexSchema } from '@/validation/entities/schemas/hex.schema';
 import { z } from 'zod';
 
 export type Message = z.infer<typeof MessageSchema>;
+
+export const MessageSchema = z.object({
+  created: z.coerce.date(),
+  modified: z.coerce.date(),
+  safe: AddressSchema,
+  messageHash: HexSchema,
+  message: z.union([z.string(), z.record(z.unknown())]),
+  proposedBy: AddressSchema,
+  safeAppId: z.number().nullish().default(null),
+  confirmations: z.array(MessageConfirmationSchema),
+  preparedSignature: HexSchema.nullish().default(null),
+});
+
+export const MessagePageSchema = buildPageSchema(MessageSchema);
