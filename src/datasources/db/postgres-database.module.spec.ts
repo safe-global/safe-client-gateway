88fdbdfab50362e12404d9a1b8277b2cc9782822
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2024-05-02 16:22:04 +0100

    Test PostgresDatabaseModule

---
+++ src/datasources/db/postgres-database.module.spec.ts
@@ -0,0 +1,46 @@
+import { Test } from '@nestjs/testing';
+import { PostgresDatabaseModule } from '@/datasources/db/postgres-database.module';
+import { ConfigurationModule } from '@/config/configuration.module';
+import configuration from '@/config/entities/__tests__/configuration';
+import { TestLoggingModule } from '@/logging/__tests__/test.logging.module';
+import postgres from 'postgres';
+import { join } from 'path';
+
+describe('PostgresDatabaseModule tests', () => {
+  let sql: postgres.Sql;
+
+  beforeEach(async () => {
+    const baseConfiguration = configuration();
+    const testConfiguration: typeof configuration = () => ({
+      ...baseConfiguration,
+      db: {
+        ...baseConfiguration.db,
+        postgres: {
+          ...baseConfiguration.db.postgres,
+          ssl: {
+            ...baseConfiguration.db.postgres.ssl,
+            caPath: join(__dirname, '../../../db_config/test/server.crt'),
+          },
+        },
+      },
+    });
+
+    const moduleRef = await Test.createTestingModule({
+      imports: [
+        PostgresDatabaseModule,
+        ConfigurationModule.register(testConfiguration),
+        TestLoggingModule,
+      ],
+    }).compile();
+
+    sql = moduleRef.get('DB_INSTANCE');
+  });
+
+  afterEach(async () => {
+    await sql.end();
+  });
+
+  it('connection is successful', async () => {
+    await sql`SELECT NOW() as current_time`;
+  });
+});
