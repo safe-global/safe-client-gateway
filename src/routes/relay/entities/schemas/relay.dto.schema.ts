ade8d69ce84e9093254e129b4ab4ff757f9b02fe
Aaron Cook <aaron@safe.global>
2024-05-08 12:01:36 +0200

    Remove legacy relay routes (#1516)
    Removes legacy relay routes:
    
    - Remove `RelayLegacyDtoSchema`, inferred type and tests
    - Remove `RelayDtoSchema['version']` fallback value
    - Remove `RelayLegacyController` and tests
      - `POST` `relay`
      - `GET` `relay/:chainId/:safeAddress`
    - Update `RelayController` tests

--- src/routes/relay/entities/schemas/relay.dto.schema.ts
+++ src/routes/relay/entities/schemas/relay.dto.schema.ts
@@ -3,18 +3,12 @@ import * as semver from 'semver';
 import { AddressSchema } from '@/validation/entities/schemas/address.schema';
 import { HexSchema } from '@/validation/entities/schemas/hex.schema';
 
-// TODO: Remove default when legacy support is removed
-const LEGACY_SUPPORTED_VERSION = '1.3.0';
-
 export const RelayDtoSchema = z.object({
   to: AddressSchema,
   data: HexSchema,
-  version: z
-    .string()
-    .refine((value) => semver.parse(value) !== null, {
-      message: 'Invalid semver string',
-    })
-    .default(LEGACY_SUPPORTED_VERSION),
+  version: z.string().refine((value) => semver.parse(value) !== null, {
+    message: 'Invalid semver string',
+  }),
   gasLimit: z
     .string()
     .optional()
--- src/routes/relay/relay.controller.module.ts
+++ src/routes/relay/relay.controller.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { RelayDomainModule } from '@/domain/relay/relay.domain.module';
 import { RelayService } from '@/routes/relay/relay.service';
 import { RelayController } from '@/routes/relay/relay.controller';
-import { RelayLegacyController } from '@/routes/relay/relay.legacy.controller';
 
 @Module({
   imports: [RelayDomainModule],
   providers: [RelayService],
-  controllers: [RelayController, RelayLegacyController],
+  controllers: [RelayController],
 })
 export class RelayControllerModule {}
