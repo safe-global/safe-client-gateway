dfca1011e9557f7e07e5418d903018afef59336b
iamacook <aaron@safe.global>
2024-05-16 11:11:06 +0200

    Ensure `CreateMessageDto['safeAppId']` is an integer

--- src/routes/messages/entities/__tests__/create-message.dto.builder.ts
+++ src/routes/messages/entities/__tests__/create-message.dto.builder.ts
@@ -5,6 +5,6 @@ import { CreateMessageDto } from '@/routes/messages/entities/create-message.dto.
 export function createMessageDtoBuilder(): IBuilder<CreateMessageDto> {
   return new Builder<CreateMessageDto>()
     .with('message', faker.word.words({ count: { min: 1, max: 5 } }))
-    .with('safeAppId', faker.number.int())
+    .with('safeAppId', faker.number.int({ min: 0 }))
     .with('signature', faker.string.hexadecimal({ length: 32 }));
 }
