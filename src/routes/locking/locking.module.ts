43b11470e09ae26320ab2b8d644afadf3202a5b0
Aaron Cook <aaron@safe.global>
2024-05-27 09:52:23 +0200

    Remove legacy locking routes (#1582)
    Removes "legacy" locking routes:
    
    - Remove `LockingController` (and test coverage)
    - Remove `LockingModule` (and registration)

--- src/routes/locking/locking.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LockingController } from '@/routes/locking/locking.controller';
-
-@Module({
-  controllers: [LockingController],
-})
-export class LockingModule {}
