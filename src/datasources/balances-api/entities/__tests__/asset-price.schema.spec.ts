369edc9f04839313571bd5f137e44da0e49d1abd
iamacook <aaron@safe.global>
2024-06-06 09:54:08 +0200

    Add errorneous test cases

--- src/datasources/balances-api/entities/__tests__/asset-price.schema.spec.ts
+++ src/datasources/balances-api/entities/__tests__/asset-price.schema.spec.ts
@@ -16,4 +16,45 @@ describe('AssetPriceSchema', () => {
 
     expect(result.success).toBe(true);
   });
+
+  it('should not validate an object with non-object values', () => {
+    const address = faker.finance.ethereumAddress();
+    const assetPrice = {
+      [address]: faker.number.int(),
+    };
+
+    const result = AssetPriceSchema.safeParse(assetPrice);
+
+    expect(!result.success && result.error.issues).toStrictEqual([
+      {
+        code: 'invalid_type',
+        expected: 'object',
+        message: 'Expected object, received number',
+        path: [address],
+        received: 'number',
+      },
+    ]);
+  });
+
+  it('should not validate an object with non-number or null values in the nested object', () => {
+    const address = faker.finance.ethereumAddress();
+    const currency = faker.finance.currencyCode();
+    const assetPrice = {
+      [address]: {
+        [currency]: faker.string.alphanumeric(),
+      },
+    };
+
+    const result = AssetPriceSchema.safeParse(assetPrice);
+
+    expect(!result.success && result.error.issues).toStrictEqual([
+      {
+        code: 'invalid_type',
+        expected: 'number',
+        message: 'Expected number, received string',
+        path: [address, currency],
+        received: 'string',
+      },
+    ]);
+  });
 });
