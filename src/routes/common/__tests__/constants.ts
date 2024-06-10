fd4a97831d5d9a3f1263091f267713a34d7d8a84
Héctor Gómez <hector@safe.global>
2024-06-10 17:19:44 +0200

    Update src/routes/common/__tests__/constants.ts
    Co-authored-by: Aaron Cook <aaron@safe.global>

--- src/routes/common/__tests__/constants.ts
+++ src/routes/common/__tests__/constants.ts
@@ -1,11 +1,9 @@
-import { getAddress } from 'viem';
-
 interface TestSafe {
   address: `0x${string}`;
   owners: `0x${string}`[];
 }
 
 export const TEST_SAFE: TestSafe = {
-  address: getAddress('0x8675B754342754A30A2AeF474D114d8460bca19b'),
-  owners: [getAddress('0x6c15f69EE76DA763e5b5DB6f7f0C29eb625bc9B7')],
+  address: '0x8675B754342754A30A2AeF474D114d8460bca19b' as const,
+  owners: ['0x6c15f69EE76DA763e5b5DB6f7f0C29eb625bc9B7' as const],
 };
