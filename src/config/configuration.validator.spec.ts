449ab9c920927600d647da3e9053e594f20d3f16
Héctor Gómez <hector@safe.global>
2024-06-12 11:18:40 +0200

    Check INFURA_API_KEY value at startup (#1642)
    Check INFURA_API_KEY value at startup

--- src/config/configuration.validator.spec.ts
+++ src/config/configuration.validator.spec.ts
@@ -18,6 +18,7 @@ describe('Configuration validator', () => {
     EMAIL_TEMPLATE_RECOVERY_TX: faker.string.alphanumeric(),
     EMAIL_TEMPLATE_UNKNOWN_RECOVERY_TX: faker.string.alphanumeric(),
     EMAIL_TEMPLATE_VERIFICATION_CODE: faker.string.alphanumeric(),
+    INFURA_API_KEY: faker.string.uuid(),
     RELAY_PROVIDER_API_KEY_GNOSIS_CHAIN: faker.string.uuid(),
     RELAY_PROVIDER_API_KEY_SEPOLIA: faker.string.uuid(),
   };
@@ -46,6 +47,7 @@ describe('Configuration validator', () => {
     { key: 'EMAIL_TEMPLATE_RECOVERY_TX' },
     { key: 'EMAIL_TEMPLATE_UNKNOWN_RECOVERY_TX' },
     { key: 'EMAIL_TEMPLATE_VERIFICATION_CODE' },
+    { key: 'INFURA_API_KEY' },
     { key: 'RELAY_PROVIDER_API_KEY_GNOSIS_CHAIN' },
     { key: 'RELAY_PROVIDER_API_KEY_SEPOLIA' },
   ])(
@@ -77,6 +79,7 @@ describe('Configuration validator', () => {
       EMAIL_TEMPLATE_RECOVERY_TX: faker.string.alphanumeric(),
       EMAIL_TEMPLATE_UNKNOWN_RECOVERY_TX: faker.string.alphanumeric(),
       EMAIL_TEMPLATE_VERIFICATION_CODE: faker.string.alphanumeric(),
+      INFURA_API_KEY: faker.string.uuid(),
       RELAY_PROVIDER_API_KEY_GNOSIS_CHAIN: faker.string.uuid(),
       RELAY_PROVIDER_API_KEY_SEPOLIA: faker.string.uuid(),
     };
