85a974b0e0b463082bef68bf6f0df6e8036b34f1
iamacook <aaron@safe.global>
2024-06-12 12:50:00 +0200

    Improve `postgres-shift` types

--- src/types/postgres-shift.d.ts
+++ src/types/postgres-shift.d.ts
@@ -1,12 +1,22 @@
 declare module 'postgres-shift' {
   import { Sql } from 'postgres';
 
-  // https://github.com/porsager/postgres-shift/blob/master/index.js
+  /**
+   * Shift the database to the latest migration.
+   *
+   * @param options.sql - The SQL instance to use.
+   * @param options.path - The path of the migrations directory.
+   * @param options.before - A callback to run before each migration.
+   * @param options.after - A callback to run after each migration.
+   *
+   * Note: this definition is for postgres-shit@0.1.0.
+   * @see https://github.com/porsager/postgres-shift/blob/e7a4cb16d66c33c73f4f730b7d1de33b7757fea9/index.js
+   */
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   export default async (options: {
     sql: Sql;
     path?: string;
-    before?: boolean | null;
-    after?: boolean | null;
-  }): Promise<unknown> => {};
+    before?: (args: { migration_id: string; name: string }) => void;
+    after?: (args: { migration_id: string; name: string }) => void;
+  }): Promise<void> => {};
 }
