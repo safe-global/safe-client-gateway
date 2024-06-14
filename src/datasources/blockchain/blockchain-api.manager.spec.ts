d323fd97b1dc10c6c79917fd3150484f47c20aa1
Héctor Gómez <hector@safe.global>
2024-06-14 11:34:38 +0200

    Ensure RPC URL formatting for Infura PublicClients (#1644)
    Ensure RPC URL formatting for Infura-backed PublicClients

--- src/datasources/blockchain/blockchain-api.manager.spec.ts
+++ src/datasources/blockchain/blockchain-api.manager.spec.ts
@@ -1,6 +1,8 @@
 import { FakeConfigurationService } from '@/config/__tests__/fake.configuration.service';
 import { BlockchainApiManager } from '@/datasources/blockchain/blockchain-api.manager';
 import { chainBuilder } from '@/domain/chains/entities/__tests__/chain.builder';
+import { rpcUriBuilder } from '@/domain/chains/entities/__tests__/rpc-uri.builder';
+import { RpcUriAuthentication } from '@/domain/chains/entities/rpc-uri-authentication.entity';
 import { IConfigApi } from '@/domain/interfaces/config-api.interface';
 import { faker } from '@faker-js/faker';
 
@@ -10,15 +12,13 @@ const configApiMock = jest.mocked({
 
 describe('BlockchainApiManager', () => {
   let target: BlockchainApiManager;
+  const infuraApiKey = faker.string.hexadecimal({ length: 32 });
 
   beforeEach(() => {
     jest.resetAllMocks();
 
     const fakeConfigurationService = new FakeConfigurationService();
-    fakeConfigurationService.set(
-      'blockchain.infura.apiKey',
-      faker.string.hexadecimal({ length: 32 }),
-    );
+    fakeConfigurationService.set('blockchain.infura.apiKey', infuraApiKey);
     target = new BlockchainApiManager(fakeConfigurationService, configApiMock);
   });
 
@@ -31,6 +31,45 @@ describe('BlockchainApiManager', () => {
       const cachedApi = await target.getBlockchainApi(chain.chainId);
 
       expect(api).toBe(cachedApi);
+    });
+
+    it('should include the INFURA_API_KEY in the RPC URI for an Infura URI with API_KEY_PATH authentication', async () => {
+      const rpcUriValue = `https://${faker.string.sample()}.infura.io/v3/`;
+      const chain = chainBuilder()
+        .with(
+          'rpcUri',
+          rpcUriBuilder()
+            .with('value', rpcUriValue)
+            .with('authentication', RpcUriAuthentication.ApiKeyPath)
+            .build(),
+        )
+        .build();
+      configApiMock.getChain.mockResolvedValue(chain);
+
+      const api = await target.getBlockchainApi(chain.chainId);
+
+      expect(api.chain?.rpcUrls.default.http[0]).toContain(infuraApiKey);
+    });
+
+    it('should not include the INFURA_API_KEY in the RPC URI for an Infura URI without API_KEY_PATH authentication', async () => {
+      const rpcUriValue = `https://${faker.string.sample()}.infura.io/v3/`;
+      const chain = chainBuilder()
+        .with('rpcUri', rpcUriBuilder().with('value', rpcUriValue).build())
+        .build();
+      configApiMock.getChain.mockResolvedValue(chain);
+
+      const api = await target.getBlockchainApi(chain.chainId);
+
+      expect(api.chain?.rpcUrls.default.http[0]).not.toContain(infuraApiKey);
+    });
+
+    it('should not include the INFURA_API_KEY in the RPC URI for a RPC provider different from Infura', async () => {
+      const chain = chainBuilder().build();
+      configApiMock.getChain.mockResolvedValue(chain);
+
+      const api = await target.getBlockchainApi(chain.chainId);
+
+      expect(api.chain?.rpcUrls.default.http[0]).not.toContain(infuraApiKey);
     });
   });
 
