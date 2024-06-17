783ded0cb453f3bf8e3d79e2d9b8472b84b51769
iamacook <aaron@safe.global>
2024-06-17 10:28:09 +0200

    Don't coerce numerical strings of `CampaignActivities`

--- src/domain/community/entities/campaign-activity.entity.ts
+++ src/domain/community/entities/campaign-activity.entity.ts
@@ -1,14 +1,15 @@
 import { buildPageSchema } from '@/domain/entities/schemas/page.schema.factory';
 import { AddressSchema } from '@/validation/entities/schemas/address.schema';
+import { NumericStringSchema } from '@/validation/entities/schemas/numeric-string.schema';
 import { z } from 'zod';
 
 export const CampaignActivitySchema = z.object({
   startDate: z.coerce.date(),
   endDate: z.coerce.date(),
   holder: AddressSchema,
-  boost: z.coerce.number(),
-  totalPoints: z.coerce.number(),
-  totalBoostedPoints: z.coerce.number(),
+  boost: NumericStringSchema,
+  totalPoints: NumericStringSchema,
+  totalBoostedPoints: NumericStringSchema,
 });
 
 export const CampaignActivityPageSchema = buildPageSchema(
