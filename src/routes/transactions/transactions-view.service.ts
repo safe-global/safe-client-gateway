97ac1e53cc9152962509ccd5aa612e2787d10a05
Hector Gómez Varela <hector@safe.global>
2024-07-03 23:16:54 +0200

    Remove useless catch block

--- src/routes/transactions/transactions-view.service.ts
+++ src/routes/transactions/transactions-view.service.ts
@@ -172,9 +172,17 @@ export class TransactionsViewService {
       chainId: args.chainId,
     });
 
-    const [fullAppData, buyToken, sellToken] = await Promise.all([
-      // Decode hash of `appData`
-      this.swapsRepository.getFullAppData(args.chainId, twapStruct.appData),
+    // Decode hash of `appData`
+    const fullAppData = await this.swapsRepository.getFullAppData(
+      args.chainId,
+      twapStruct.appData,
+    );
+
+    if (!this.twapOrderHelper.isAppAllowed(fullAppData)) {
+      throw new Error(`Unsupported App: ${fullAppData.fullAppData?.appCode}`);
+    }
+
+    const [buyToken, sellToken] = await Promise.all([
       this.swapOrderHelper.getToken({
         chainId: args.chainId,
         address: twapStruct.buyToken,
@@ -183,14 +191,7 @@ export class TransactionsViewService {
         chainId: args.chainId,
         address: twapStruct.sellToken,
       }),
-    ]).catch((error) => {
-      this.loggingService.error(error);
-      throw new Error('Error fetching full app data');
-    });
-
-    if (!this.twapOrderHelper.isAppAllowed(fullAppData)) {
-      throw new Error(`Unsupported App: ${fullAppData.fullAppData?.appCode}`);
-    }
+    ]);
 
     return new CowSwapTwapConfirmationView({
       method: args.dataDecoded.method,
