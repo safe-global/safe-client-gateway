535e91dd24f4f93ee7e41e8f6c8ad584cdc394a2
Hector Gómez Varela <hector@safe.global>
2024-07-17 17:28:22 +0200

    Rename Account.accountId to Account.id on the route-level entity

--- src/routes/accounts/entities/account.entity.ts
+++ src/routes/accounts/entities/account.entity.ts
@@ -2,18 +2,14 @@ import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class Account {
   @ApiProperty()
-  accountId: string;
+  id: string;
   @ApiPropertyOptional({ type: String, nullable: true })
   groupId: string | null;
   @ApiProperty()
   address: `0x${string}`;
 
-  constructor(
-    accountId: string,
-    groupId: string | null,
-    address: `0x${string}`,
-  ) {
-    this.accountId = accountId;
+  constructor(id: string, groupId: string | null, address: `0x${string}`) {
+    this.id = id;
     this.groupId = groupId;
     this.address = address;
   }
