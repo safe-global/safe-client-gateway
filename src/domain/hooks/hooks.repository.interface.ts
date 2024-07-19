53b7964032eb4415b14365989729cd3e75106d85
Aaron Cook <aaron@safe.global>
2024-07-19 11:52:45 +0200

    Move hook domain logic from routes to domain (#1774)
    Moves domain-considered logic from `CacheHooksService` to a new `HooksRepository` in the domain:
    
    - Move domain logic from `CacheHooksService` to `HooksRepository`
    - Propagate changes accordingly

---
+++ src/domain/hooks/hooks.repository.interface.ts
@@ -0,0 +1,35 @@
+import { BalancesRepositoryModule } from '@/domain/balances/balances.repository.interface';
+import { BlockchainRepositoryModule } from '@/domain/blockchain/blockchain.repository.interface';
+import { ChainsRepositoryModule } from '@/domain/chains/chains.repository.interface';
+import { CollectiblesRepositoryModule } from '@/domain/collectibles/collectibles.repository.interface';
+import { HooksRepository } from '@/domain/hooks/hooks.repository';
+import { MessagesRepositoryModule } from '@/domain/messages/messages.repository.interface';
+import { QueuesRepositoryModule } from '@/domain/queues/queues-repository.interface';
+import { SafeAppsRepositoryModule } from '@/domain/safe-apps/safe-apps.repository.interface';
+import { SafeRepositoryModule } from '@/domain/safe/safe.repository.interface';
+import { TransactionsRepositoryModule } from '@/domain/transactions/transactions.repository.interface';
+import { Event } from '@/routes/cache-hooks/entities/event.entity';
+import { Module } from '@nestjs/common';
+
+export const IHooksRepository = Symbol('IHooksRepository');
+
+export interface IHooksRepository {
+  onEvent(event: Event): Promise<unknown>;
+}
+
+@Module({
+  imports: [
+    BalancesRepositoryModule,
+    BlockchainRepositoryModule,
+    ChainsRepositoryModule,
+    CollectiblesRepositoryModule,
+    MessagesRepositoryModule,
+    SafeAppsRepositoryModule,
+    SafeRepositoryModule,
+    TransactionsRepositoryModule,
+    QueuesRepositoryModule,
+  ],
+  providers: [{ provide: IHooksRepository, useClass: HooksRepository }],
+  exports: [IHooksRepository],
+})
+export class HooksRepositoryModule {}
--- src/routes/cache-hooks/cache-hooks.module.ts
+++ src/routes/cache-hooks/cache-hooks.module.ts
@@ -1,28 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CacheHooksController } from '@/routes/cache-hooks/cache-hooks.controller';
+import { HooksRepositoryModule } from '@/domain/hooks/hooks.repository.interface';
 import { CacheHooksService } from '@/routes/cache-hooks/cache-hooks.service';
-import { BalancesRepositoryModule } from '@/domain/balances/balances.repository.interface';
-import { CollectiblesRepositoryModule } from '@/domain/collectibles/collectibles.repository.interface';
-import { ChainsRepositoryModule } from '@/domain/chains/chains.repository.interface';
-import { SafeRepositoryModule } from '@/domain/safe/safe.repository.interface';
-import { TransactionsRepositoryModule } from '@/domain/transactions/transactions.repository.interface';
-import { MessagesRepositoryModule } from '@/domain/messages/messages.repository.interface';
-import { SafeAppsRepositoryModule } from '@/domain/safe-apps/safe-apps.repository.interface';
-import { QueuesRepositoryModule } from '@/domain/queues/queues-repository.interface';
-import { BlockchainRepositoryModule } from '@/domain/blockchain/blockchain.repository.interface';
 
 @Module({
-  imports: [
-    BalancesRepositoryModule,
-    BlockchainRepositoryModule,
-    ChainsRepositoryModule,
-    CollectiblesRepositoryModule,
-    MessagesRepositoryModule,
-    SafeAppsRepositoryModule,
-    SafeRepositoryModule,
-    TransactionsRepositoryModule,
-    QueuesRepositoryModule,
-  ],
+  imports: [HooksRepositoryModule],
   providers: [CacheHooksService],
   controllers: [CacheHooksController],
 })
