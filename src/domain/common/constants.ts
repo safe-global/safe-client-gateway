5f5f15a990491cbd00929f05e22a1ae9389c64ac
Hector Gómez Varela <hector@safe.global>
2024-07-26 11:46:31 +0200

    Add DB_MAX_SAFE_INTEGER constant

---
+++ src/domain/common/constants.ts
@@ -0,0 +1,5 @@
+/**
+ * The maximum safe integer for the database.
+ * This is the maximum value that can be stored in the database for an integer column.
+ */
+export const DB_MAX_SAFE_INTEGER = Math.pow(2, 31) - 1;
