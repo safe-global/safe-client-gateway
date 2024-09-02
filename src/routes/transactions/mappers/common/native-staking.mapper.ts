e77712fbb2c211d2a1f0b63a8489d4d6ef729599
Hector Gómez Varela <hector@safe.global>
2024-09-02 17:48:30 +0200

    Use Promise.all in NativeStakingMapper

--- src/routes/transactions/mappers/common/native-staking.mapper.ts
+++ src/routes/transactions/mappers/common/native-staking.mapper.ts
@@ -196,11 +196,13 @@ export class NativeStakingMapper {
     value: string | null;
     transaction: MultisigTransaction | ModuleTransaction | null;
   }): Promise<NativeStakingWithdrawTransactionInfo> {
-    const chain = await this.chainsRepository.getChain(args.chainId);
-    const deployment = await this.stakingRepository.getDeployment({
-      chainId: args.chainId,
-      address: args.to,
-    });
+    const [chain, deployment] = await Promise.all([
+      this.chainsRepository.getChain(args.chainId),
+      this.stakingRepository.getDeployment({
+        chainId: args.chainId,
+        address: args.to,
+      }),
+    ]);
 
     if (
       deployment.product_type !== 'dedicated' ||
