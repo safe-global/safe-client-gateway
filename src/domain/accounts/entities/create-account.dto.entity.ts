d3780c25758977251c3c9927f0de24efe545a119
Hector Gómez Varela <hector@safe.global>
2024-09-05 23:25:46 +0200

    Modify Account.name regular expression for validation

--- src/domain/accounts/entities/create-account.dto.entity.ts
+++ src/domain/accounts/entities/create-account.dto.entity.ts
@@ -7,7 +7,7 @@ export const CreateAccountDtoSchema = z.object({
     .string()
     .min(3, { message: 'Account names must be at least 3 characters long' })
     .max(20, { message: 'Account names must be at most 20 characters long' })
-    .regex(/^[a-zA-Z0-9]([._-]?[a-zA-Z0-9]+)*$/, {
+    .regex(/^[a-zA-Z0-9]+(?:[._-][a-zA-Z0-9]+)*$/, {
       message:
         'Account names must start with a letter or number and can contain alphanumeric characters, periods, underscores, and hyphens',
     }),
