160715b1c17544d82f7ecb8867805ec1570d7411
Pooya Raki <Pooya.raki@gmail.com>
2024-09-23 17:39:02 +0200

    add new notification entities

---
+++ src/routes/notifications/v2/entities/notificationSubscription.entity.ts
@@ -0,0 +1,64 @@
+import { NotificationDevice } from '@/routes/notifications/v2/entities/notificationDevices.entity';
+import type { NotificationSubscriptionNotificationType } from '@/routes/notifications/v2/entities/notificationSubscriptionNotificationType.entity';
+import {
+  Column,
+  Entity,
+  Unique,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  OneToMany,
+} from 'typeorm';
+
+@Entity('notification_subscriptions')
+@Unique([
+  'chain_id',
+  'safe_address',
+  'push_notification_device',
+  'signer_address',
+])
+export class NotificationSubscription {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @ManyToOne(() => NotificationDevice, (device) => device.id, {
+    onDelete: 'CASCADE',
+  })
+  push_notification_device!: NotificationDevice;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  chain_id!: string;
+
+  @Column({
+    type: 'varchar',
+    length: 42,
+  })
+  safe_address!: string;
+
+  @Column({
+    type: 'varchar',
+    length: 42,
+  })
+  signer_address!: string;
+
+  @Column({
+    type: 'timestamp with time zone',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  created_at!: Date;
+
+  @Column({
+    type: 'timestamp with time zone',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at!: Date;
+
+  @OneToMany(
+    () => NotificationSubscription,
+    (notificationSubscription) => notificationSubscription.id,
+  )
+  notification_subscription_notification_type!: NotificationSubscriptionNotificationType[];
+}
