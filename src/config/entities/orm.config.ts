db940af8b589558a445df1901bddcce30bec781f
Pooya Raki <Pooya.raki@gmail.com>
2024-09-27 12:44:01 +0200

    registers typeorm manually, restructure datasources

--- src/config/entities/orm.config.ts
+++ src/config/entities/orm.config.ts
@@ -3,7 +3,6 @@ import configuration from '@/config/entities/configuration';
 import { postgresConfig } from '@/config/entities/postgres.config';
 
 export default new DataSource({
-  entities: ['dist/src/datasources/**/entities/*.entity.db{ .ts,.js}'],
   ...postgresConfig({
     ...configuration().db.connection.postgres,
     type: 'postgres',
