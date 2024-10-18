7340b6485b108c2c9f65812105ef43c137bf9807
Hector Gómez Varela <hector@safe.global>
2024-10-18 20:06:10 +0200

    Add first Outreach data file migration

---
+++ migrations/00008_targeted_messaging_add_outreach_1/index.sql
@@ -0,0 +1,19 @@
+INSERT INTO outreaches (
+    source_id,
+    name,
+    start_date,
+    end_date,
+    type,
+    team_name,
+    source_file,
+    source_file_checksum
+) VALUES (
+    1,
+    '',
+    '1970-01-01 00:00:00.000',
+    '1970-01-01 00:00:00.000',
+    '',
+    '',
+    'cluster_5_campaign.json',
+    '443bf977e13c19788cd8e677e7efa73e2820d8c2bb68377a6ae1644e69486115'
+);
