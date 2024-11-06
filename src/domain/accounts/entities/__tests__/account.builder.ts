fc749ad9d8a857bb443ac8907f647edd68ecbe66
Hector Gómez Varela <hector@safe.global>
2024-11-06 17:10:19 +0100

    Add accountNameBuilder

--- src/domain/accounts/entities/__tests__/account.builder.ts
+++ src/domain/accounts/entities/__tests__/account.builder.ts
@@ -1,20 +1,17 @@
 import type { IBuilder } from '@/__tests__/builder';
 import { Builder } from '@/__tests__/builder';
+import { accountNameBuilder } from '@/domain/accounts/entities/__tests__/account-name.builder';
 import type { Account } from '@/domain/accounts/entities/account.entity';
 import { DB_MAX_SAFE_INTEGER } from '@/domain/common/constants';
 import { faker } from '@faker-js/faker';
 import { getAddress } from 'viem';
-
-// Note: regular expression is simplified because faker has limited support for regex.
-// https://fakerjs.dev/api/helpers#fromregexp
-export const accountNameSimpleRegex = /[a-zA-Z0-9]{12,20}/i;
 
 export function accountBuilder(): IBuilder<Account> {
   return new Builder<Account>()
     .with('id', faker.number.int({ max: DB_MAX_SAFE_INTEGER }))
     .with('group_id', faker.number.int({ max: DB_MAX_SAFE_INTEGER }))
     .with('address', getAddress(faker.finance.ethereumAddress()))
-    .with('name', faker.helpers.fromRegExp(accountNameSimpleRegex))
+    .with('name', accountNameBuilder())
     .with('created_at', faker.date.recent())
     .with('updated_at', faker.date.recent());
 }
--- src/domain/accounts/entities/__tests__/create-account.dto.builder.ts
+++ src/domain/accounts/entities/__tests__/create-account.dto.builder.ts
@@ -1,6 +1,6 @@
 import type { IBuilder } from '@/__tests__/builder';
 import { Builder } from '@/__tests__/builder';
-import { accountNameSimpleRegex } from '@/domain/accounts/entities/__tests__/account.builder';
+import { accountNameBuilder } from '@/domain/accounts/entities/__tests__/account-name.builder';
 import type { CreateAccountDto } from '@/domain/accounts/entities/create-account.dto.entity';
 import { faker } from '@faker-js/faker';
 import { getAddress } from 'viem';
@@ -8,5 +8,5 @@ import { getAddress } from 'viem';
 export function createAccountDtoBuilder(): IBuilder<CreateAccountDto> {
   return new Builder<CreateAccountDto>()
     .with('address', getAddress(faker.finance.ethereumAddress()))
-    .with('name', faker.helpers.fromRegExp(accountNameSimpleRegex));
+    .with('name', accountNameBuilder());
 }
