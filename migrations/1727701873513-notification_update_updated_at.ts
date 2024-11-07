a80866ac707c421558dd38487f53a789ba300c38
Pooya Raki <Pooya.raki@gmail.com>
2024-11-07 18:02:05 +0100

    Add a new migration to clean up notification tables before creating new tables

--- migrations/1727701873513-notification_update_updated_at.ts
+++ migrations/1727701873513-notification_update_updated_at.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import type { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class NotificationUpdateUpdatedAt1727701873513
   implements MigrationInterface
