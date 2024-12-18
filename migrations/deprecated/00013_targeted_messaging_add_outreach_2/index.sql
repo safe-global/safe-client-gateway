905a4b26009e797177d7c6e56e9245d05f1258c8
James Mealy <jamesmealy1@gmail.com>
2024-12-18 14:02:57 +0000

    Create new outreach campaign (#2210)
    * Feat: add a new outreach campaign
    
    * fix formatting
    
    * add test for new migration

---
+++ migrations/deprecated/00013_targeted_messaging_add_outreach_2/index.sql
@@ -0,0 +1,17 @@
+INSERT INTO outreaches (
+    source_id,
+    name,
+    start_date,
+    end_date,
+    type,
+    team_name,
+    target_all
+) VALUES (
+    2,
+    'micro-campaign',
+    '2025-01-01 00:00:00.000',
+    '2025-03-01 00:00:00.000',
+    '',
+    '',
+    true
+);
