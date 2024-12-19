a6d5ca00ceb927b02684da84f4a1708690da7176
Aaron Cook <aaron@safe.global>
2024-12-19 06:36:40 +0000

    Remove TODO from AssetPriceSchema

--- src/datasources/balances-api/entities/asset-price.entity.ts
+++ src/datasources/balances-api/entities/asset-price.entity.ts
@@ -2,7 +2,6 @@ import { z } from 'zod';
 
 export type AssetPrice = z.infer<typeof AssetPriceSchema>;
 
-// TODO: Enforce Ethereum address keys (and maybe checksum them)
 export const AssetPriceSchema = z.record(z.record(z.number().nullable()));
 
 export const AssetPricesSchema = z.array(AssetPriceSchema);
