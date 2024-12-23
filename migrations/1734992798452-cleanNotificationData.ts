b4b0c96ef171b37ff2dd8e5c776ffc0b9407e864
Pooya Raki <Pooya.raki@gmail.com>
2024-12-23 23:27:46 +0100

    Remove: clean notification data

---
+++ migrations/1734992798452-cleanNotificationData.ts
@@ -0,0 +1,13 @@
+import type { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CleanNotificationData1734992798452 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DELETE FROM notification_subscription_notification_types;`,
+    );
+    await queryRunner.query(`DELETE FROM notification_subscriptions;`);
+    await queryRunner.query(`DELETE FROM push_notification_devices;`);
+  }
+
+  public async down(): Promise<void> {}
+}
