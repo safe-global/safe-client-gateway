f3f38a9d6641346e554910d7e1fef0d0b1b8362e
Aaron Cook <aaron@safe.global>
2025-01-15 17:25:03 +0100

    Add more tests

--- src/routes/portfolio/mappers/portfolio.mapper.spec.ts
+++ src/routes/portfolio/mappers/portfolio.mapper.spec.ts
@@ -5,7 +5,9 @@ import {
   assetByProtocolsBuilder,
   complexProtocolPositionBuilder,
   nestedProtocolPositionBuilder,
+  portfolioAssetBuilder,
   portfolioBuilder,
+  protocolPositionBuilder,
   regularProtocolPositionBuilder,
 } from '@/domain/portfolio/entities/__tests__/portfolio.builder';
 import { PortfolioMapper } from '@/routes/portfolio/mappers/portfolio.mapper';
@@ -30,12 +32,11 @@ describe('PortfolioMapper', () => {
     const [key, chainId] = faker.helpers.arrayElement(chainKeys);
     const protocol = faker.string.sample();
     const protocolType = faker.helpers.arrayElement(ProtocolPositionType);
-
     const regularProtocolPosition = regularProtocolPositionBuilder().build();
     const protocolPositions = { [protocolType]: regularProtocolPosition };
     const assetByProtocolChains = assetByProtocolChainsBuilder()
       .with(key, {
-        protocolPositions: protocolPositions,
+        protocolPositions,
       })
       .build();
     const assetByProtocol = assetByProtocolBuilder()
@@ -62,6 +63,7 @@ describe('PortfolioMapper', () => {
           protocolPositions: [
             {
               type: 'REGULAR',
+              fiatBalance: regularProtocolPosition.totalValue,
               name: regularProtocolPosition.name,
               assets: regularProtocolPosition.assets.map((asset) => {
                 return {
@@ -77,7 +79,6 @@ describe('PortfolioMapper', () => {
                   trusted: false,
                 };
               }),
-              fiatBalance: regularProtocolPosition.totalValue,
             },
           ],
         },
@@ -92,7 +93,6 @@ describe('PortfolioMapper', () => {
     const [key, chainId] = faker.helpers.arrayElement(chainKeys);
     const protocol = faker.string.sample();
     const protocolType = faker.helpers.arrayElement(ProtocolPositionType);
-
     const nestedProtocolPosition = nestedProtocolPositionBuilder().build();
     const complexProtocolPosition = complexProtocolPositionBuilder()
       .with('protocolPositions', [nestedProtocolPosition])
@@ -100,7 +100,7 @@ describe('PortfolioMapper', () => {
     const protocolPositions = { [protocolType]: complexProtocolPosition };
     const assetByProtocolChains = assetByProtocolChainsBuilder()
       .with(key, {
-        protocolPositions: protocolPositions,
+        protocolPositions,
       })
       .build();
     const assetByProtocol = assetByProtocolBuilder()
@@ -127,11 +127,12 @@ describe('PortfolioMapper', () => {
           protocolPositions: [
             {
               type: 'COMPLEX',
+              fiatBalance: complexProtocolPosition.totalValue,
               name: complexProtocolPosition.name,
               positions: [
                 {
-                  name: nestedProtocolPosition.name,
                   fiatBalance: nestedProtocolPosition.value,
+                  name: nestedProtocolPosition.name,
                   healthRate: nestedProtocolPosition.healthRate,
                   assets: [
                     ...nestedProtocolPosition.assets.map((asset) => {
@@ -213,7 +214,6 @@ describe('PortfolioMapper', () => {
                   ],
                 },
               ],
-              fiatBalance: complexProtocolPosition.totalValue,
             },
           ],
         },
@@ -224,9 +224,244 @@ describe('PortfolioMapper', () => {
     });
   });
 
-  it.todo('should map both regular and complex positions');
+  it('should map both regular and complex positions', () => {
+    const [key, chainId] = faker.helpers.arrayElement(chainKeys);
+    const regularProtocol = faker.string.sample();
+    const complexProtocol = faker.string.sample();
+    const protocolType = faker.helpers.arrayElement(ProtocolPositionType);
+    const regularProtocolPosition = regularProtocolPositionBuilder().build();
+    const regularProtocolPositions = {
+      [protocolType]: regularProtocolPosition,
+    };
+    const regularAssetByProtocolChains = assetByProtocolChainsBuilder()
+      .with(key, {
+        protocolPositions: regularProtocolPositions,
+      })
+      .build();
+    const regularAssetByProtocol = assetByProtocolBuilder()
+      .with('chains', regularAssetByProtocolChains)
+      .build();
+    const nestedProtocolPosition = nestedProtocolPositionBuilder().build();
+    const complexProtocolPosition = complexProtocolPositionBuilder()
+      .with('protocolPositions', [nestedProtocolPosition])
+      .build();
+    const complexProtocolPositions = {
+      [protocolType]: complexProtocolPosition,
+    };
+    const complexAssetByProtocolChains = assetByProtocolChainsBuilder()
+      .with(key, {
+        protocolPositions: complexProtocolPositions,
+      })
+      .build();
+    const complexAssetByProtocol = assetByProtocolBuilder()
+      .with('chains', complexAssetByProtocolChains)
+      .build();
+    const assetByProtocols = assetByProtocolsBuilder()
+      .with(regularProtocol, regularAssetByProtocol)
+      .with(complexProtocol, complexAssetByProtocol)
+      .build();
+    const portfolio = portfolioBuilder()
+      .with('assetByProtocols', assetByProtocols)
+      .build();
 
-  it.todo('should return no results if no positions are found');
+    const result = target.mapChainPortfolio({
+      chainId,
+      portfolio,
+    });
 
-  it.todo('should throw for unknown position types');
+    expect(result).toEqual({
+      results: [
+        {
+          fiatBalance: regularAssetByProtocol.value,
+          name: regularAssetByProtocol.name,
+          logoUri: regularAssetByProtocol.imgLarge,
+          protocolPositions: [
+            {
+              type: 'REGULAR',
+              name: regularProtocolPosition.name,
+              assets: regularProtocolPosition.assets.map((asset) => {
+                return {
+                  type: 'GENERAL',
+                  address: asset.contract,
+                  decimals: asset.decimal,
+                  logoUri: asset.imgSmall,
+                  name: asset.name,
+                  symbol: asset.symbol,
+                  balance: asset.balance,
+                  price: asset.price,
+                  fiatBalance: asset.value,
+                  trusted: false,
+                };
+              }),
+              fiatBalance: regularProtocolPosition.totalValue,
+            },
+          ],
+        },
+        {
+          fiatBalance: complexAssetByProtocol.value,
+          name: complexAssetByProtocol.name,
+          logoUri: complexAssetByProtocol.imgLarge,
+          protocolPositions: [
+            {
+              type: 'COMPLEX',
+              fiatBalance: complexProtocolPosition.totalValue,
+              name: complexProtocolPosition.name,
+              positions: [
+                {
+                  fiatBalance: nestedProtocolPosition.value,
+                  name: nestedProtocolPosition.name,
+                  healthRate: nestedProtocolPosition.healthRate,
+                  assets: [
+                    ...nestedProtocolPosition.assets.map((asset) => {
+                      return {
+                        type: 'GENERAL',
+                        address: asset.contract,
+                        decimals: asset.decimal,
+                        logoUri: asset.imgSmall,
+                        name: asset.name,
+                        symbol: asset.symbol,
+                        balance: asset.balance,
+                        price: asset.price,
+                        fiatBalance: asset.value,
+                        trusted: false,
+                      };
+                    }),
+                    ...(nestedProtocolPosition.borrowAssets ?? []).map(
+                      (asset) => {
+                        return {
+                          type: 'BORROW',
+                          address: asset.contract,
+                          decimals: asset.decimal,
+                          logoUri: asset.imgSmall,
+                          name: asset.name,
+                          symbol: asset.symbol,
+                          balance: asset.balance,
+                          price: asset.price,
+                          fiatBalance: asset.value,
+                          trusted: false,
+                        };
+                      },
+                    ),
+                    ...(nestedProtocolPosition.dexAssets ?? []).map((asset) => {
+                      return {
+                        type: 'DEX',
+                        address: asset.contract,
+                        decimals: asset.decimal,
+                        logoUri: asset.imgSmall,
+                        name: asset.name,
+                        symbol: asset.symbol,
+                        balance: asset.balance,
+                        price: asset.price,
+                        fiatBalance: asset.value,
+                        trusted: false,
+                      };
+                    }),
+                    ...(nestedProtocolPosition.rewardAssets ?? []).map(
+                      (asset) => {
+                        return {
+                          type: 'REWARDS',
+                          address: asset.contract,
+                          decimals: asset.decimal,
+                          logoUri: asset.imgSmall,
+                          name: asset.name,
+                          symbol: asset.symbol,
+                          balance: asset.balance,
+                          price: asset.price,
+                          fiatBalance: asset.value,
+                          trusted: false,
+                        };
+                      },
+                    ),
+                    ...(nestedProtocolPosition.supplyAssets ?? []).map(
+                      (asset) => {
+                        return {
+                          type: 'SUPPLY',
+                          address: asset.contract,
+                          decimals: asset.decimal,
+                          logoUri: asset.imgSmall,
+                          name: asset.name,
+                          symbol: asset.symbol,
+                          balance: asset.balance,
+                          price: asset.price,
+                          fiatBalance: asset.value,
+                          trusted: false,
+                        };
+                      },
+                    ),
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      count: 2,
+      next: null,
+      previous: null,
+    });
+  });
+
+  it('should return no results if no positions are found', () => {
+    const [key, chainId] = faker.helpers.arrayElement(chainKeys);
+    const protocol = faker.string.sample();
+    const assetByProtocolChains = assetByProtocolChainsBuilder().build();
+    // No positions on chain
+    delete assetByProtocolChains[key];
+    const assetByProtocol = assetByProtocolBuilder()
+      .with('chains', assetByProtocolChains)
+      .build();
+    const assetByProtocols = assetByProtocolsBuilder()
+      .with(protocol, assetByProtocol)
+      .build();
+    const portfolio = portfolioBuilder()
+      .with('assetByProtocols', assetByProtocols)
+      .build();
+
+    const result = target.mapChainPortfolio({
+      chainId,
+      portfolio,
+    });
+
+    expect(result).toEqual({
+      results: [],
+      count: 0,
+      next: null,
+      previous: null,
+    });
+  });
+
+  it('should throw for unknown position types', () => {
+    const [key, chainId] = faker.helpers.arrayElement(chainKeys);
+    const protocol = faker.string.sample();
+    const protocolType = faker.helpers.arrayElement(ProtocolPositionType);
+    const protocolPosition = protocolPositionBuilder()
+      // Unknown as both assets and protocolPositions are present
+      .with('assets', [portfolioAssetBuilder().build()])
+      .with('protocolPositions', [nestedProtocolPositionBuilder().build()])
+      .build();
+    const protocolPositions = { [protocolType]: protocolPosition };
+    const assetByProtocolChains = assetByProtocolChainsBuilder()
+      .with(key, {
+        protocolPositions,
+      })
+      .build();
+    const assetByProtocol = assetByProtocolBuilder()
+      .with('chains', assetByProtocolChains)
+      .build();
+    const assetByProtocols = assetByProtocolsBuilder()
+      .with(protocol, assetByProtocol)
+      .build();
+    const portfolio = portfolioBuilder()
+      .with('assetByProtocols', assetByProtocols)
+      .build();
+
+    expect(() =>
+      target.mapChainPortfolio({
+        chainId,
+        portfolio,
+      }),
+    ).toThrow(
+      `Unknown position. type=${protocolType}, assets=1, protocolPositions=1`,
+    );
+  });
 });
