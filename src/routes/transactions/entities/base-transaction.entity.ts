4156f81c10a0f0db3a9ca4c57719809fd40d97f7
Aaron Cook <aaron@safe.global>
2025-01-24 12:49:00 +0100

    Add and extend `BaseTransaction` entity (#2289)
    Adds a new `BaseTransaction` entity, which `Transaction`, `TransactionDetails` and `TransactionPreview` extend from:
    
    - Add `BaseTransaction` entity
    - Extend `TransactionDetails`, `TransactionPreview` and `Transaction` from it

---
+++ src/routes/transactions/entities/base-transaction.entity.ts
@@ -0,0 +1,47 @@
+import { ApiExtraModels, ApiProperty, getSchemaPath } from '@nestjs/swagger';
+import { CreationTransactionInfo } from '@/routes/transactions/entities/creation-transaction-info.entity';
+import { CustomTransactionInfo } from '@/routes/transactions/entities/custom-transaction.entity';
+import { SettingsChangeTransaction } from '@/routes/transactions/entities/settings-change-transaction.entity';
+import { NativeStakingDepositTransactionInfo } from '@/routes/transactions/entities/staking/native-staking-deposit-info.entity';
+import { NativeStakingValidatorsExitTransactionInfo } from '@/routes/transactions/entities/staking/native-staking-validators-exit-info.entity';
+import { NativeStakingWithdrawTransactionInfo } from '@/routes/transactions/entities/staking/native-staking-withdraw-info.entity';
+import { SwapOrderTransactionInfo } from '@/routes/transactions/entities/swaps/swap-order-info.entity';
+import { TwapOrderTransactionInfo } from '@/routes/transactions/entities/swaps/twap-order-info.entity';
+import { TransactionInfo } from '@/routes/transactions/entities/transaction-info.entity';
+import { TransferTransactionInfo } from '@/routes/transactions/entities/transfer-transaction-info.entity';
+import { SwapTransferTransactionInfo } from '@/routes/transactions/swap-transfer-transaction-info.entity';
+
+@ApiExtraModels(
+  TransactionInfo,
+  CreationTransactionInfo,
+  CustomTransactionInfo,
+  SettingsChangeTransaction,
+  TransferTransactionInfo,
+  SwapOrderTransactionInfo,
+  SwapTransferTransactionInfo,
+  TwapOrderTransactionInfo,
+  NativeStakingDepositTransactionInfo,
+  NativeStakingValidatorsExitTransactionInfo,
+  NativeStakingWithdrawTransactionInfo,
+)
+export class BaseTransaction {
+  @ApiProperty({
+    oneOf: [
+      { $ref: getSchemaPath(CreationTransactionInfo) },
+      { $ref: getSchemaPath(CustomTransactionInfo) },
+      { $ref: getSchemaPath(SettingsChangeTransaction) },
+      { $ref: getSchemaPath(TransferTransactionInfo) },
+      { $ref: getSchemaPath(SwapOrderTransactionInfo) },
+      { $ref: getSchemaPath(SwapTransferTransactionInfo) },
+      { $ref: getSchemaPath(TwapOrderTransactionInfo) },
+      { $ref: getSchemaPath(NativeStakingDepositTransactionInfo) },
+      { $ref: getSchemaPath(NativeStakingValidatorsExitTransactionInfo) },
+      { $ref: getSchemaPath(NativeStakingWithdrawTransactionInfo) },
+    ],
+  })
+  txInfo: TransactionInfo;
+
+  constructor(txInfo: TransactionInfo) {
+    this.txInfo = txInfo;
+  }
+}
