e9040c125b56bcbb6d1c3d638f4c433f667a6e8f
Pooya Raki <Pooya.raki@gmail.com>
2025-02-24 12:16:06 +0100

    feat: add verification for transaction notes (#2377)
    This PR improves and clarifies transaction notes for better accuracy and readability.

--- src/routes/transactions/mappers/multisig-transactions/multisig-transaction-note.mapper.spec.ts
+++ src/routes/transactions/mappers/multisig-transactions/multisig-transaction-note.mapper.spec.ts
@@ -16,6 +16,23 @@ describe('Multisig Transaction note mapper (Unit)', () => {
     expect(note).toBe(noteText);
   });
 
+  it('should parse transaction `origin` and return a valid note', async () => {
+    const noteText = `<script>document.write('<img src=s onerror=alert("Hello World")>')</script>`;
+    const noteText2 = `<script>&#97;&#108;&#101;&#114;&#116;('Hello World')</script>`;
+    const transaction = (await multisigTransactionBuilder())
+      .with('origin', JSON.stringify({ note: noteText }))
+      .build();
+    const transaction2 = (await multisigTransactionBuilder())
+      .with('origin', JSON.stringify({ note: noteText2 }))
+      .build();
+
+    const note = mapper.mapTxNote(transaction);
+    const note2 = mapper.mapTxNote(transaction2);
+
+    expect(note).toBe('');
+    expect(note2).toBe('');
+  });
+
   it('should return undefined if `origin` is not a valid JSON', async () => {
     const transaction = (await multisigTransactionBuilder())
       .with('origin', 'invalid-json')
