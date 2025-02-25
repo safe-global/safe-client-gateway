25628f23c8de8895923081113e4b91aa88284619
Aaron Cook <aaron@safe.global>
2025-02-25 12:49:51 +0100

    fix: add tests for `safeTxHash` generation

--- src/domain/common/utils/safe.ts
+++ src/domain/common/utils/safe.ts
@@ -7,26 +7,72 @@ const CHAIN_ID_DOMAIN_HASH_VERSION = '>=1.3.0';
 const TRANSACTION_PRIMARY_TYPE = 'SafeTx';
 const BASE_GAS_SAFETX_HASH_VERSION = '>=1.0.0';
 
+export type _BaseMultisigTransaction = Pick<
+  MultisigTransaction,
+  | 'to'
+  | 'value'
+  | 'data'
+  | 'operation'
+  | 'safeTxGas'
+  | 'baseGas'
+  | 'gasPrice'
+  | 'gasToken'
+  | 'refundReceiver'
+  | 'nonce'
+>;
+
 export function getSafeTxHash(args: {
   chainId: string;
-  transaction: Pick<
-    MultisigTransaction,
-    | 'to'
-    | 'value'
-    | 'data'
-    | 'operation'
-    | 'safeTxGas'
-    | 'baseGas'
-    | 'gasPrice'
-    | 'gasToken'
-    | 'refundReceiver'
-    | 'nonce'
-  >;
+  transaction: _BaseMultisigTransaction;
   safe: Safe;
 }): `0x${string}` {
   if (!args.safe.version) {
     throw new Error('Safe version is required');
   }
+
+  const domain = _getSafeTxDomain({
+    address: args.safe.address,
+    version: args.safe.version,
+    chainId: args.chainId,
+  });
+  const { types, message } = _getSafeTxTypesAndMessage({
+    transaction: args.transaction,
+    version: args.safe.version,
+  });
+
+  try {
+    return hashTypedData({
+      domain,
+      primaryType: TRANSACTION_PRIMARY_TYPE,
+      types,
+      message,
+    });
+  } catch {
+    throw new Error('Failed to hash transaction data');
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function _getSafeTxDomain(args: {
+  address: Safe['address'];
+  version: NonNullable<Safe['version']>;
+  chainId: string;
+}) {
+  const includesChainId = semverSatisfies(
+    args.version,
+    CHAIN_ID_DOMAIN_HASH_VERSION,
+  );
+  return {
+    ...(includesChainId && { chainId: Number(args.chainId) }),
+    verifyingContract: args.address,
+  };
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function _getSafeTxTypesAndMessage(args: {
+  transaction: _BaseMultisigTransaction;
+  version: NonNullable<Safe['version']>;
+}) {
   const {
     to,
     value,
@@ -52,53 +98,38 @@ export function getSafeTxHash(args: {
     throw new Error('Transaction data is incomplete');
   }
 
-  const includesChainId = semverSatisfies(
-    args.safe.version,
-    CHAIN_ID_DOMAIN_HASH_VERSION,
-  );
-  const domain = {
-    ...(includesChainId && { chainId: Number(args.chainId) }),
-    verifyingContract: args.safe.address,
-  };
-
   const usesBaseGas = semverSatisfies(
-    args.safe.version,
+    args.version,
     BASE_GAS_SAFETX_HASH_VERSION,
   );
   const dataGasOrBaseGas = (usesBaseGas ? 'baseGas' : 'dataGas') as 'baseGas';
 
-  try {
-    return hashTypedData({
-      domain,
-      primaryType: TRANSACTION_PRIMARY_TYPE,
-      types: {
-        [TRANSACTION_PRIMARY_TYPE]: [
-          { name: 'to', type: 'address' },
-          { name: 'value', type: 'uint256' },
-          { name: 'data', type: 'bytes' },
-          { name: 'operation', type: 'uint8' },
-          { name: 'safeTxGas', type: 'uint256' },
-          { name: dataGasOrBaseGas, type: 'uint256' },
-          { name: 'gasPrice', type: 'uint256' },
-          { name: 'gasToken', type: 'address' },
-          { name: 'refundReceiver', type: 'address' },
-          { name: 'nonce', type: 'uint256' },
-        ],
-      },
-      message: {
-        to,
-        value: BigInt(value),
-        data,
-        operation,
-        safeTxGas: BigInt(safeTxGas),
-        [dataGasOrBaseGas]: BigInt(baseGas),
-        gasPrice: BigInt(gasPrice),
-        gasToken,
-        refundReceiver,
-        nonce: BigInt(nonce),
-      },
-    });
-  } catch {
-    throw new Error('Failed to hash transaction data');
-  }
+  return {
+    types: {
+      [TRANSACTION_PRIMARY_TYPE]: [
+        { name: 'to', type: 'address' },
+        { name: 'value', type: 'uint256' },
+        { name: 'data', type: 'bytes' },
+        { name: 'operation', type: 'uint8' },
+        { name: 'safeTxGas', type: 'uint256' },
+        { name: dataGasOrBaseGas, type: 'uint256' },
+        { name: 'gasPrice', type: 'uint256' },
+        { name: 'gasToken', type: 'address' },
+        { name: 'refundReceiver', type: 'address' },
+        { name: 'nonce', type: 'uint256' },
+      ],
+    },
+    message: {
+      to,
+      value: BigInt(value),
+      data,
+      operation,
+      safeTxGas: BigInt(safeTxGas),
+      [dataGasOrBaseGas]: BigInt(baseGas),
+      gasPrice: BigInt(gasPrice),
+      gasToken,
+      refundReceiver,
+      nonce: BigInt(nonce),
+    },
+  };
 }
