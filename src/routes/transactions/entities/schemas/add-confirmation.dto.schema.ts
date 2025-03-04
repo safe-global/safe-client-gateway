7a1ac893f98591796da6908260822e6bf655c463
Aaron Cook <aaron@safe.global>
2025-03-04 10:13:33 +0100

    fix: allow confirmation DTOs signatures under `signedSafeTxHash` property

--- src/routes/transactions/entities/schemas/add-confirmation.dto.schema.ts
+++ src/routes/transactions/entities/schemas/add-confirmation.dto.schema.ts
@@ -1,6 +1,18 @@
 import { z } from 'zod';
 import { HexSchema } from '@/validation/entities/schemas/hex.schema';
 
-export const AddConfirmationDtoSchema = z.object({
-  signature: HexSchema,
-});
+export const AddConfirmationDtoSchema = z
+  .object({
+    signature: HexSchema,
+  })
+  .or(
+    z.object({
+      // Note: mobile proposes signatures under the signedSafeTxHash property
+      signedSafeTxHash: HexSchema,
+    }),
+  )
+  .transform((data) => {
+    return {
+      signature: 'signature' in data ? data.signature : data.signedSafeTxHash,
+    };
+  });
