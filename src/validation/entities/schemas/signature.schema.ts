fe02b7f834824c6eee2cf200811bddb23d3e605f
Aaron Cook <aaron@safe.global>
2025-03-11 11:51:40 +0100

    Update src/validation/entities/schemas/signature.schema.ts
    Co-authored-by: Héctor Gómez <hector@safe.global>

--- src/validation/entities/schemas/signature.schema.ts
+++ src/validation/entities/schemas/signature.schema.ts
@@ -2,7 +2,7 @@ import { HexBytesSchema } from '@/validation/entities/schemas/hexbytes.schema';
 
 // This does not take dynamic parts into account but we can safely
 // apply it to proposed signatures as we do not support contract
-// signatures in the inferface
+// signatures in the interface
 function isSignature(value: `0x${string}`): boolean {
   // We accept proposals of singular or concatenated signatures
   return (value.length - 2) % 130 === 0;
