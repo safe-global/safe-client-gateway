0b4d029487b50f1607858bf8735d4cae71cbd262
Héctor Gómez <hector@safe.global>
2025-03-17 12:57:07 +0100

    Update src/datasources/errors/helpers/is-unique-constraint-error.helper.ts
    Co-authored-by: Aaron Cook <aaron@safe.global>

--- src/datasources/errors/helpers/is-unique-constraint-error.helper.ts
+++ src/datasources/errors/helpers/is-unique-constraint-error.helper.ts
@@ -4,7 +4,9 @@ const UNIQUE_CONSTRAINT_ERROR_CODE = '23505';
 
 export function isUniqueConstraintError(
   err: unknown,
-): err is QueryFailedError & { driverError: { code: string; detail: string } } {
+): err is QueryFailedError & {
+  driverError: { code: typeof UNIQUE_CONSTRAINT_ERROR_CODE; detail?: string };
+} {
   return (
     err instanceof QueryFailedError &&
     'code' in err.driverError &&
