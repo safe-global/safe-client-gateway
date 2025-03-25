649157ef02fa8bfce587245224dca0422aa9619e
Daniel Dimitrov <daniel@compojoom.com>
2025-03-25 09:04:29 +0100

    fix: settings-change entity wrong SettingsInfo type (#2494)
    The possible settingsInfo types were wrong in the generated swagger and susequently in the generated RKT code.

--- src/routes/transactions/entities/settings-changes/set-guard.entity.ts
+++ src/routes/transactions/entities/settings-changes/set-guard.entity.ts
@@ -6,6 +6,9 @@ import {
 } from '@/routes/transactions/entities/settings-changes/settings-change.entity';
 
 export class SetGuard extends SettingsChange {
+  @ApiProperty({ enum: [SettingsChangeType.SetGuard] })
+  override type!: SettingsChangeType.SetGuard;
+
   @ApiProperty()
   guard: AddressInfo;
 
