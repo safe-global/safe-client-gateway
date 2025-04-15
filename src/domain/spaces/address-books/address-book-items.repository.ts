a8ef05f3b960db5738ca2e2e769028bf0482d808
Hector Gomez <hector@safe.global>
2025-04-15 18:29:35 +0200

    Add AddressBookItemsRepository.upsertMany

--- src/domain/spaces/address-books/address-book-items.repository.ts
+++ src/domain/spaces/address-books/address-book-items.repository.ts
@@ -1,3 +1,4 @@
+import { IConfigurationService } from '@/config/configuration.service.interface';
 import { PostgresDatabaseService } from '@/datasources/db/v2/postgres-database.service';
 import { AddressBookItem as DbAddressBookItem } from '@/datasources/spaces/entities/address-book-item.entity.db';
 import { AuthPayload } from '@/domain/auth/entities/auth-payload.entity';
@@ -6,39 +7,75 @@ import type { AddressBookItem } from '@/domain/spaces/address-books/entities/add
 import { Space } from '@/domain/spaces/entities/space.entity';
 import { ISpacesRepository } from '@/domain/spaces/spaces.repository.interface';
 import { IUsersRepository } from '@/domain/users/users.repository.interface';
-import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
-import { In } from 'typeorm';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { EntityManager, In } from 'typeorm';
 
 @Injectable()
 export class AddressBookItemsRepository implements IAddressBookItemsRepository {
+  private readonly maxAddressBookItems: number;
+
   constructor(
     private readonly db: PostgresDatabaseService,
     @Inject(ISpacesRepository)
     private readonly spacesRepository: ISpacesRepository,
     @Inject(IUsersRepository)
     private readonly userRepository: IUsersRepository,
-  ) {}
+    @Inject(IConfigurationService)
+    private readonly configurationService: IConfigurationService,
+  ) {
+    this.maxAddressBookItems = this.configurationService.getOrThrow<number>(
+      'spaces.addressBooks.maxAddressBookItems',
+    );
+  }
 
-  async findAllBySpaceId(args: {
+  public async findAllBySpaceId(args: {
     authPayload: AuthPayload;
     spaceId: Space['id'];
   }): Promise<Array<AddressBookItem>> {
-    const space = await this.getSpaceAsMember(args);
+    const space = await this.getSpaceAs({
+      ...args,
+      memberRoleIn: ['ADMIN', 'MEMBER'],
+    });
     const repository = await this.db.getRepository(DbAddressBookItem);
     return repository.findBy({ space: { id: space.id } });
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  upsertMany(args: {
+  public async upsertMany(args: {
     authPayload: AuthPayload;
     spaceId: Space['id'];
     addressBookItems: Array<AddressBookItem>;
   }): Promise<Array<AddressBookItem>> {
-    throw new Error('Method not implemented.');
+    const space = await this.getSpaceAs({
+      ...args,
+      memberRoleIn: ['ADMIN'],
+    });
+    const repository = await this.db.getRepository(DbAddressBookItem);
+    await this.db.transaction(async (entityManager) => {
+      const existingAddresses = await this.updateExistingAddressBookItems({
+        entityManager,
+        addressBookItems: args.addressBookItems,
+        space,
+        authPayload: args.authPayload,
+      });
+      await this.createNewAddressBookItems({
+        entityManager,
+        addressBookItems: args.addressBookItems.filter(
+          (item) => !existingAddresses.includes(item.address),
+        ),
+        space,
+        authPayload: args.authPayload,
+      });
+    });
+    return repository.findBy({ space: { id: space.id } });
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  deleteMany(args: {
+  public deleteMany(args: {
     authPayload: AuthPayload;
     spaceId: Space['id'];
     addressBookItemIds: Array<string>;
@@ -46,9 +83,10 @@ export class AddressBookItemsRepository implements IAddressBookItemsRepository {
     throw new Error('Method not implemented.');
   }
 
-  private async getSpaceAsMember(args: {
+  private async getSpaceAs(args: {
     authPayload: AuthPayload;
     spaceId: Space['id'];
+    memberRoleIn: Array<string>;
   }): Promise<Space> {
     if (!args.authPayload.signer_address) {
       throw new UnauthorizedException('Signer address not provided.');
@@ -59,8 +97,75 @@ export class AddressBookItemsRepository implements IAddressBookItemsRepository {
     return this.spacesRepository.findOneOrFail({
       where: {
         id: args.spaceId,
-        members: { status: In(['ACTIVE', 'INVITED']), user: { id: userId } },
+        members: {
+          status: In(['ACTIVE', 'INVITED']),
+          role: In(args.memberRoleIn),
+          user: { id: userId },
+        },
       },
     });
+  }
+
+  private async updateExistingAddressBookItems(args: {
+    authPayload: AuthPayload;
+    addressBookItems: Array<AddressBookItem>;
+    space: Space;
+    entityManager: EntityManager;
+  }): Promise<Array<DbAddressBookItem['address']>> {
+    const repository = args.entityManager.getRepository(DbAddressBookItem);
+    const existingAddressBookItems = await repository.findBy({
+      space: { id: args.space.id },
+      address: In(args.addressBookItems.map((item) => item.address)),
+    });
+    for (const item of existingAddressBookItems) {
+      const patch = args.addressBookItems.find(
+        (addressBookItem) => addressBookItem.address === item.address,
+      );
+      await repository.update(item.id, {
+        name: patch!.name,
+        chainIds: patch!.chainIds,
+        lastUpdatedBy: args.authPayload.signer_address,
+      });
+    }
+    return existingAddressBookItems.map((item) => item.address);
+  }
+
+  private async createNewAddressBookItems(args: {
+    authPayload: AuthPayload;
+    addressBookItems: Array<AddressBookItem>;
+    space: Space;
+    entityManager: EntityManager;
+  }): Promise<Array<DbAddressBookItem['id']>> {
+    await this.checkItemsLimit(args);
+    const repository = args.entityManager.getRepository(DbAddressBookItem);
+    const insertedIds = await repository.insert(
+      args.addressBookItems.map((addressBookItem) => ({
+        space: args.space,
+        address: addressBookItem.address,
+        name: addressBookItem.name,
+        chainIds: addressBookItem.chainIds,
+        createdBy: args.authPayload.signer_address,
+        lastUpdatedBy: args.authPayload.signer_address,
+      })),
+    );
+    return insertedIds.identifiers.map((i) => i.id);
+  }
+
+  private async checkItemsLimit(args: {
+    space: Space;
+    addressBookItems: Array<AddressBookItem>;
+    entityManager: EntityManager;
+  }): Promise<void> {
+    const repository = args.entityManager.getRepository(DbAddressBookItem);
+    const existingAddressBookItems = await repository.count({
+      where: { space: { id: args.space.id } },
+    });
+    const totalAddressBookItemsCount =
+      existingAddressBookItems + args.addressBookItems.length;
+    if (totalAddressBookItemsCount > this.maxAddressBookItems) {
+      throw new BadRequestException(
+        `This Space only allows a maximum of ${this.maxAddressBookItems} Address Book Items. You can only add up to ${this.maxAddressBookItems - existingAddressBookItems} more.`,
+      );
+    }
   }
 }
