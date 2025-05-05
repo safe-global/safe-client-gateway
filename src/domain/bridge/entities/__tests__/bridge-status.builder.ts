aa9279329de9dcf0de88c6f579d40b3fd1fc001a
Aaron Cook <aaron@safe.global>
2025-05-05 19:18:23 +0200

    feat: add schema for `BridgeStatus`

--- src/domain/bridge/entities/__tests__/bridge-status.builder.ts
+++ src/domain/bridge/entities/__tests__/bridge-status.builder.ts
@@ -1,8 +1,185 @@
+import { faker } from '@faker-js/faker';
+import { getAddress } from 'viem';
 import { Builder } from '@/__tests__/builder';
+import {
+  StatusMessages,
+  SubstatusesDone,
+  SubstatusesFailed,
+  SubstatusesPending,
+} from '@/domain/bridge/entities/bridge-status.entity';
 import type { IBuilder } from '@/__tests__/builder';
-import type { BridgeStatus } from '@/domain/bridge/entities/bridge-status.entity';
+import type {
+  BaseTransactionInfo,
+  BridgeStatus,
+  ExtendedTransactionInfo,
+  FailedStatusData,
+  FeeCost,
+  FullStatusData,
+  IncludedStep,
+  PendingReceivingInfo,
+  SetupToolDetails,
+  StatusData,
+  Token,
+  TransferMetadata,
+} from '@/domain/bridge/entities/bridge-status.entity';
+
+export function baseStatusDataBuilder<
+  T extends FullStatusData | StatusData | FailedStatusData,
+>(): IBuilder<T> {
+  return new Builder<T>()
+    .with('status', faker.helpers.arrayElement(StatusMessages))
+    .with(
+      'substatus',
+      faker.helpers.arrayElement([
+        ...SubstatusesPending,
+        ...SubstatusesDone,
+        ...SubstatusesFailed,
+      ]),
+    )
+    .with('substatusMessage', faker.lorem.sentence());
+}
+
+export function baseTransactionInfoBuilder<
+  T extends BaseTransactionInfo | ExtendedTransactionInfo = BaseTransactionInfo,
+>(): IBuilder<T> {
+  return new Builder<T>()
+    .with('txHash', faker.string.hexadecimal({ length: 64 }) as `0x${string}`)
+    .with('chainId', faker.string.numeric())
+    .with('txLink', faker.internet.url({ appendSlash: false }));
+}
+
+export function tokenBuilder(): IBuilder<Token> {
+  return new Builder<Token>()
+    .with('chainId', faker.string.numeric())
+    .with('address', getAddress(faker.finance.ethereumAddress()))
+    .with('symbol', faker.word.sample())
+    .with('decimals', faker.number.int({ min: 1, max: 18 }))
+    .with('name', faker.word.words())
+    .with('coinKey', faker.word.sample())
+    .with('logoURI', faker.internet.url({ appendSlash: false }))
+    .with('priceUSD', faker.number.float({ min: 0, max: 1_000 }).toString());
+}
+
+export function setupToolDetailsBuilder(): IBuilder<SetupToolDetails> {
+  return new Builder<SetupToolDetails>()
+    .with('key', faker.word.sample())
+    .with('name', faker.word.sample())
+    .with('logoURI', faker.internet.url({ appendSlash: false }));
+}
+
+export function includedStepBuilder(): IBuilder<IncludedStep> {
+  return new Builder<IncludedStep>()
+    .with('fromAmount', faker.number.float({ min: 0, max: 1_000 }).toString())
+    .with('fromToken', tokenBuilder().build())
+    .with('toAmount', faker.number.float({ min: 0, max: 1_000 }).toString())
+    .with('toToken', tokenBuilder().build())
+    .with(
+      'bridgedAmount',
+      faker.number.float({ min: 0, max: 1_000 }).toString(),
+    )
+    .with('tool', faker.word.sample())
+    .with('toolDetails', setupToolDetailsBuilder().build());
+}
+
+export function pendingReceivingInfoBuilder(): IBuilder<PendingReceivingInfo> {
+  return new Builder<PendingReceivingInfo>().with(
+    'chainId',
+    faker.string.numeric(),
+  );
+}
+
+export function extendedTransactionInfoBuilder<
+  T extends ExtendedTransactionInfo = ExtendedTransactionInfo,
+>(): IBuilder<T> {
+  return baseTransactionInfoBuilder<T>()
+    .with('amount', faker.number.float({ min: 0, max: 1_000 }).toString())
+    .with('amountUSD', faker.number.float({ min: 0, max: 1_000 }).toString())
+    .with('token', tokenBuilder().build())
+    .with('gasPrice', faker.number.float({ min: 0, max: 1_000 }).toString())
+    .with('gasUsed', faker.number.float({ min: 0, max: 1_000 }).toString())
+    .with('gasToken', tokenBuilder().build())
+    .with('gasAmount', faker.number.float({ min: 0, max: 1_000 }).toString())
+    .with('gasAmountUSD', faker.number.float({ min: 0, max: 1_000 }).toString())
+    .with('timestamp', faker.number.int())
+    .with('value', faker.number.float({ min: 0, max: 1_000 }).toString())
+    .with(
+      'includedSteps',
+      faker.helpers.multiple(() => includedStepBuilder().build(), {
+        count: { min: 1, max: 5 },
+      }),
+    );
+}
+
+export function feeCostBuilder(): IBuilder<FeeCost> {
+  return new Builder<FeeCost>()
+    .with('name', faker.word.sample())
+    .with('description', faker.lorem.sentence())
+    .with('percentage', faker.number.float({ min: 0, max: 1 }).toString())
+    .with('token', tokenBuilder().build())
+    .with('amount', faker.number.float({ min: 0, max: 1_000 }).toString())
+    .with('amountUSD', faker.number.float({ min: 0, max: 1_000 }).toString())
+    .with('included', faker.datatype.boolean());
+}
+
+export function transferMetadataBuilder(): IBuilder<TransferMetadata> {
+  return new Builder<TransferMetadata>().with(
+    'integrator',
+    faker.word.sample(),
+  );
+}
+
+export function fullStatusDataBuilder<
+  T extends FullStatusData = FullStatusData,
+>(): IBuilder<T> {
+  return baseStatusDataBuilder<T>()
+    .with(
+      'transactionId',
+      faker.string.hexadecimal({ length: 64 }) as `0x${string}`,
+    )
+    .with('sending', extendedTransactionInfoBuilder().build())
+    .with(
+      'receiving',
+      faker.helpers
+        .arrayElement([
+          pendingReceivingInfoBuilder(),
+          extendedTransactionInfoBuilder(),
+        ])
+        .build(),
+    )
+    .with(
+      'feeCosts',
+      faker.helpers.multiple(() => feeCostBuilder().build(), {
+        count: { min: 1, max: 5 },
+      }),
+    )
+    .with('lifiExplorerLink', faker.internet.url({ appendSlash: false }))
+    .with('fromAddress', getAddress(faker.finance.ethereumAddress()))
+    .with('toAddress', getAddress(faker.finance.ethereumAddress()))
+    .with('metadata', transferMetadataBuilder().build())
+    .with('bridgeExplorerLink', faker.internet.url({ appendSlash: false }));
+}
+
+export function statusDataBuilder<
+  T extends StatusData = StatusData,
+>(): IBuilder<T> {
+  return baseStatusDataBuilder<T>()
+    .with('tool', faker.word.sample())
+    .with('sending', baseTransactionInfoBuilder().build())
+    .with('receiving', pendingReceivingInfoBuilder().build());
+}
+
+export function failedStatusDataBuilder<
+  T extends FailedStatusData = FailedStatusData,
+>(): IBuilder<T> {
+  return baseStatusDataBuilder<T>()
+    .with('status', 'FAILED')
+    .with('sending', baseTransactionInfoBuilder().build());
+}
 
 export function bridgeStatusBuilder(): IBuilder<BridgeStatus> {
-  // TODO: Populate with BridgeStatus
-  return new Builder<BridgeStatus>();
+  return faker.helpers.arrayElement([
+    fullStatusDataBuilder(),
+    statusDataBuilder(),
+    failedStatusDataBuilder(),
+  ]);
 }
--- src/domain/bridge/entities/bridge-status.entity.spec.ts
+++ src/domain/bridge/entities/bridge-status.entity.spec.ts
@@ -1,1 +1,327 @@
-it.todo('BridgeStatusSchema');
+import { faker } from '@faker-js/faker';
+import { getAddress } from 'viem';
+import {
+  baseStatusDataBuilder,
+  baseTransactionInfoBuilder,
+  bridgeStatusBuilder,
+  extendedTransactionInfoBuilder,
+  failedStatusDataBuilder,
+  feeCostBuilder,
+  fullStatusDataBuilder,
+  includedStepBuilder,
+  pendingReceivingInfoBuilder,
+  setupToolDetailsBuilder,
+  statusDataBuilder,
+  tokenBuilder,
+  transferMetadataBuilder,
+} from '@/domain/bridge/entities/__tests__/bridge-status.builder';
+import {
+  BaseStatusDataSchema,
+  BaseTransactionInfoSchema,
+  BridgeStatusSchema,
+  ExtendedTransactionInfoSchema,
+  FailedStatusDataSchema,
+  FeeCostSchema,
+  FullStatusDataSchema,
+  IncludedStepSchema,
+  PendingReceivingInfoSchema,
+  SetupToolDetailsSchema,
+  StatusDataSchema,
+  StatusMessages,
+  TokenSchema,
+  TransferMetadataSchema,
+} from '@/domain/bridge/entities/bridge-status.entity';
+
+describe('BridgeStatusSchema', () => {
+  describe('BaseStatusDataSchema', () => {
+    it('should validate a BaseStatusData', () => {
+      const baseStatusData = baseStatusDataBuilder().build();
+
+      const result = BaseStatusDataSchema.safeParse(baseStatusData);
+
+      expect(result.success).toBe(true);
+    });
+
+    it.each(['status' as const, 'substatus' as const])(
+      '%s should fallback to UNKNOWN',
+      (key) => {
+        const baseStatusData = baseStatusDataBuilder()
+          .with(key, 'not a real status' as 'UNKNOWN')
+          .build();
+
+        const result = BaseStatusDataSchema.safeParse(baseStatusData);
+
+        expect(result.success && result.data[key]).toBe('UNKNOWN');
+      },
+    );
+
+    it('should default substatusMessage to null', () => {
+      const baseStatusData = baseStatusDataBuilder().build();
+      // @ts-expect-error - inferred type expects defined value
+      delete baseStatusData.substatusMessage;
+
+      const result = BaseStatusDataSchema.safeParse(baseStatusData);
+
+      expect(result.success && result.data.substatusMessage).toBe(null);
+    });
+  });
+
+  describe('BaseTransactionInfoSchema', () => {
+    it('should validate a BaseTransactionInfo', () => {
+      const baseTransactionInfo = baseTransactionInfoBuilder().build();
+
+      const result = BaseTransactionInfoSchema.safeParse(baseTransactionInfo);
+
+      expect(result.success).toBe(true);
+    });
+
+    it('should coerce chainId to string', () => {
+      const chainId = faker.number.int();
+      const baseTransactionInfo = baseTransactionInfoBuilder()
+        .with('chainId', chainId as unknown as string)
+        .build();
+
+      const result = BaseTransactionInfoSchema.safeParse(baseTransactionInfo);
+
+      expect(result.success && result.data.chainId).toBe(String(chainId));
+    });
+  });
+
+  describe('TokenSchema', () => {
+    it('should validate a Token', () => {
+      const token = tokenBuilder().build();
+
+      const result = TokenSchema.safeParse(token);
+
+      expect(result.success).toBe(true);
+    });
+
+    it('should coerce chainId to string', () => {
+      const chainId = faker.number.int();
+      const token = tokenBuilder()
+        .with('chainId', chainId as unknown as string)
+        .build();
+
+      const result = TokenSchema.safeParse(token);
+
+      expect(result.success && result.data.chainId).toBe(String(chainId));
+    });
+
+    it('should checksum address', () => {
+      const nonChecksummedAddress = faker.finance
+        .ethereumAddress()
+        .toLowerCase();
+      const token = tokenBuilder()
+        .with('address', nonChecksummedAddress as `0x${string}`)
+        .build();
+
+      const result = TokenSchema.safeParse(token);
+
+      expect(result.success && result.data.address).toBe(
+        getAddress(nonChecksummedAddress),
+      );
+    });
+
+    it.each(['coinKey' as const, 'logoURI' as const])(
+      '%s should default to null',
+      (key) => {
+        const token = tokenBuilder().build();
+        delete token[key];
+
+        const result = TokenSchema.safeParse(token);
+
+        expect(result.success && result.data[key]).toBe(null);
+      },
+    );
+  });
+
+  describe('SetupToolDetailsSchema', () => {
+    it('should validate a SetupToolDetails', () => {
+      const setupToolDetails = setupToolDetailsBuilder().build();
+
+      const result = SetupToolDetailsSchema.safeParse(setupToolDetails);
+
+      expect(result.success).toBe(true);
+    });
+  });
+
+  describe('IncludedStepSchema', () => {
+    it('should validate an IncludedStep', () => {
+      const includedStep = includedStepBuilder().build();
+
+      const result = IncludedStepSchema.safeParse(includedStep);
+
+      expect(result.success).toBe(true);
+    });
+
+    it('should default bridgedAmount to null', () => {
+      const includedStep = includedStepBuilder().build();
+      // @ts-expect-error - inferred type expects defined value
+      delete includedStep.bridgedAmount;
+
+      const result = IncludedStepSchema.safeParse(includedStep);
+
+      expect(result.success && result.data.bridgedAmount).toBe(null);
+    });
+  });
+
+  describe('PendingReceivingInfoSchema', () => {
+    it('should validate a PendingReceivingInfo', () => {
+      const pendingReceivingInfo = pendingReceivingInfoBuilder().build();
+
+      const result = PendingReceivingInfoSchema.safeParse(pendingReceivingInfo);
+
+      expect(result.success).toBe(true);
+    });
+
+    it('should coerce chainId to string', () => {
+      const chainId = faker.number.int();
+      const pendingReceivingInfo = pendingReceivingInfoBuilder()
+        .with('chainId', chainId as unknown as string)
+        .build();
+
+      const result = PendingReceivingInfoSchema.safeParse(pendingReceivingInfo);
+
+      expect(result.success && result.data.chainId).toBe(String(chainId));
+    });
+  });
+
+  describe('ExtendedTransactionInfoSchema', () => {
+    it('should validate an ExtendedTransactionInfo', () => {
+      const extendedTransactionInfo = extendedTransactionInfoBuilder().build();
+
+      const result = ExtendedTransactionInfoSchema.safeParse(
+        extendedTransactionInfo,
+      );
+
+      expect(result.success).toBe(true);
+    });
+
+    it.each([
+      'amount' as const,
+      'amountUSD' as const,
+      'token' as const,
+      'timestamp' as const,
+      'value' as const,
+      'includedSteps' as const,
+    ])('%s should default to null', (key) => {
+      const extendedTransactionInfo = extendedTransactionInfoBuilder().build();
+      delete extendedTransactionInfo[key];
+
+      const result = ExtendedTransactionInfoSchema.safeParse(
+        extendedTransactionInfo,
+      );
+
+      expect(result.success && result.data[key]).toBe(null);
+    });
+  });
+
+  describe('FeeCostSchema', () => {
+    it('should validate a FeeCost', () => {
+      const feeCost = feeCostBuilder().build();
+
+      const result = FeeCostSchema.safeParse(feeCost);
+
+      expect(result.success).toBe(true);
+    });
+  });
+
+  describe('TransferMetadataSchema', () => {
+    it('should validate a TransferMetadata', () => {
+      const transferMetadata = transferMetadataBuilder().build();
+
+      const result = TransferMetadataSchema.safeParse(transferMetadata);
+
+      expect(result.success).toBe(true);
+    });
+  });
+
+  describe('FullStatusDataSchema', () => {
+    it('should validate a FullStatusData', () => {
+      const fullStatusData = fullStatusDataBuilder().build();
+
+      const result = FullStatusDataSchema.safeParse(fullStatusData);
+
+      expect(result.success).toBe(true);
+    });
+
+    it.each(['fromAddress' as const, 'toAddress' as const])(
+      '%s should be checksummed',
+      (key) => {
+        const nonChecksummedAddress = faker.finance
+          .ethereumAddress()
+          .toLowerCase();
+        const fullStatusData = fullStatusDataBuilder()
+          .with(key, nonChecksummedAddress as `0x${string}`)
+          .build();
+
+        const result = FullStatusDataSchema.safeParse(fullStatusData);
+
+        expect(result.success && result.data[key]).toBe(
+          getAddress(nonChecksummedAddress),
+        );
+      },
+    );
+
+    it('should default bridgeExplorerLink to null', () => {
+      const fullStatusData = fullStatusDataBuilder().build();
+      // @ts-expect-error - inferred type expects defined value
+      delete fullStatusData.bridgeExplorerLink;
+
+      const result = FullStatusDataSchema.safeParse(fullStatusData);
+
+      expect(result.success && result.data.bridgeExplorerLink).toBe(null);
+    });
+  });
+
+  describe('StatusDataSchema', () => {
+    it('should validate a StatusData', () => {
+      const statusData = statusDataBuilder().build();
+
+      const result = StatusDataSchema.safeParse(statusData);
+
+      expect(result.success).toBe(true);
+    });
+  });
+
+  describe('FailedStatusDataSchema', () => {
+    it('should validate a FailedStatusData', () => {
+      const failedStatusData = failedStatusDataBuilder().build();
+
+      const result = FailedStatusDataSchema.safeParse(failedStatusData);
+
+      expect(result.success).toBe(true);
+    });
+
+    it('should only accept FAILED status', () => {
+      const status = faker.helpers.arrayElement(
+        StatusMessages.filter((status) => status !== 'FAILED'),
+      );
+      const failedStatusData = failedStatusDataBuilder()
+        .with('status', status as 'FAILED')
+        .build();
+
+      const result = FailedStatusDataSchema.safeParse(failedStatusData);
+
+      expect(!result.success && result.error.issues).toStrictEqual([
+        {
+          code: 'invalid_literal',
+          expected: 'FAILED',
+          message: 'Invalid literal value, expected "FAILED"',
+          path: ['status'],
+          received: status,
+        },
+      ]);
+    });
+  });
+
+  describe('BridgeStatusSchema', () => {
+    it('should validate a BridgeStatus', () => {
+      const bridgeStatus = bridgeStatusBuilder().build();
+
+      const result = BridgeStatusSchema.safeParse(bridgeStatus);
+
+      expect(result.success).toBe(true);
+    });
+  });
+});
