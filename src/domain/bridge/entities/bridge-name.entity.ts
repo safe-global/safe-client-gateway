d12b70217999422496f2934682ae112a791c44cd
Aaron Cook <aaron@safe.global>
2025-05-06 13:20:42 +0200

    fix: update comments

--- src/domain/bridge/entities/bridge-name.entity.ts
+++ src/domain/bridge/entities/bridge-name.entity.ts
@@ -23,7 +23,6 @@ export const BridgeNames = [
   'mayan',
 ] as const;
 
-// TODO: Validate incoming bridge name
 export const BridgeNameSchema = z.enum(BridgeNames);
 
 export type BridgeName = z.infer<typeof BridgeNameSchema>;
