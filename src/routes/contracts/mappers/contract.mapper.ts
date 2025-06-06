eb79274b4049b358d449d60832b79bd61c5149c0
dsh <sebastian0794@rambler.com>
2025-06-06 18:50:16 +0200

    feat: Switch contract fetching from Tx Service to Decoder Service

---
+++ src/routes/contracts/mappers/contract.mapper.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { Contract } from '@/domain/contracts/entities/contract.entity';
+import { Contract as DataDecoderContract } from '@/domain/data-decoder/v2/entities/contract.entity';
+
+@Injectable()
+export class ContractMapper {
+  constructor() {}
+
+  mapContract(contract: DataDecoderContract): Contract {
+    const contractAbi = contract.abi.abiJson
+      ? { abi: contract.abi.abiJson }
+      : null;
+    return {
+      address: contract.address,
+      name: contract.name,
+      displayName: contract.displayName ?? '',
+      logoUri: contract.project?.logoFile ?? null,
+      contractAbi: contractAbi,
+      trustedForDelegateCall: false, //TODO fix when the value is available
+    };
+  }
+}
