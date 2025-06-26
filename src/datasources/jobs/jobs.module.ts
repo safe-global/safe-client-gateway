c2793b54eb99c904526ceb769c0fddd1011f4898
Guilherme Endres <guilherme.endres@safe.global>
2025-06-26 16:56:38 +0200

    Move BullMQ configuration to app module

--- src/datasources/jobs/jobs.module.ts
+++ src/datasources/jobs/jobs.module.ts
@@ -2,24 +2,12 @@ import { Global, Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { JobsService } from '@/datasources/jobs/jobs.service';
 import { HelloWorldProcessor } from '@/datasources/jobs/processors/hello-world.processor';
-import { IConfigurationService } from '@/config/configuration.service.interface';
 
 export const JOBS_QUEUE_NAME = 'jobs';
 
 @Global()
 @Module({
   imports: [
-    BullModule.forRootAsync({
-      inject: [IConfigurationService],
-      useFactory: (configurationService: IConfigurationService) => ({
-        connection: {
-          host: configurationService.getOrThrow<string>('redis.host'),
-          port: Number(configurationService.getOrThrow<string>('redis.port')),
-          username: configurationService.get<string>('redis.user'),
-          password: configurationService.get<string>('redis.pass'),
-        },
-      }),
-    }),
     BullModule.registerQueue({
       name: JOBS_QUEUE_NAME,
       defaultJobOptions: {
