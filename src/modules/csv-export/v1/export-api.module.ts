c4434c0585c6d926ab1f33c10d5c750dfa13b6ec
dsh <11198975+LucieFaire@users.noreply.github.com>
2025-06-26 13:39:26 +0200

    feat: add datasource for export api
    Signed-off-by: dsh <11198975+LucieFaire@users.noreply.github.com>

---
+++ src/modules/csv-export/v1/export-api.module.ts
@@ -0,0 +1,12 @@
+import { CacheFirstDataSourceModule } from '@/datasources/cache/cache.first.data.source.module';
+import { HttpErrorFactory } from '@/datasources/errors/http-error-factory';
+import { IExportApi } from '@/modules/csv-export/v1/export-api.interface';
+import { ExportApi } from '@/modules/csv-export/v1/export-api.service';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [CacheFirstDataSourceModule],
+  providers: [HttpErrorFactory, { provide: IExportApi, useClass: ExportApi }],
+  exports: [IExportApi],
+})
+export class ExportApiModule {}
