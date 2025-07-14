4c7c9d9a78f6cd590ea6aa080c2e4c29b97e1160
dsh <11198975+LucieFaire@users.noreply.github.com>
2025-07-14 15:50:19 +0200

    add csv-utils module
    Signed-off-by: dsh <11198975+LucieFaire@users.noreply.github.com>

--- src/modules/csv-export/v1/csv-export.module.ts
+++ src/modules/csv-export/v1/csv-export.module.ts
@@ -1,7 +1,16 @@
+import { CloudStorageModule } from '@/datasources/storage/cloud-storage.module';
+import { CsvModule } from '@/modules/csv-export/csv-utils/csv.module';
 import { CsvExportService } from '@/modules/csv-export/v1/csv-export.service';
 import { Module } from '@nestjs/common';
 
 @Module({
+  imports: [
+    CsvModule,
+    CloudStorageModule.register(
+      'csvExport.fileStorage.aws.bucketName',
+      'csvExport.fileStorage.aws.basePath',
+    ),
+  ],
   providers: [CsvExportService],
   exports: [CsvExportService],
 })
