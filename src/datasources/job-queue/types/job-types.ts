3d1c41d905fd58c2a0bbebee4c8c430f94a16d47
Dasha Kobzeva <11198975+LucieFaire@users.noreply.github.com>
2025-07-22 16:50:26 +0200

    Revert "feat: Add BullMQ job queue system (#2625)" (#2654)
    This reverts commit 73d1ecb30298ea71c481453050cc2392fbcc50bb.

--- src/datasources/job-queue/types/job-types.ts
+++
@@ -1,9 +0,0 @@
-export enum JobType {
-  TEST_JOB = 'test-job', // for testing purposes
-}
-
-export type JobTypeName = (typeof JobType)[keyof typeof JobType];
-
-export interface JobData {
-  timestamp: number;
-}
