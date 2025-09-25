5797e9c273cbae90beec52cad47374d8fca4ce06
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-09-25 11:23:21 +0200

    refactor: extract mapping logic for recipient analysis results into a new utility function

--- src/modules/safe-shield/recipient-analysis/recipient-analysis.service.ts
+++ src/modules/safe-shield/recipient-analysis/recipient-analysis.service.ts
@@ -24,6 +24,7 @@ import { ILoggingService, LoggingService } from '@/logging/logging.interface';
 import { extractRecipients } from '../utils/recipient-extraction.utils';
 import { logCacheHit, logCacheMiss } from '@/modules/safe-shield/utils/common';
 
+
 /**
  * Service responsible for analyzing transaction recipients and bridge configurations.
  */
