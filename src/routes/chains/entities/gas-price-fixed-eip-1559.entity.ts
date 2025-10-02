05c98ef50127e2e1c56aaafb6dba0ed652ce3573
Daniel Dimitrov <daniel.d@safe.global>
2025-10-02 14:17:59 +0200

    fix: improve swagger types

--- src/routes/chains/entities/gas-price-fixed-eip-1559.entity.ts
+++ src/routes/chains/entities/gas-price-fixed-eip-1559.entity.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { GasPriceFixedEIP1559 as DomainGasPriceFixedEIP1559 } from '@/domain/chains/entities/gas-price-fixed-eip-1559.entity';
 
 export class GasPriceFixedEIP1559 implements DomainGasPriceFixedEIP1559 {
-  @ApiProperty()
+  @ApiProperty({ enum: ['fixed1559'] })
   type!: 'fixed1559';
   @ApiProperty()
   maxFeePerGas!: string;
