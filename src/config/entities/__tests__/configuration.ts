e330514a6120dd1466574718748659e3f6a375f5
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-10-02 13:43:58 +0200

    Replace URL generation for key 37111 with 232

--- src/config/entities/__tests__/configuration.ts
+++ src/config/entities/__tests__/configuration.ts
@@ -336,7 +336,7 @@ export default (): ReturnType<typeof configuration> => ({
       100: faker.internet.url({ appendSlash: false }),
       137: faker.internet.url({ appendSlash: false }),
       8453: faker.internet.url({ appendSlash: false }),
-      37111: faker.internet.url({ appendSlash: false }),
+      232: faker.internet.url({ appendSlash: false }),
       42161: faker.internet.url({ appendSlash: false }),
       43114: faker.internet.url({ appendSlash: false }),
       11155111: faker.internet.url({ appendSlash: false }),
