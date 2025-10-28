32e181c8dd7fbe06d643dd1ea07c64ffd636ffc3
valleXYZ <valentin.seehausen@gmail.com>
2025-10-28 19:51:25 +0100

    refactor(chains): improve ChainIdSchema and move ChainIdsSchema

--- src/domain/chains/entities/schemas/chain-id.schema.ts
+++ src/domain/chains/entities/schemas/chain-id.schema.ts
@@ -1,9 +1,11 @@
-import { NumericStringSchema } from '@/validation/entities/schemas/numeric-string.schema';
+import { z } from 'zod';
 import { CHAIN_ID_MAXLENGTH } from '@/routes/common/constants';
 
-export const ChainIdSchema = NumericStringSchema.refine(
-  (val) => val.length <= CHAIN_ID_MAXLENGTH,
-  {
-    message: `Value must be less than or euqal to ${CHAIN_ID_MAXLENGTH}`,
-  },
-);
+export const ChainIdSchema = z
+  .string()
+  .regex(/^\d+$/, {
+    message: 'Chain ID must be a positive integer',
+  })
+  .refine((val) => val.length <= CHAIN_ID_MAXLENGTH, {
+    message: `Value must be less than or equal to ${CHAIN_ID_MAXLENGTH}`,
+  });
