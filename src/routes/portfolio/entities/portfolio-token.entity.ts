18c0e0a7d57c8226dfa6d4d1aba8dfdfb85062aa
valleXYZ <valentin.seehausen@gmail.com>
2025-11-04 16:55:51 +0100

    fix: remove extra types

--- src/routes/portfolio/entities/portfolio-token.entity.ts
+++ src/routes/portfolio/entities/portfolio-token.entity.ts
@@ -1,11 +1,10 @@
-import { ApiExtraModels, ApiProperty } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 import {
   NativeToken,
   Erc20Token,
   Erc721Token,
 } from '@/routes/balances/entities/token.entity';
 
-@ApiExtraModels(NativeToken, Erc20Token, Erc721Token)
 export class PortfolioNativeToken extends NativeToken {
   @ApiProperty({ description: 'The chain ID' })
   chainId!: string;
@@ -16,7 +15,6 @@ export class PortfolioNativeToken extends NativeToken {
   trusted!: boolean;
 }
 
-@ApiExtraModels(NativeToken, Erc20Token, Erc721Token)
 export class PortfolioErc20Token extends Erc20Token {
   @ApiProperty({ description: 'The chain ID' })
   chainId!: string;
@@ -27,7 +25,6 @@ export class PortfolioErc20Token extends Erc20Token {
   trusted!: boolean;
 }
 
-@ApiExtraModels(NativeToken, Erc20Token, Erc721Token)
 export class PortfolioErc721Token extends Erc721Token {
   @ApiProperty({ description: 'The chain ID' })
   chainId!: string;
