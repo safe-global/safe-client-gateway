d907bbf5e65373557c52f4fc0ae37a94bef97560
valleXYZ <valentin.seehausen@gmail.com>
2025-11-04 12:19:52 +0100

    fix: revert ChainIdSchema

--- src/domain/chains/entities/schemas/chain-id.schema.ts
+++ src/domain/chains/entities/schemas/chain-id.schema.ts
@@ -1,11 +1,9 @@
-import { z } from 'zod';
+import { NumericStringSchema } from '@/validation/entities/schemas/numeric-string.schema';
 import { CHAIN_ID_MAXLENGTH } from '@/routes/common/constants';
 
-export const ChainIdSchema = z
-  .string()
-  .regex(/^\d+$/, {
-    message: 'Chain ID must be a positive integer',
-  })
-  .refine((val) => val.length <= CHAIN_ID_MAXLENGTH, {
-    message: `Value must be less than or equal to ${CHAIN_ID_MAXLENGTH}`,
-  });
+export const ChainIdSchema = NumericStringSchema.refine(
+  (val) => val.length <= CHAIN_ID_MAXLENGTH,
+  {
+    message: `Value must be less than or euqal to ${CHAIN_ID_MAXLENGTH}`,
+  },
+);
