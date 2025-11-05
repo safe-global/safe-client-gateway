79fd175c3239b8ef9a2fa3119e764277d98586aa
dsh <11198975+LucieFaire@users.noreply.github.com>
2025-11-05 10:35:28 +0100

    fix(safe-shield): disable state override for Blockaid
    Signed-off-by: dsh <11198975+LucieFaire@users.noreply.github.com>

--- src/modules/safe-shield/threat-analysis/blockaid/blockaid-api.service.ts
+++ src/modules/safe-shield/threat-analysis/blockaid/blockaid-api.service.ts
@@ -1,5 +1,5 @@
 import { IBlockaidApi } from '@/modules/safe-shield/threat-analysis/blockaid/blockaid-api.interface';
-import { GUARD_STORAGE_POSITION } from '@/modules/safe-shield/threat-analysis/blockaid/blockaid-api.constants';
+// import { GUARD_STORAGE_POSITION } from '@/modules/safe-shield/threat-analysis/blockaid/blockaid-api.constants';
 import Blockaid from '@blockaid/client';
 import { TransactionScanResponse } from '@blockaid/client/resources/evm/evm';
 import { JsonRpcScanParams } from '@blockaid/client/resources/evm/json-rpc';
@@ -31,15 +31,18 @@ export class BlockaidApi implements IBlockaidApi {
       options: ['simulation', 'validation'],
       metadata: origin ? { domain: origin } : { non_dapp: true as const },
       account_address: walletAddress,
-      state_override: {
-        [safeAddress]: {
-          stateDiff: {
-            // Set the Guard storage slot to zero address to disable guard
-            [GUARD_STORAGE_POSITION]:
-              '0x0000000000000000000000000000000000000000000000000000000000000000',
-          },
-        },
-      },
+
+      // Temporary disable state override (to be reverted in
+      // https://linear.app/safe-global/issue/COR-802/put-back-blockaid-state-override)
+      // state_override: {
+      //   [safeAddress]: {
+      //     stateDiff: {
+      //       // Set the Guard storage slot to zero address to disable guard
+      //       [GUARD_STORAGE_POSITION]:
+      //         '0x0000000000000000000000000000000000000000000000000000000000000000',
+      //     },
+      //   },
+      // },
     };
 
     return await this.blockaidClient.evm.jsonRpc.scan(params);
