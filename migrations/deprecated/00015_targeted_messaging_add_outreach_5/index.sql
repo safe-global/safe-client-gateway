e0b0f6819bc13f2ea57e605426f70738c1b37585
Dasha Kobzeva <11198975+LucieFaire@users.noreply.github.com>
2025-11-27 10:58:08 +0100

    fix: update HN outreach migration (#2814)
    Signed-off-by: dsh <11198975+LucieFaire@users.noreply.github.com>

--- migrations/deprecated/00015_targeted_messaging_add_outreach_5/index.sql
+++ migrations/deprecated/00015_targeted_messaging_add_outreach_5/index.sql
@@ -8,13 +8,12 @@ INSERT INTO outreaches (
     source_file,
     source_file_checksum
 ) VALUES (
-    3,
+    5,
     'Hypernative Outreach',
     '1970-01-01 00:00:00.000',
     '1970-01-01 00:00:00.000',
     '',
     'Core Experience',
     'targeted_feature_hypernative_safes.json',
-    'b131a35ebcc8a842798a20250d44bbfcd1a59378a2ca8541733efd1ae13bc9a5'
+    '400b155c81558593db42f6d931c4fc25d6911680a287d08a65900871f234023a'
 );
-
