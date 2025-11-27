ba623a337834a8c88ab8dd1959e86cd19abb1bc8
Greg Jeanmart <greg@jeanmart.me>
2025-11-27 11:21:17 +0100

    migrate portfolio

--- src/modules/portfolio/domain/portfolio.service.interface.ts
+++ src/modules/portfolio/domain/portfolio.service.interface.ts
@@ -1,8 +1,5 @@
-import { Module } from '@nestjs/common';
 import type { Address } from 'viem';
 import type { Portfolio } from '@/modules/portfolio/domain/entities/portfolio.entity';
-import { PortfolioService } from '@/modules/portfolio/domain/portfolio.service';
-import { PortfolioRepositoryModule } from '@/modules/portfolio/domain/portfolio.repository.module';
 
 export const IPortfolioService = Symbol('IPortfolioService');
 
@@ -17,15 +14,3 @@ export interface IPortfolioService {
 
   clearPortfolio(args: { address: Address }): Promise<void>;
 }
-
-@Module({
-  imports: [PortfolioRepositoryModule],
-  providers: [
-    {
-      provide: IPortfolioService,
-      useClass: PortfolioService,
-    },
-  ],
-  exports: [IPortfolioService],
-})
-export class PortfolioServiceModule {}
--- src/modules/portfolio/portfolio.module.ts
+++ src/modules/portfolio/portfolio.module.ts
@@ -1,15 +1,31 @@
 import { Module } from '@nestjs/common';
-import { PortfolioApiModule } from '@/modules/portfolio/datasources/portfolio-api.module';
-import { PortfolioRepositoryModule } from '@/modules/portfolio/domain/portfolio.repository.module';
-import { PortfolioServiceModule } from '@/modules/portfolio/domain/portfolio.service.interface';
-import { PortfolioModule as PortfolioV1Module } from '@/modules/portfolio/v1/portfolio.module';
+import { HttpErrorFactory } from '@/datasources/errors/http-error-factory';
+import { IPortfolioApi } from '@/modules/portfolio/interfaces/portfolio-api.interface';
+import { ZerionPortfolioApi } from '@/modules/portfolio/datasources/zerion-portfolio-api.service';
+import { PortfolioRepository } from '@/modules/portfolio/domain/portfolio.repository';
+import { IPortfolioRepository } from '@/modules/portfolio/domain/portfolio.repository.interface';
+import { PortfolioService } from '@/modules/portfolio/domain/portfolio.service';
+import { IPortfolioService } from '@/modules/portfolio/domain/portfolio.service.interface';
+import { PortfolioController } from '@/modules/portfolio/v1/portfolio.controller';
+import { PortfolioApiService } from '@/modules/portfolio/v1/portfolio.service';
+import { PortfolioRouteMapper } from '@/modules/portfolio/v1/portfolio.mapper';
 
 @Module({
-  imports: [
-    PortfolioApiModule,
-    PortfolioRepositoryModule,
-    PortfolioServiceModule,
-    PortfolioV1Module,
+  controllers: [PortfolioController],
+  providers: [
+    HttpErrorFactory,
+    { provide: IPortfolioApi, useClass: ZerionPortfolioApi },
+    {
+      provide: IPortfolioRepository,
+      useClass: PortfolioRepository,
+    },
+    {
+      provide: IPortfolioService,
+      useClass: PortfolioService,
+    },
+    PortfolioApiService,
+    PortfolioRouteMapper,
   ],
+  exports: [IPortfolioApi, IPortfolioRepository, IPortfolioService],
 })
 export class PortfolioModule {}
