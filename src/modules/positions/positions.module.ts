bf49d9a910e26bc0b375b75b72ae1803742d5682
Greg Jeanmart <greg@jeanmart.me>
2025-11-27 11:15:41 +0100

    Migrate position module

--- src/modules/positions/positions.module.ts
+++ src/modules/positions/positions.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
-import { PositionsApiModule } from '@/modules/positions/datasources/positions-api.module';
-import { PositionsRepositoryModule } from '@/modules/positions/domain/positions.repository.interface';
-import { PositionsModule as PositionsRoutesModule } from '@/modules/positions/routes/positions.module';
+import { CacheFirstDataSourceModule } from '@/datasources/cache/cache.first.data.source.module';
+import { HttpErrorFactory } from '@/datasources/errors/http-error-factory';
+import { IPositionsApi } from '@/domain/interfaces/positions-api.interface';
+import { ZerionPositionsApi } from '@/modules/positions/datasources/zerion-positions-api.service';
+import { PositionsRepository } from '@/modules/positions/domain/positions.repository';
+import { IPositionsRepository } from '@/modules/positions/domain/positions.repository.interface';
+import { PositionsController } from '@/modules/positions/routes/positions.controller';
+import { PositionsService } from '@/modules/positions/routes/positions.service';
+import { ChainsModule } from '@/modules/chains/chains.module';
 
 @Module({
-  imports: [
-    PositionsApiModule,
-    PositionsRepositoryModule,
-    PositionsRoutesModule,
+  imports: [CacheFirstDataSourceModule, ChainsModule],
+  controllers: [PositionsController],
+  providers: [
+    HttpErrorFactory,
+    { provide: IPositionsApi, useClass: ZerionPositionsApi },
+    {
+      provide: IPositionsRepository,
+      useClass: PositionsRepository,
+    },
+    PositionsService,
   ],
+  exports: [IPositionsApi, IPositionsRepository],
 })
 export class PositionsModule {}
