a715b05bea57fb578ddafd4a6ceed9cca60a0259
Greg Jeanmart <greg@jeanmart.me>
2025-11-28 09:54:09 +0100

    partially merge csv export

--- src/modules/csv-export/csv-export.module.ts
+++ src/modules/csv-export/csv-export.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { CsvModule } from '@/modules/csv-export/csv-utils/csv.module';
-import { CsvExportModule as CsvExportV1Module } from '@/modules/csv-export/v1/csv-export.module';
+import { CsvService } from '@/modules/csv-export/csv-utils/csv.service';
+import { CsvExportV1Module } from '@/modules/csv-export/v1/csv-export.module';
 
 @Module({
-  imports: [CsvModule, CsvExportV1Module],
+  imports: [CsvExportV1Module],
+  providers: [CsvService],
+  exports: [CsvService],
 })
 export class CsvExportModule {}
--- src/modules/csv-export/csv-utils/csv.module.ts
+++
@@ -1,8 +0,0 @@
-import { CsvService } from '@/modules/csv-export/csv-utils/csv.service';
-import { Module } from '@nestjs/common';
-
-@Module({
-  providers: [CsvService],
-  exports: [CsvService],
-})
-export class CsvModule {}
