57de80e1df18694938ef257c61d0206c69db1458
Greg Jeanmart <greg@jeanmart.me>
2025-11-28 08:49:31 +0100

    partially migrate hooks

--- src/modules/hooks/hooks.module.ts
+++ src/modules/hooks/hooks.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
 import { EventCacheHelperModule } from '@/modules/hooks/domain/helpers/event-cache.helper.module';
 import { EventNotificationsHelperModule } from '@/modules/hooks/domain/helpers/event-notifications.helper';
-import { HooksRepositoryModule } from '@/modules/hooks/domain/hooks.repository.interface';
-import { HooksModule as HooksRoutesModule } from '@/modules/hooks/routes/hooks.module';
+import { HooksRepository } from '@/modules/hooks/domain/hooks.repository';
+import { IHooksRepository } from '@/modules/hooks/domain/hooks.repository.interface';
+import { HooksController } from '@/modules/hooks/routes/hooks.controller';
+import { HooksService } from '@/modules/hooks/routes/hooks.service';
+import { QueuesRepositoryModule } from '../queues/domain/queues-repository.interface';
 
 @Module({
   imports: [
     EventCacheHelperModule,
     EventNotificationsHelperModule,
-    HooksRepositoryModule,
-    HooksRoutesModule,
+    QueuesRepositoryModule,
   ],
+  controllers: [HooksController],
+  providers: [
+    { provide: IHooksRepository, useClass: HooksRepository },
+    HooksService,
+  ],
+  exports: [IHooksRepository],
 })
 export class HooksModule {}
