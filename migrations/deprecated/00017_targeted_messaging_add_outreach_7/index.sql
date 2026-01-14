c7a15c47c25a1ae74be1d95bbaf5b2f99359d899
dsh <11198975+LucieFaire@users.noreply.github.com>
2026-01-14 17:02:10 +0100

    feat: new (updated)  HN campaign migration
    Signed-off-by: dsh <11198975+LucieFaire@users.noreply.github.com>

---
+++ migrations/deprecated/00017_targeted_messaging_add_outreach_7/index.sql
@@ -0,0 +1,19 @@
+INSERT INTO outreaches (
+    source_id,
+    name,
+    start_date,
+    end_date,
+    type,
+    team_name,
+    source_file,
+    source_file_checksum
+) VALUES (
+    7,
+    'Hypernative Outreach (update 14.01.26)',
+    '1970-01-01 00:00:00.000',
+    '1970-01-01 00:00:00.000',
+    '',
+    'Wallet',
+    'targeted_feature_hypernative_safes_14-01-26.json',
+    '6140f3db250aa20154c93219ea24ad5a1274a3ce3c612b2a263f9dae67a7af2e'
+);
