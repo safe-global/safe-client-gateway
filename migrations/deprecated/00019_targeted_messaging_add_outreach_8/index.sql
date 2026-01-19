47f03235b374e6bb37489390c08542a4dca63eee
Dasha Kobzeva <11198975+LucieFaire@users.noreply.github.com>
2026-01-19 17:05:59 +0100

    feat: new HN outreach migration (#2872)
    Signed-off-by: dsh <11198975+LucieFaire@users.noreply.github.com>

---
+++ migrations/deprecated/00019_targeted_messaging_add_outreach_8/index.sql
@@ -0,0 +1,19 @@
+INSERT INTO outreaches (
+    source_id,
+    name,
+    start_date,
+    end_date,
+    type,
+    team_name,
+    source_file,
+    source_file_checksum
+) VALUES (
+    8,
+    'Hypernative Allowlist',
+    '1970-01-01 00:00:00.000',
+    '1970-01-01 00:00:00.000',
+    '',
+    'Wallet',
+    'targeted_feature_hypernative_allowlist_campaign.json',
+    'b9c2d0182412ef2e3f6c58986ac427a7342a7ffb348cda745e42c2be2d906709'
+);
