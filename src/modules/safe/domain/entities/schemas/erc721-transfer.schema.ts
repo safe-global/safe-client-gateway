a148d39a11749e86f1e870c6edfa5dbf8dd97141
dsh <11198975+LucieFaire@users.noreply.github.com>
2026-02-05 16:09:45 +0100

    add new transfer base schema
    Signed-off-by: dsh <11198975+LucieFaire@users.noreply.github.com>

--- src/modules/safe/domain/entities/schemas/erc721-transfer.schema.ts
+++ src/modules/safe/domain/entities/schemas/erc721-transfer.schema.ts
@@ -1,15 +1,9 @@
 import { z } from 'zod';
 import { AddressSchema } from '@/validation/entities/schemas/address.schema';
-import { HexSchema } from '@/validation/entities/schemas/hex.schema';
+import { TransferBaseSchema } from '@/modules/safe/domain/entities/schemas/transfer-base.schema';
 
-export const Erc721TransferSchema = z.object({
+export const Erc721TransferSchema = TransferBaseSchema.extend({
   type: z.literal('ERC721_TRANSFER'),
-  executionDate: z.coerce.date(),
-  blockNumber: z.number(),
-  transactionHash: HexSchema,
-  to: AddressSchema,
-  from: AddressSchema,
   tokenId: z.string(),
   tokenAddress: AddressSchema,
-  transferId: z.string(),
 });
