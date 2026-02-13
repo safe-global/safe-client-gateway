770156d1f8f7d2026e726b4ef6d78ae28304b028
Aryan Shah <aryan@safe.global>
2026-02-13 14:59:02 +0530

    chore: formatting

--- src/modules/users/datasources/entities/__tests__/users.entity.db.builder.ts
+++ src/modules/users/datasources/entities/__tests__/users.entity.db.builder.ts
@@ -8,10 +8,7 @@ import { UserStatus } from '@/modules/users/domain/entities/user.entity';
 export function userBuilder(): IBuilder<User> {
   return new Builder<User>()
     .with('id', faker.number.int())
-    .with(
-      'status',
-      faker.helpers.arrayElement(getStringEnumKeys(UserStatus)),
-    )
+    .with('status', faker.helpers.arrayElement(getStringEnumKeys(UserStatus)))
     .with('wallets', [])
     .with('members', [])
     .with('createdAt', new Date())
