5acb8d530148ffc3c50afc7e8acd92fc3cfaa33a
dsh <11198975+LucieFaire@users.noreply.github.com>
2026-02-23 16:48:30 +0100

    add licence headers
    Signed-off-by: dsh <11198975+LucieFaire@users.noreply.github.com>

--- src/modules/relay/domain/relay.repository.ts
+++ src/modules/relay/domain/relay.repository.ts
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: FSL-1.1-MIT
 import { Inject, Injectable } from '@nestjs/common';
 import { IRelayManager } from '@/modules/relay/domain/interfaces/relay-manager.interface';
 import { Relay } from '@/modules/relay/domain/entities/relay.entity';
--- src/modules/relay/routes/relay.service.ts
+++ src/modules/relay/routes/relay.service.ts
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: FSL-1.1-MIT
 import { Injectable } from '@nestjs/common';
 import { RelayRepository } from '@/modules/relay/domain/relay.repository';
 import { RelayDto } from '@/modules/relay/routes/entities/relay.dto.entity';
