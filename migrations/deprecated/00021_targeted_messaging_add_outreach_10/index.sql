722222b0d81f0e51f0ffbb044e23b67ad8bb34e2
dsh <11198975+LucieFaire@users.noreply.github.com>
2026-03-06 14:11:25 +0100

    chore: update HN allowlist

---
+++ migrations/deprecated/00021_targeted_messaging_add_outreach_10/index.sql
@@ -0,0 +1,21 @@
+-- SPDX-License-Identifier: FSL-1.1-MIT
+
+INSERT INTO outreaches (
+    source_id,
+    name,
+    start_date,
+    end_date,
+    type,
+    team_name,
+    source_file,
+    source_file_checksum
+) VALUES (
+    10,
+    'Hypernative Allowlist (update 06.03.26)',
+    '1970-01-01 00:00:00.000',
+    '1970-01-01 00:00:00.000',
+    '',
+    'Wallet',
+    'targeted_feature_hypernative_allowlist_campaign_06-03-26.json',
+    '782f43d02c65ce44e21bd5b066d8f59f2cdafe5c9cda74f7c0e0f0f6fdd374e8'
+);
