9566e5ee1d886b231cbca08811abc3423367ba60
dsh <11198975+LucieFaire@users.noreply.github.com>
2026-03-18 16:22:49 +0100

    override auth0 module in tests

--- src/modules/auth/auth0/auth0.module.ts
+++ src/modules/auth/auth0/auth0.module.ts
@@ -1,14 +1,22 @@
 // SPDX-License-Identifier: FSL-1.1-MIT
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@/datasources/jwt/jwt.module';
-import { Auth0ApiModule } from '@/modules/auth/auth0/datasources/auth0-api.module';
+import { NetworkModule } from '@/datasources/network/network.module';
+import { HttpErrorFactory } from '@/datasources/errors/http-error-factory';
+import { IAuth0Api } from '@/modules/auth/auth0/datasources/auth0-api.interface';
+import { Auth0Api } from '@/modules/auth/auth0/datasources/auth0-api.service';
 import { Auth0TokenVerifier } from '@/modules/auth/auth0/domain/auth0-token.verifier';
 import { IAuth0Repository } from '@/modules/auth/auth0/domain/auth0.repository.interface';
 import { Auth0Repository } from '@/modules/auth/auth0/domain/auth0.repository';
 
 @Module({
-  imports: [Auth0ApiModule, JwtModule],
+  imports: [NetworkModule, JwtModule],
   providers: [
+    HttpErrorFactory,
+    {
+      provide: IAuth0Api,
+      useClass: Auth0Api,
+    },
     Auth0TokenVerifier,
     {
       provide: IAuth0Repository,
