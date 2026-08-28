c7d2f1bc788c716ff907036c70aa956c6d25b57e
Akshay Patel <16358982+akshay-ap@users.noreply.github.com>
2026-08-28 13:40:34 +0200

    feat(polices): Configuration changes

--- src/modules/policies/policies.module.ts
+++ src/modules/policies/policies.module.ts
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: FSL-1.1-MIT
-import { Module } from "@nestjs/common";
-import { HttpErrorFactory } from "@/datasources/errors/http-error-factory";
-import { PolicyIndexerApi } from "@/modules/policies/datasources/policy-indexer-api.service";
-import { PolicyIndexerRepository } from "@/modules/policies/domain/policy-indexer.repository";
-import { IPolicyIndexerRepository } from "@/modules/policies/domain/policy-indexer.repository.interface";
+import { Module } from '@nestjs/common';
+import { HttpErrorFactory } from '@/datasources/errors/http-error-factory';
+import { PolicyIndexerApi } from '@/modules/policies/datasources/policy-indexer-api.service';
+import { PolicyIndexerRepository } from '@/modules/policies/domain/policy-indexer.repository';
+import { IPolicyIndexerRepository } from '@/modules/policies/domain/policy-indexer.repository.interface';
 
 /**
  * A policy reaches a Safe through one of three mechanisms - an enabled module,
